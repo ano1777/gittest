c60976779e59eb993eb12ef327e637d328eca3c1
S <s>
2023-09-30 13:41:04 +0400

    {"types":"C","runs":1}

--- aa.java
+++ aa.java
@@ -1,4 +1,11 @@
-
-public class aa {
-
+import stanford.karel.Karel;
+public class aa extends karel{
+ public void run() {
+	 move();
+	 pickBeeper(); 
+	 putBeeper();
+	 turnLeft();
+	 move();
+}
+ 
 }

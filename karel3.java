cdf15b19c908888a8134dbe10ec92eb4e59ff6b7
S <s>
2023-10-01 15:17:41 +0400

    {"types":"C","runs":5}

--- karel3.java
+++ karel3.java
@@ -1,7 +1,34 @@
 import stanford.karel.Karel;
 public class karel3 extends Karel {
-public void run(){
+	void turnRight(){
+		turnLeft();
+		turnLeft();
+		turnLeft();
+	}
+ public void run(){
+	while(facingNorth()){
+		for(int i=0;i<3;i++){
+		putBeeper();
+		move();
+		}
+		turnRight();
+		move();
+		turnRight();
+	while (facingSouth()){
+		for(int i=0;i<3;i++){
+			move();
+		}
+		turnLeft();
+		move();
+		turnLeft();
+	}
+	}
 	
+	
+	
+	
+	
+
 	
 }
 }

61c2186d0a7112bc5f819b815a887802670634ee
S <s>
2023-10-03 17:15:41 +0400

    {"types":"C","runs":12}

--- bonus1.java
+++ bonus1.java
@@ -1,4 +1,28 @@
+import stanford.karel.*;
+public class bonus1 extends Karel{
+	void turnRight(){
+		turnLeft();
+		turnLeft();
+		turnLeft();
+	
+	}
+public void run(){
+	while(frontIsClear()){
+		putBeeper();
+		move();
+	}
+	while(facingEast()){
+		if (frontIsBlocked()){
+			turnLeft();
+			move();
+			turnLeft();
+		}
+		if (facingWest()){
+			turnRight();
+			move();
+		}
+		
+	}
 
-public class bonus1 {
-
+}
 }

998f9cd2e9bfac4c00114b4dba55a3aaa6bc8a77
S <s>
2023-10-03 23:02:41 +0400

    {"types":"C","runs":7}

--- task6.java
+++ task6.java
@@ -1,4 +1,23 @@
-
-public class task6 {
-
+import stanford.karel.Karel;
+public class task6 extends Karel{
+	private void putBeeper2(){
+		putBeeper();
+		putBeeper();
+	}
+	private void turnAround(){
+		turnLeft();
+		turnLeft();
+	}
+public void run(){
+	move();
+	while (beepersPresent()){
+		pickBeeper();
+		turnLeft();
+		move();
+		putBeeper2();
+	    turnAround();
+	    move();
+	    turnAround();
+	}
+}
 }

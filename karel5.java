ece2229aea63ab5b635d853c5babcb30a7c4c5a9
S <s>
2023-10-05 19:18:48 +0400

    {"types":"C","runs":19}

--- karel5.java
+++ karel5.java
@@ -3,12 +3,59 @@ public class karel5 extends SuperKarel{
 	public void run(){
 		move();
 		backupBeepers();
-		//backupBeepersOnTheSecondPile
-		//goBackAndPutBeepersBack
+		backupBeepersOnTheSecondPile();
+		goBackAndPutBeepersBack1();
+		goBackAndPutBeepersBack2();
 		
 	
 	}
 	
+	private void goBackAndPutBeepersBack2() {
+		turnAround();
+		move();
+		while(beepersPresent()){
+		turnRight();
+		move();
+		pickBeeper();
+		turnAround();
+		move();
+		putBeeper();
+		turnAround();
+		
+	}
+
+	private void goBackAndPutBeepersBack1() {
+		turnLeft();
+		move();
+		while(beepersPresent()){
+			pickBeeper();
+			turnAround();
+			move();
+			putBeeper();
+			turnLeft();
+		}
+     		
+	}
+
+	private void backupBeepersOnTheSecondPile() {
+		move();
+		while(beepersPresent()){
+			pickBeeper();
+			turnLeft();
+			move();
+			putBeeper();
+			turnAround();
+			move();
+			turnLeft();
+			move();
+			putBeeper();
+			turnAround();
+			move();
+			turnAround();
+		}
+    		
+	}
+
 	private void backupBeepers(){
 		while(beepersPresent()){
 			pickBeeper();

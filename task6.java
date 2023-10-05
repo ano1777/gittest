000cfb9fb9f310126c95c279bbb00a9c64377cc9
S <s>
2023-10-05 23:17:16 +0400

    {"types":"C","runs":45}

--- task6.java
+++ task6.java
@@ -1,24 +1,17 @@
 import stanford.karel.SuperKarel;
 public class task6 extends SuperKarel{
 	public void run(){
-		moveTowards();
-		makeOperation();
-		}
-		
-	private void makeOperation() {
-        putBeeper();
-        turnLeft();
-	}
-
-	private void moveTowards() {
-		 for (int i=0;i<8;i++){
-			if (frontIsClear()){
-		        move();
-			}else{
-				makeOperation();
-			
+		for (int i=0;i<8;i++){
+			while(frontIsClear()){
+				move();
 			}
-		}
+			while(frontIsBlocked()){
+				turnLeft();
+				putBeeper();
+			}
+		
+			}
+		
 	}
 	}
 	

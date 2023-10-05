79fd28b947d5a2703a5c27fa512e24e094c896ff
S <s>
2023-10-05 18:53:02 +0400

    {"types":"C","runs":16}

--- task6.java
+++ task6.java
@@ -1,10 +1,9 @@
 import stanford.karel.SuperKarel;
 public class task6 extends SuperKarel{
 	public void run(){
-		for(int i=0;i<7;i++){
+		for(int i=0;i<8;i++) {
 			move();	
-		
-		jumpOverTheWall();
+	        jumpOverTheWall();
 		}
 	}
 	

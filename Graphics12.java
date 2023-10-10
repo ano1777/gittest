0727cc85bfea6308a73ef9a437e4eb48cc109e6f
S <s>
2023-10-10 17:32:13 +0400

    {"types":"C","runs":9}

--- Graphics12.java
+++ Graphics12.java
@@ -1,3 +1,4 @@
+import acm.graphics.GOval;
 import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
 
@@ -5,7 +6,9 @@ public class Graphics12 extends GraphicsProgram{
 	public void run(){
 		GRect myRect = new GRect (100,200);
 		add (myRect,100,100);
-		add (myRect,200,200);
+		add(myRect,200,200);
+		GOval myOval =new GOval (100,200);
+		add (myOval,100,100);
 		
 	}
 

e783cda4f5d20c2139d6b26f23d709702646fdab
S <s>
2023-10-10 18:00:07 +0400

    {"types":"C","runs":15}

--- Graphics12.java
+++ Graphics12.java
@@ -10,15 +10,21 @@ import acm.program.GraphicsProgram;
 public class Graphics12 extends GraphicsProgram{
 	public void run(){
 		GRect myRect = new GRect (100,200);
-		add (myRect,100,100);
-		add(myRect,250,300);
+		add (myRect,200,300);
+		add(myRect,250,270);
 		
 		GOval myOval =new GOval (100,100);
 		add (myOval,100,100);
 		
 		add (myOval);
 		myOval.setFilled(true);
-		myOval.setColor(Color.RED);
+		myOval.setColor(Color.YELLOW);
+		
+		GLine myGLine=new GRect(250,270,200,330);
+		add (myGLine(200,330));
+	
+	}
+
 	
 	}
 

72b0e294a7ab455a83f509223c403cee5ce825fb
S <s>
2023-10-11 23:41:13 +0400

    {"types":"C","runs":15}

--- Graphics12.java
+++ Graphics12.java
@@ -35,14 +35,18 @@ public class Graphics12 extends GraphicsProgram{
         
         GRect GRect2=new GRect(70,100);
         add (GRect2,360,220);
+        
         GLine GLine2=new GLine(305,220,305,290);
         add (GLine2,305,220);
 
-        GLine GLine4=new GLine(270,290,349,290);
+        GLine GLine4=new GLine(270,255,305,255);
         add (GLine4,270,290);
         
-        GLine GLine3=new GLine(305,220,395,220);
+        GLine GLine3=new GLine(360,255,430,255);
         add (GLine3,305,220);
+        
+        GLine GLine5=new GLine(395,220,395,290);
+        add (GLine5,395,220);
         
         
        

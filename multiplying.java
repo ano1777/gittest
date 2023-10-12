079fb6773c5dc1eb84919870ef20f132ac1c8093
S <s>
2023-10-12 22:24:33 +0400

    {"types":"C","runs":2}

--- multiplying.java
+++ multiplying.java
@@ -1,4 +1,25 @@
+import stanford.karel.SuperKarel;
 
-public class multiplying {
+public class multiplying extends SuperKarel{
+	public void run(){
+		move();
+		while(beepersPresent()){
+			pickBeeper();
+			backUpBeeper();
+		}
+		
+		
+	}
+
+	private void backUpBeeper() {
+		// backs up the beeper while beeper is present
+		turnLeft();
+		move();
+		putBeeper();
+		turnAround();
+		move();
+		turnLeft();
+		
+	}
 
 }

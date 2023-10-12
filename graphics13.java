79c9c32a5d46d1ff8179926502c1f81c92fd51cb
S <s>
2023-10-12 17:26:15 +0400

    {"types":"C","runs":1}

--- graphics13.java
+++ graphics13.java
@@ -1,4 +1,23 @@
+import acm.graphics.GLabel;
+import acm.graphics.GLine;
+import acm.program.GraphicsProgram;
 
-public class graphics13 {
+public class graphics13 extends GraphicsProgram{
+	public void run(){
+		drawMidLine();
+	}
+
+	private void drawMidLine() {
+		double x1=0;
+		double y1= getHeight() / 2;
+		double x2= getWidth();
+		double y2= y1;
+		GLine line = new GLine(x1,y1,x2,y2);
+		add (line);
+		
+		
+	}
+		
+	}
 
 }

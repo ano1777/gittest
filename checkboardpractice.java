8a084b36931c5f0c47c5874e54757be622c33ed0
S <s>
2023-10-13 08:51:33 +0400

    {"types":"C","runs":6}

--- checkboardpractice.java
+++ checkboardpractice.java
@@ -2,20 +2,77 @@ import stanford.karel.SuperKarel;
 
 public class checkboardpractice extends SuperKarel {
 	public void run(){
-		while(frontIsClear()){
-			putBeeper();
-			if(frontIsClear()){
-			move();
-			turnLeft();
-			move();
-			putBeeper();
-			turnRight();
-			move();
-			turnLeft();
-			move();
-			turnRight();
-		}
+		
+		while(leftIsClear()){
+		fillTheLine();
+		goBack();
+		goUpToTheNextLine1();
+		fillTheLine2();
+		goBack();
+		
 	}
 
 }
+
+	private void goUpToTheNextLine1() {
+		// TODO Auto-generated method stub
+		while(rightIsClear()){
+		turnRight();
+		}
+		move();
+		turnLeft();
+		fillTheLine2();
+		}
+
+	private void fillTheLine2() {
+		// TODO Auto-generated method stub
+		while(frontIsClear()){
+			move();
+			putBeeper();
+			if(frontIsClear()){
+				move();
+			}
+			if(frontIsClear()){
+				move();
+			    putBeeper();
+			}
+			while(leftIsClear()){
+			  if(frontIsBlocked()){
+				goBack();
+			  }
+		    }
+	  }
+	}
+
+	private void fillTheLine() {
+		// TODO Auto-generated method stub
+		while(frontIsClear()){
+			putBeeper();
+			if (frontIsClear()){
+				move();
+				
+			}
+			if(frontIsClear()){
+				move();
+			}
+			while(leftIsClear()){
+			  if(frontIsBlocked()){
+				goBack();
+			 }
+			}
+		}
+	}
+
+	private void goBack() {
+		// TODO Auto-generated method stub
+		turnAround();
+		while(frontIsClear()){
+			move();
+			if(frontIsBlocked()){
+				goUpToTheNextLine1();
+			}
+		}
+		
+	}
 }
+

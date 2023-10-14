c35defaafa05df6359ed0075b4b0ad008f7d6fbb
S <s>
2023-10-14 08:35:35 +0400

    {"types":"C","runs":15}

--- checkboardpractice.java
+++ checkboardpractice.java
@@ -1,6 +1,25 @@
 import stanford.karel.SuperKarel;
 
-public class checkboardpractice extends SuperKarel {
+ public class checkboardpractice extends SuperKarel {
 	public void run(){
+		fillTheLine();
+	}
+
+	private void fillTheLine() {
+		/*goes to the last pile and puts beeper on one pile from to
+		  */
+		while(frontIsClear()){
+		putBeeper();
+		if(frontIsClear()){
+			move();
+		}
+		if (frontIsClear()){
+			move();
+			putBeeper();
+		}
 		
-	
+		
+	}
+		
+}
+ }

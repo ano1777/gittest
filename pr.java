879d77daa7792303adcaeb20d8dfcc9c29bf6e45
S <s>
2023-10-15 08:42:24 +0400

    {"types":"C","runs":3}

--- pr.java
+++ pr.java
@@ -7,26 +7,49 @@ public void run(){
 	pickBeepersExceptTheMiddle();
 	
 }
-
-private void putBeepersOnTheSides() {
-	// TODO Auto-generated method stub
-	putBeeper();
-	while(frontIsClear()){
-	     move();
-	     if(frontIsBlocked()){
-	    	 putBeeper();
+/*
+ * before this function karel stands on 1x1, facing East. firstly, it puts beeper on that spot and then goes
+ * to the last pile of the row, at the wall, puts beeper on it and finally turns around.
+ * after this action karel is facing West.
+ */
+  private void putBeepersOnTheSides() {
+	   putBeeper();
+	     while(frontIsClear()){
+	      move();
+	      if(frontIsBlocked()){
+	     	 putBeeper();
 	    	 turnAround();
 	    	 
 	     }
 	}
 }
+ /*
+  * 
+  */
 
-
-private void fillTheRowAnMoveToTheMiddlePoint() {
-	// TODO Auto-generated method stub
+    private void fillTheRowAnMoveToTheMiddlePoint() {
+    	while(frontIsClear()){
+    	move();
+    	putBeeper();
+    	while(noBeepersPresent()){
+    		move();
+    		if(beepersPresent()){
+    			turnAround();
+    			if(frontIsClear()){
+    			move();
+    			putBeeper();
+    			}
+    		}
+    	}
 	
-
+    	}
 }
+
+    private void pickBeepersExceptTheMiddle() {
+	
+	
+}
+
 
 }
 

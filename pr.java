ac603635abe626a0f59c42f151c717ce2a92f988
S <s>
2023-10-15 08:36:02 +0400

    {"types":"C","runs":2}

--- pr.java
+++ pr.java
@@ -2,10 +2,33 @@ import stanford.karel.SuperKarel;
 
 public class pr extends SuperKarel {
 public void run(){
+	putBeepersOnTheSides();
+	fillTheRowAnMoveToTheMiddlePoint();
+	pickBeepersExceptTheMiddle();
 	
 }
 
+private void putBeepersOnTheSides() {
+	// TODO Auto-generated method stub
+	putBeeper();
+	while(frontIsClear()){
+	     move();
+	     if(frontIsBlocked()){
+	    	 putBeeper();
+	    	 turnAround();
+	    	 
+	     }
+	}
+}
 
+
+private void fillTheRowAnMoveToTheMiddlePoint() {
+	// TODO Auto-generated method stub
+	
+
+}
+
+}
 
 
 

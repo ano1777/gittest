0dfc24ddcdcb80a1ccae02e35211702c10ffb6b8
S <s>
2023-10-15 11:43:20 +0400

    {"types":"C","runs":34}

--- karel1.java
+++ karel1.java
@@ -38,3 +38,85 @@ public class karel1 extends SuperKarel{
 
 }
 }
+
+
+
+private void pickBeepersExceptTheMiddle() {
+	if(beepersPresent()){
+		while(frontIsClear()){
+			move();
+			pickBeeper();
+			if(frontIsBlocked()){
+				pickBeeper();
+				turnAround();
+				goToTheOtherHalf();
+			}
+		}
+	}
+
+
+}
+/*
+ * karel is facing West.it must go to the 1x1 pile, picking up 
+ * remaining beepers except the middle one.
+ */
+ private void goToTheOtherHalf() {
+	 while(noBeepersPresent()){
+		 move();
+		 if(beepersPresent()){
+			 move();
+			 pickUpAllBeepers();
+		 }
+		 if(frontIsBlocked()){
+			 goToTheMiddlePoint();
+		 }
+	 }
+	
+	
+}
+private void goToTheMiddlePoint() {
+	// TODO Auto-generated method stub
+	turnAround();
+	while(noBeepersPresent()){
+		move();
+	}
+}
+private void pickUpAllBeepers() {
+	// TODO Auto-generated method stub
+	while(beepersPresent()){
+	pickBeeper();
+	if(frontIsClear()){
+	move();
+	}
+	
+}
+
+
+}
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

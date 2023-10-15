b0046b42c04047bea8fdf4b9c074b33baedec2d0
S <s>
2023-10-15 14:35:13 +0400

    {"types":"C","runs":48}

--- pr.java
+++ pr.java
@@ -4,12 +4,12 @@ public class pr extends SuperKarel {
   public void run(){
 	  if(frontIsBlocked()){
     checkIfFrontAndLeftIsClear();
-	  }
+	  
 	putTwoBeepersMirroringEachOther();
 	standOnTheMiddlePoint();
 	
   
-
+  }
 
   }
    private void checkIfFrontAndLeftIsClear() {

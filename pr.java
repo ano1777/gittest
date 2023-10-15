94b287a59463af202b03b33c7cc7a143cd5a2815
S <s>
2023-10-15 15:42:45 +0400

    {"types":"C","runs":56}

--- pr.java
+++ pr.java
@@ -26,7 +26,7 @@ public class pr extends SuperKarel {
   
 /*
  * the function helps karel to solve the middle point finding problem in 2x2 world.
- *  it makes the robot move and put  the diamond on 2x1 pile.
+ *  it makes the robot move to the East and put  the diamond on 2x1 pile.
  */
 
     private void checkTheWorld2x2() {
@@ -35,31 +35,82 @@ public class pr extends SuperKarel {
 		  putBeeper();
 	    }
     }
-
-    
-    private void standOnTheMiddlePoint() {
-	    turnAround();
-	    move();
-	    pickBeeper();
-}
    
+/*
+ * before doing this method, karel stands on 1x1, facing East. at first, it must move
+ * forwards until the wall is present. when it reaches the wall, it puts diamond on
+ * the pile on which it is standing, turn around and do the same thing on the West
+ * side of the world. Then it takes beeper from the 1x1 pile and carries it to 2x1,
+ * does the same thing on the East side, turns back to the West and does the same 
+ * thing until it reaches the middle point of the first row.
+ */
+
+
+   private void putTwoBeepersMirroringEachOther() { 
+	   putBeepersOnTheSides();
+	   while(beepersPresent()){	   
+		   carryBeeperToTheNextPile();
+		   moveForwards();
+		   carryBeeperToTheNextPile2();
+	    }
+   }
+   
+/*
+ * before this action karel stands on 1x1, facing East.
+ * this function makes karel put diamonds on the edges of the first row. 
+ * at first it moves while front is clear and puts beeper on the last pile. then
+ * the robot turns around and moves to the West until the wall is present, it puts beeper there, too.
+ * after this method the robot stands on the 1x1, facing West.
+ */
+
+	private void putBeepersOnTheSides() {
+   	    while(frontIsClear()){
+   		    move();
+   		    if(frontIsBlocked()){	
+   		     	putBeeper();
+   		        turnAround();
+   		         while(frontIsClear()){
+   		            	move();
+   		    	        if(frontIsBlocked()){
+   		    		    putBeeper();
+   		    	        } 
+   		         }
+   		     }
+   		
+       	}
+	}
+	
+/*
+ * before this function karel stands on 1x1, facing West, and the beeper is present on that pile.
+ * karel must take this beeper and put it on 2x1. to do this, it must  pick the beeper, turn around,
+ * move and put beeper. with this function the robot carries beepers from West to the East and
+ * it works on the West side of the middle point. this method is in the "while" cycle, which means that it will be 
+ * repeated until it reaches the middle point of the row.
+ */
+	private void carryBeeperToTheNextPile() {		
+		pickBeeper();
+		turnAround();
+		move();
+		putBeeper();
+	}
+   
+	
 /*
  * 
  */
 
-
-   private void putTwoBeepersMirroringEachOther() {
-	
-	 putBeepersOnTheSides();
-	 while(beepersPresent()){	
-		carryBeeperToTheNextPile();
-	
-		moveForwards();
+	private void moveForwards() {
+		move();
+	    while(noBeepersPresent()){
+		move();
+		if(beepersPresent()){
 		carryBeeperToTheNextPile2();
+		}
 	}
-}
+ }
    
-
+   
+   
  /*
   * 
   */
@@ -72,55 +123,17 @@ public class pr extends SuperKarel {
 	putBeeper();
 	move();
 }
-    
-/*
- * 
- */
 
-	private void putBeepersOnTheSides() {
-	// TODO Auto-generated method stub
-    	while(frontIsClear()){
-    		move();
-    		if(frontIsBlocked()){
-    			putBeeper();
-    		    turnAround();
-    		    while(frontIsClear()){
-    		    	move();
-    		    	if(frontIsBlocked()){
-    		    		putBeeper();
-    		    	}
-    		    }
-    		}
-    		
-    	}
-}
+
+
 	
-/*
- */
- 
-
-	private void carryBeeperToTheNextPile() {
-	// TODO Auto-generated method stub
-	pickBeeper();
-	turnAround();
-	move();
-	putBeeper();
-}
 	
-/*
- * 
- */
-
-	private void moveForwards() {
-	// TODO Auto-generated method stub
-		move();
-	while(noBeepersPresent()){
-		move();
-		if(beepersPresent()){
-		carryBeeperToTheNextPile2();
-		}
-	}
- }
+	 
+    private void standOnTheMiddlePoint() {
+	    turnAround();
+	    move();
+	    pickBeeper();
+}
 }
     
     

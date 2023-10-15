3737ddf2fff113182b482591c1c3e37826d2460a
S <s>
2023-10-15 10:44:05 +0400

    {"types":"C","runs":31}

--- karel1.java
+++ karel1.java
@@ -1,13 +1,40 @@
 import stanford.karel.*;
-public class karel1 extends Karel{
+public class karel1 extends SuperKarel{
  public void run(){
-
-	while (frontIsClear()){
-		if(noBeepersPresent()){
-			putBeeper();
-		}
-		move();
-	}
+	 while(frontIsClear()){ 
+	    	move();
+		    if(beepersPresent()){
+		    	turnAround(); 
+		    	move();
+		    	if(noBeepersPresent()){
+		    	putBeeper();
+		    }
+		    	if(beepersPresent()){
+		    		move();
+		    		if(noBeepersPresent()){
+		    			move();
+		    		
+		    		if(noBeepersPresent()){
+		    			turnAround();
+		    			move();
+		    			move();
+		    			
+		    		}
+		    		}
+		    	}
+		    }     
+			    while(noBeepersPresent()){
+			    	move();
+			    	if(beepersPresent()){
+			    		turnAround();
+			    		move();
+			    		if(noBeepersPresent()){
+			    		putBeeper();
+			    		}
+			    	}
+			    }
+			}
+	 
 
 }
 }

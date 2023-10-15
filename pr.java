b2bbc88d55fda5e230fa87eec8c8669ebf5e0899
S <s>
2023-10-15 09:12:37 +0400

    {"types":"C","runs":13}

--- pr.java
+++ pr.java
@@ -47,15 +47,60 @@ public void run(){
     }
 
     private void pickBeepersExceptTheMiddle() {
+    	if(beepersPresent()){
+    		while(frontIsClear()){
+    			move();
+    			pickBeeper();
+    			if(frontIsBlocked()){
+    				pickBeeper();
+    				turnAround();
+    				goToTheOtherHalf():
+    			}
+    		}
+    	}
 	
 	
+}
+    /*
+     * karel is facing West.it must go to the 1x1 pile, picking up 
+     * remaining beepers except the middle one.
+     */
+	 private void goToTheOtherHalf() {
+		 while(noBeepersPresent()){
+			 move();
+			 if(beepersPresent()){
+				 move();
+				 pickUpAllBeepers();
+			 }
+			 if(frontIsBlocked()){
+				 goToTheMiddlePoint();
+			 }
+		 }
+		
+		
+	}
+	private void goToTheMiddlePoint() {
+		// TODO Auto-generated method stub
+		turnAround();
+		while(noBeepersPresent()){
+			move();
+		}
+	}
+	private void pickUpAllBeepers() {
+		// TODO Auto-generated method stub
+		while(beepersPresent()){
+		pickBeeper();
+		if(frontIsClear()){
+		move();
+		}
+		
+	}
+
+
 }
 
 
 }
-
-
-
 
 
 

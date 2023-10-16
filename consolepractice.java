8368dd243558842d9d213be8428ec769bb2e5f49
S <s>
2023-10-17 00:09:34 +0400

    {"types":"C","runs":5}

--- consolepractice.java
+++ consolepractice.java
@@ -6,7 +6,7 @@ public class consolepractice extends ConsoleProgram{
 		int n1 = readInt("Enter n1: ");
 		int n2 = readInt("Enter n2: ");
 		int total = n1 + n2;
-		println("The total is" + total +".");
+		println("The total is" + total + ".");
 	}
 
 }

18988e343b3832266e4b82818edfbc69a4b53207
S <s>
2023-10-17 00:01:03 +0400

    {"types":"C","runs":1}

--- consolepractice.java
+++ consolepractice.java
@@ -1,4 +1,12 @@
+import acm.program.ConsoleProgram;
 
-public class consolepractice {
+public class consolepractice extends ConsoleProgram{
+	public void run(){
+		println("this program adds two numbers.");
+		int n1 = readInt("20: ");
+		int n2 = readInt("10:");
+		int total = n1 + n2;
+		println("The total is" + total +".");
+	}
 
 }

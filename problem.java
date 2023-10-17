425d2252947591abecc70a44ce9fab88973f11a5
S <s>
2023-10-17 12:30:05 +0400

    {"types":"C","runs":3}

--- problem.java
+++ problem.java
@@ -26,9 +26,18 @@ public class problem extends GraphicsProgram {
 		double y1 = getHeight()/2 -60 +20;
 		double width = EYES_WIDTH;
 		double height = EYES_HEIGHT;
-		GRect grect= new GRect(x1, y1,width, height);
-		add (grect);
+		GRect eye1= new GRect(x1, y1,width, height);
+		add (eye1);
+		
+		double x2 = getWidth()/2 -40-20;
+		double y2 = getHeight()/2 -60-20;
+		double width1 = EYES_WIDTH;
+		double height1 = EYES_HEIGHT;
+		GRect eye2= new GRect(x2, y2, width1, height1);
+		add (eye2);
+		
 	}
+	
 
 	private void addFace() {
 		// TODO Auto-generated method stub

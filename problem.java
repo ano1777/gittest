1603021ee08207d9cf302d4eb2eb4c588fe6f324
S <s>
2023-10-17 10:26:47 +0400

    {"types":"C","runs":4}

--- problem.java
+++ problem.java
@@ -1,10 +1,25 @@
 import acm.graphics.GLine;
+import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
+
+
 public class problem extends GraphicsProgram {
+	private static final double FACE_WIDTH = 40;
+	private static final int FACE_HEIGHT = 60;
 	public void run (){
 		addLine();
+		addRobotFace();
 	
  }
+
+	private void addRobotFace() {
+		double x1 = getWidth()/2 - 20;
+		double y1 = getHeight()/2 - 30;
+		double width = FACE_WIDTH;
+		double height = FACE_HEIGHT;
+		GRect myGRect = new GRect(x1,y1, width, height);
+		add (myGRect);
+	}
 
 	private void addLine() {
 		double x1 = getWidth()/2;

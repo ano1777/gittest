4dc09eff1a532106ad952be72891b324244781ab
S <s>
2023-10-17 17:15:24 +0400

    {"types":"C","runs":3}

--- concsolepr.java
+++ concsolepr.java
@@ -4,9 +4,10 @@ public class concsolepr extends ConsoleProgram {
 	public void run(){
 	
 	 	int num1 = readInt("Enter num1 =");
-		int num2 = readInt("Enter num2 =")
+		int num2 = readInt("Enter num2 =");
 		int average =(num1+num2)/2;
 		print(average);
+		
 	}
 
 }

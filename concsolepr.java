e58d9078225a18539036246352843776d2a05ae1
S <s>
2023-10-17 23:52:26 +0400

    {"types":"C","runs":13}

--- concsolepr.java
+++ concsolepr.java
@@ -2,13 +2,12 @@ import acm.program.ConsoleProgram;
 
 public class concsolepr extends ConsoleProgram {
 	public void run(){
-	
+		
+		println("this program counts the average of two integers");
 	 	double num1 = readInt("Enter num1");
 	 	double num2 = readInt("Enter num2");
 		double averageInDoubles = (double)(num1+num2)/2;
 		print(averageInDoubles);
-		
-	
 		
 	}
 

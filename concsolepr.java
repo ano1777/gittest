c7a1cf22f3dc1c59fc37141833bafa9fc5e2365b
S <s>
2023-10-17 16:44:07 +0400

    {"types":"C","runs":2}

--- concsolepr.java
+++ concsolepr.java
@@ -6,7 +6,7 @@ public class concsolepr extends ConsoleProgram {
 		int x = readInt("Enter x = ");
 		int y = readInt("Enter y = ");
 		int z = readInt("Enter z =");
-		double harmonic = (double) (x*y*z)/(x*y)+(y*z)+(x*z);
+		double harmonic = (double) (x*y*z)/((x*y)+(y*z)+(x*z));
 		print (harmonic);
 	}
 

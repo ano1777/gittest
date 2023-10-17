3f2bc7b5c4a24c0d7224474eae1e4e177dd20c75
S <s>
2023-10-17 16:51:33 +0400

    {"types":"C","runs":2}

--- concsolepr.java
+++ concsolepr.java
@@ -2,12 +2,8 @@ import acm.program.ConsoleProgram;
 
 public class concsolepr extends ConsoleProgram {
 	public void run(){
-		println("this program counts average harmonic");
-		int x = readInt("Enter x = ");
-		int y = readInt("Enter y = ");
-		int z = readInt("Enter z =");
-		double harmonic = (double) (x*y*z)/((x*y)+(y*z)+(x*z));
-		print (harmonic);
+		
+	 		
 	}
 
 }

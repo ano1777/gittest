dbe3ffdd81b71462bb854b5155ba7e80f4646d41
S <s>
2023-10-17 10:19:39 +0400

    {"types":"C","runs":3}

--- problem.java
+++ problem.java
@@ -1,6 +1,18 @@
-import stanford.karel.*;
-public class problem extends Karel {
+import acm.graphics.GLine;
+import acm.program.GraphicsProgram;
+public class problem extends GraphicsProgram {
+	public void run (){
+		addLine();
 	
  }
+
+	private void addLine() {
+		double x1 = getWidth()/2;
+		double y1 = 0;
+		double x2 = x1;
+		double y2 = getHeight();
+		GLine midLine = new GLine(x1,y1,x2,y2);
+		add (midLine);
+	}
 	 
 }

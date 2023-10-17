513369c39690560acf4ae4ffb04f659c9108ee69
S <s>
2023-10-17 23:45:06 +0400

    {"types":"C","runs":10}

--- concsolepr.java
+++ concsolepr.java
@@ -3,9 +3,9 @@ import acm.program.ConsoleProgram;
 public class concsolepr extends ConsoleProgram {
 	public void run(){
 	
-	 	int num1 = readInt("Enter num1 =");
-		int num2 = readInt("Enter num2 =");
-		int average =(num1+num2)/2;
+	 	double num1 = readInt("Enter num1 =");
+		double num2 = readInt("Enter num2 =");
+		double average =(num1+num2)/2;
 		print(average);
 		
 		

2d7ea7ad7fbf47ac6d021a24472e789c60582375
S <s>
2023-10-17 13:20:00 +0400

    {"types":"C","runs":11}

--- concsolepr.java
+++ concsolepr.java
@@ -1,4 +1,10 @@
+import acm.program.ConsoleProgram;
 
-public class concsolepr {
+public class concsolepr extends ConsoleProgram {
+	public void run(){
+		string num1 = readLine("Enter string:");
+		println(num1);
+		println("")
+	}
 
 }

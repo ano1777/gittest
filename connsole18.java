2ec554ef2eb33511bd62661011099e99b608eff3
S <s>
2023-10-17 23:29:48 +0400

    {"types":"C","runs":3}

--- connsole18.java
+++ connsole18.java
@@ -6,10 +6,10 @@ public class connsole18 extends ConsoleProgram {
 		int num1 = readInt("Enter num1: ");
 		int num2 = readInt("Enter num2: ");
 		if ( num1 > num2){
-		    print (num1);
+		    print (num2);
 		}
 		if (num2 > num1) {
-			print (num2);
+			print (num1);
 		}
 		
 	}

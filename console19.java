2e7ccd058976fc5ea3ff114562b2410a4617d71c
S <s>
2023-10-17 23:40:19 +0400

    {"types":"C","runs":6}

--- console19.java
+++ console19.java
@@ -1,4 +1,11 @@
+import acm.program.ConsoleProgram;
 
-public class console19 {
+public class console19 extends ConsoleProgram {
+	public void run(){
+		int n= readInt("Enter n:");
+		for(int i=0; i<n; i++) {
+			println("HELLO" + i );
+		}
+	}
 
 }

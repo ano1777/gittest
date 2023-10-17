3fb135ca43506a4e2d9938e4b7cbc0af1c3fda09
S <s>
2023-10-17 17:59:39 +0400

    {"types":"C","runs":5}

--- connsole18.java
+++ connsole18.java
@@ -1,4 +1,17 @@
+import acm.program.ConsoleProgram;
 
-public class connsole18 {
+public class connsole18 extends ConsoleProgram {
+	public void run(){
+		
+		int num1 = readInt("Enter num1: ");
+		int num2 = readInt("Enter num2: ");
+		if ( num1 > num2){
+		    print ("num1: ");
+		}
+		if (num2 > num1) {
+			print ("num2");
+		}
+		println(Math.max) (num1, num2);
+	}
 
 }

2233f175d9937666e0abdc6a2706b9f3728a4c15
S <s>
2023-10-17 17:59:39 +0400

    {"types":"C","runs":5}

--- console17.java
+++ console17.java
@@ -1,4 +1,19 @@
+import acm.program.ConsoleProgram;
 
-public class console17 {
+public class console17 extends ConsoleProgram{
+	double a = readDouble("Enter real number");
+	int integerPart;
+	double fractionPart= a-integerPart;
+	
+	println("Integer part:" + "Integer part:");
+	println("Fraction part:" + "Fraction part:");
+	
+	private void calculateBigDecimal( double a) {
+	bigDecimal decimal = new BigDecimal{"" +a);
+	
+	int integer
+	}
+			
+	
 
 }

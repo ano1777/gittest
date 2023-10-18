cfe2c4c42befecf838e1c4ab5a3b2c936fe21bc0
S <s>
2023-10-18 10:30:10 +0400

    {"types":"C","runs":9}

--- console23.java
+++ console23.java
@@ -3,7 +3,7 @@ import acm.program.ConsoleProgram;
 public class console23 extends ConsoleProgram {
 	public void run(){
 		int n = readInt("Enter n:");
-		int addSum = (1+(n+1))*n/2;
+		int addSum = ((1+n)*n)/2;
 		for (int i=1; i<n+1;i++){
 			print(addSum);
 		}

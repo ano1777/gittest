f22f94b12ce4485c6e117f439138dc0455c736bd
S <s>
2023-10-22 13:23:29 +0400

    {"types":"C","runs":11}

--- consoleBoard.java
+++ consoleBoard.java
@@ -1,4 +1,21 @@
+import acm.graphics.GLine;
+import acm.program.GraphicsProgram;
 
-public class consoleBoard {
+public class consoleBoard extends GraphicsProgram{
+	private static final int NUM_COLUMNS = 11;
+	private static final int NUM_ROWS = 11;
+	public void run() {
+		int space = getWidth() / NUM_COLUMNS ;
+		for (int i=0; i < NUM_COLUMNS ; i++){
+			for (int j=0; j < NUM_ROWS ;  j++){
+				int x = i * space;
+				int y = j * space;
+			
+			GLine line = new GLine(x , y, getWidth(), getHeight());
+			add( line);
+		}
+	
+	}
 
+}
 }

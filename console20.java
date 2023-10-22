46b67d0ef6b4af754409a5ce5494de5b94620c95
S <s>
2023-10-22 15:18:38 +0400

    {"types":"C","runs":22}

--- console20.java
+++ console20.java
@@ -3,11 +3,12 @@ import acm.program.ConsoleProgram;
 public class console20 extends ConsoleProgram {
 	public void run() {
 	 int n = readInt("Enter number:");
+	 int sum = 0;
 	 for (int i=0; i < n+1; i++){
-	 int number = readInt("Enter number:" + i);
-	 println (number);
-
-	}
+	     int number = readInt("Enter number:" + i + ":" );
+	     sum = sum + number;
+   }
+	 println( "sum" +sum);
 	}
 
 }

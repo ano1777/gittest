301715c51f68bbf8ee582b4257446098e9431d05
S <s>
2023-10-22 14:28:17 +0400

    {"types":"C","runs":15}

--- circles.java
+++ circles.java
@@ -9,11 +9,7 @@ import acm.program.GraphicsProgram;
 public class circles extends GraphicsProgram{
 	public void run(){
 
-GRect dd = new GRect (100,20,30,60);
-add (dd);
-dd.setFilled(true);
-dd.setColor(Color.MAGNETE);
-		
+
 	}
 
 }

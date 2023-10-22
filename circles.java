ada617c5475040adf7e905e938116ee5dcbe1767
S <s>
2023-10-22 10:44:16 +0400

    {"types":"C","runs":2}

--- circles.java
+++ circles.java
@@ -1,9 +1,18 @@
+import java.awt.Label;
+
+import com.sun.prism.paint.Color;
+
 import acm.graphics.GLabel;
+import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
 
 public class circles extends GraphicsProgram{
 	public void run(){
-		GLabel helloWorld = new GLabel( "HELLO, WORLD" 200, 150);
+
+GRect dd = new GRect (100,20,30,60);
+add (dd);
+dd.setFilled(true);
+dd.setColor(Color.MAGNETE);
 		
 	}
 

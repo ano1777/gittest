c8be642cf914345a68ba81de82daef6605adc93d
S <s>
2023-10-22 10:44:17 +0400

    {"types":"C","runs":2}

--- console17.java
+++ console17.java
@@ -3,12 +3,10 @@ import java.math.BigDecimal;
 import acm.program.ConsoleProgram;
 
 public class console17 extends ConsoleProgram {
-	double a = readDouble("Enter real number");
-	
-	private void calculateBigDecimal(double a) {
-	BigDecimal decimal = new BigDecimal{"" +a);
-	}
-			
-	
+	public void run() {
+	int = 7;
+	double y = x/2;
+
+}	
 
 }

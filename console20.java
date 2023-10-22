bea3a881e580a2d64b81928125408f146c425d3a
S <s>
2023-10-22 14:49:05 +0400

    {"types":"C","runs":18}

--- console20.java
+++ console20.java
@@ -1,4 +1,11 @@
+import acm.program.ConsoleProgram;
 
-public class console20 {
+public class console20 extends ConsoleProgram {
+	public void run() {
+	 int n = readInt("Enter number:");
+	 for (int i=0; i < n+1; i++); 
+	 int number = readInt ("Enter number");
+	}
+	
 
 }

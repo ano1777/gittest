98b5d68a9a053b2e2e4b574bbdfd2fbfade785b1
S <s>
2023-10-22 14:04:34 +0400

    {"types":"C","runs":13}

--- consoleBoard.java
+++ consoleBoard.java
@@ -5,17 +5,27 @@ public class consoleBoard extends GraphicsProgram{
 	private static final int NUM_COLUMNS = 11;
 	private static final int NUM_ROWS = 11;
 	public void run() {
-		int space = getWidth() / NUM_COLUMNS ;
-		for (int i=0; i < NUM_COLUMNS ; i++){
-			for (int j=0; j < NUM_ROWS ;  j++){
-				int x = i * space;
-				int y = j * space;
-			
-			GLine line = new GLine(x , y, getWidth(), getHeight());
-			add( line);
-		}
-	
+		addVerticalLine();
+		addHorizontalLine();
 	}
+	private void addHorizontalLine() {
+		// TODO Auto-generated method stub
+		
+	}
+	private void addVerticalLine() {
+		double width  = (double)getWidth() / NUM_COLUMNS - 1;
+		for (int i=0; i< NUM_COLUMNS; i++);{
+			double x = getWidth() / NUM_COLUMNS; 
+		    double y = 0;
+		    double x1 = x;
+		    double y1 = getHeight();
+			GLine verticalLine = new GLine( x, y, x1, y1);
+			add (verticalLine);
+		}
+		
+		
+	}
+	
+	
 
-}
 }

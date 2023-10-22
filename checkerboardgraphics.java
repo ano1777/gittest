ce114db373b200d76208535d27986e3491b8e534
S <s>
2023-10-22 12:58:46 +0400

    {"types":"C","runs":5}

--- checkerboardgraphics.java
+++ checkerboardgraphics.java
@@ -1,4 +1,20 @@
+import acm.graphics.GRect;
+import acm.program.GraphicsProgram;
 
-public class checkerboardgraphics {
+public class checkerboardgraphics extends GraphicsProgram{
+	private static final int NUM_COLUMNS = 8;
+	private static final int NUM_ROWS = 8;
+	public void run() {
+		int squareSize = getWidth() / NUM_COLUMNS;
+		for (int i = 0; i < NUM_COLUMNS ;i++ ){
+			for (int j=0; j< NUM_ROWS ; j++){
+				int x = i * squareSize;
+				int y = j * squareSize;
+				GRect square = new GRect(x, y,squareSize, squareSize );
+				add(square);
+			}
+		}
+		
+	}
 
 }

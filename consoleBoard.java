f746e76b65a810e54271f548076536459f717380
S <s>
2023-10-22 14:20:36 +0400

    {"types":"C","runs":14}

--- consoleBoard.java
+++ consoleBoard.java
@@ -2,30 +2,36 @@ import acm.graphics.GLine;
 import acm.program.GraphicsProgram;
 
 public class consoleBoard extends GraphicsProgram{
-	private static final int NUM_COLUMNS = 11;
-	private static final int NUM_ROWS = 11;
-	public void run() {
-		addVerticalLine();
-		addHorizontalLine();
+	private static final int NUM_LINES = 11;
+	
+    public void run() {
+	
+	for (int i=0; i< NUM_LINES; i++) {
+        addVerticalLine(i);
+		addHorizontalLine(i);
 	}
-	private void addHorizontalLine() {
-		// TODO Auto-generated method stub
-		
-	}
-	private void addVerticalLine() {
-		double width  = (double)getWidth() / NUM_COLUMNS - 1;
-		for (int i=0; i< NUM_COLUMNS; i++);{
-			double x = getWidth() / NUM_COLUMNS; 
-		    double y = 0;
-		    double x1 = x;
-		    double y1 = getHeight();
-			GLine verticalLine = new GLine( x, y, x1, y1);
-			add (verticalLine);
-		}
-		
-		
+}
+    
+	private void addHorizontalLine(int i) {
+		double height = (double)getHeight()/(NUM_LINES -1);
+		double x = 0;
+		double y = i * height;
+		double x1 = getWidth();
+		GLine horizontalLine = new GLine(x,y,x1,y);
+		add(horizontalLine);
 	}
 	
+	private void addVerticalLine(int i) {
+		double width  = (double)getWidth() /(NUM_LINES - 1);
+			double x = i * width; 
+		    double y = 0;
+		    double y1 = getHeight();
+			GLine verticalLine = new GLine( x, y, x, y1);
+			add (verticalLine);
+		}		
+		
+}
 	
 
-}
+
+

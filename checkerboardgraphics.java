f013da71f3fcf66be3687596d7be774f8a0f6246
S <s>
2023-10-22 13:03:08 +0400

    {"types":"C","runs":10}

--- checkerboardgraphics.java
+++ checkerboardgraphics.java
@@ -13,7 +13,7 @@ public class checkerboardgraphics extends GraphicsProgram{
 				int x = i * squareSize;
 				int y = j * squareSize;
 				GRect square = new GRect(x, y,squareSize, squareSize );
-				square.setFilled(((i+j) % 2) == 1);
+				square.setFilled(((i+j) % 2) == 0);
 				add(square);
 				
 			}

dbc4ab836d98e043426e8e85c83d03e1933f2ac5
S <s>
2023-10-24 20:30:45 +0400

    {"types":"C","runs":4}

--- console25.java
+++ console25.java
@@ -1,13 +1,13 @@
+import acm.graphics.GLabel;
 import acm.program.ConsoleProgram;
 import java.lang.Math;
+
+import com.sun.prism.paint.Color;
+
 public class console25 extends ConsoleProgram{
 	public void run() {
-		double x = 16;
-		double y = Math.sqrt(x);
-		double z = Math.pow(x,y);
-		println(x);
-		println(y);
-		println(z);
+	GLabel lab = new GLabel("ani", 10,10);
+
 	}
 
 }

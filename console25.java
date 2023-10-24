24d3ceaa3af4eba16f808be36acb67f06cf071e1
S <s>
2023-10-24 20:26:58 +0400

    {"types":"C","runs":1}

--- console25.java
+++ console25.java
@@ -1,4 +1,10 @@
-
-public class console25 {
+import acm.program.ConsoleProgram;
+import java.lang.Math;
+public class console25 extends ConsoleProgram{
+	public void run() {
+		double x = 16;
+		double y = Math.sqrt(x);
+		double z = Math.pow(x,y);
+	}
 
 }

ad5dbdcbc4f591e3a492ad9e8853f19743e0c54f
S <s>
2023-10-24 20:26:58 +0400

    {"types":"C","runs":1}

--- console24.java
+++ console24.java
@@ -3,10 +3,13 @@ import acm.program.ConsoleProgram;
 public class console24 extends ConsoleProgram {
 	public void run(){
 		int n = readInt("Enter n:");
-		int 
+		
 	}
 	private int numberOfDivisors (int n){
-		int 
+	    for (int i=0; i <= n; i++){
+	    	
+	    }
+	    
 	}
 
 }

ede65595dfdb7eae7bd3628175cc622f499fb78c
S <s>
2023-10-25 01:20:43 +0400

    {"types":"C","runs":17}

--- console24.java
+++ console24.java
@@ -2,12 +2,23 @@ import acm.program.ConsoleProgram;
 
 public class console24 extends ConsoleProgram {
 	public void run(){
-		int n = readInt("Enter n:");
-		int a = numberOfDivisors(n);
+		int n = readPositiveInt("Enter n:");
+		int Divisors = numberOfDivisors(n);
+	    println ("number of divisors :" + Divisors);
 	}
+	private int readPositiveInt(String string) {
+		int n = readInt(string);
+		while(n <= 0){
+			
+			println( "positive number required");
+			n = readInt(string);
+		}
+		return n;
+	}
+	
 	private int numberOfDivisors (int n){
 		int number = 0;
-	    for (int i=0; i <= n; i++){
+	    for (int i=1; i <= n; i++){
 	    	 if (n % i == 0 ){
 	    		 number += 1;
 	    	 }

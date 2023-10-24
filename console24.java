ebb85cf02988b0f47f20328a83188272aca68d1a
S <s>
2023-10-25 00:12:58 +0400

    {"types":"C","runs":15}

--- console24.java
+++ console24.java
@@ -6,9 +6,13 @@ public class console24 extends ConsoleProgram {
 		
 	}
 	private int numberOfDivisors (int n){
+		int number = 0;
 	    for (int i=0; i <= n; i++){
-	    	
+	    	 if (n % i == 0 ){
+	    		 number += 1;
+	    	 }
 	    }
+	    return number;
 	    
 	}
 

f83c8e2534b9d82f574f0c86592c3b95135bf9ec
S <s>
2023-10-26 19:00:30 +0400

    {"types":"C","runs":1}

--- console27.java
+++ console27.java
@@ -4,7 +4,7 @@ public class console27 extends ConsoleProgram{
 	public void run() {
 		int n = readInt("Enter number:");
 		int m = readInt( "enter number:");
-		println (Math.pow(n,m));
+		println(Math.pow(n,m));
 	}
 
 }

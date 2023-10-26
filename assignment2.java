4445be0e6d4d278936ce6005e89671aa215ab5ea
S <s>
2023-10-27 01:04:10 +0400

    {"types":"C","runs":25}

--- assignment2.java
+++ assignment2.java
@@ -6,12 +6,12 @@ import acm.program.GraphicsProgram;
 
 public class assignment2 extends GraphicsProgram{
 	private static final double RECT_WIDTH = 100;
-	private static final double RECT_HEIGHT =  30;
+	private static final double RECT_HEIGHT =  50;
 	public void run() {
 		GLabel label = new GLabel ("program",getWidth()/2, getHeight() / 2 -30);
 		add (label);
 		
-		GRect myRect = new GRect(getWidth() /2 - RECT_WIDTH, getHeight() / 2 - RECT_HEIGHT, RECT_WIDTH,RECT_HEIGHT);
+		GRect myRect = new GRect(getWidth() /2 - label.getWidth()/2, getHeight() / 2 - label.getHeight()/2, RECT_WIDTH,RECT_HEIGHT);
 		add (myRect);
 		
 	

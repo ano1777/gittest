9f0eb18c460106785e7b7f6bc6349edb65778599
S <s>
2023-10-26 17:35:03 +0400

    {"types":"C","runs":1}

--- console25.java
+++ console25.java
@@ -1,28 +1,20 @@
-import acm.graphics.GLabel;
+
 import acm.program.ConsoleProgram;
 import java.lang.Math;
 
-import com.sun.prism.paint.Color;
-
  
 public class console25 extends ConsoleProgram{
-	 private static final int NUMBER = 5;
-	 
-	 public void run() {
-		
-		 for(int i=0; i <= NUMBER; i++){
-			 println(i + "!=" + factorial(i));
-		 }
-
+	public void run() {
+		int n = readInt(" Enter n:");
+		int m = readInt("Enter m:");
+		for (int i = 1; i <= Math.min (n, m); i++){
+			if ( n % i == 0 && m % i ==0){
+				println(i);
+				break;
+			}
+		}
 	}
-
-	private int factorial(int n) {
-		int result = 1;
-		for (int i=1; i<=n; i++){
-	        result = i*result;
-		} 
-		return result;
-	}
+	
 
 }
 	

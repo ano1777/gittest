573f55aca842b54b34a6994c7308e7a124278b0c
S <s>
2023-10-26 19:35:02 +0400

    {"types":"C","runs":7}

--- console25.java
+++ console25.java
@@ -7,7 +7,7 @@ public class console25 extends ConsoleProgram{
 	public void run() {
 		int n = readInt(" Enter n:");
 		int m = readInt("Enter m:");
-		for (int i = 1; i <= Math.min (n, m); i++){
+		for (int i = Math.min(n, m); i >=1; i--){
 			if ( n % i == 0 && m % i ==0){
 				println(i);
 			}

900684e02420346f2fcc5566246a04774430b72f
S <s>
2023-10-26 17:46:28 +0400

    {"types":"C","runs":2}

--- console28.java
+++ console28.java
@@ -1,4 +1,26 @@
+import acm.program.ConsoleProgram;
 
-public class console28 {
+public class console28 extends ConsoleProgram {
+	public void run() {
+		int n = readInt(" Enter num: ");
+		for (int i=0; i>=0; i++){
+			println(n);
+			if (n == -1){
+				break;
+			}
+		}
+		int EvenNumbers = numberOfEvenNumbers();
+		
+	}
+
+	private int numberOfEvenNumbers(int n) {
+	    int total = 0;  
+		if(n % 2 == 0){
+			total += 1;
+		}
+		
+
+		return total;
+	}
 
 }

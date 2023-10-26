1fbb572ce2cf7277eb64024243f8dfaf387dbcf2
S <s>
2023-10-26 19:41:45 +0400

    {"types":"C","runs":9}

--- console25.java
+++ console25.java
@@ -5,14 +5,23 @@ import java.lang.Math;
  
 public class console25 extends ConsoleProgram{
 	public void run() {
-		int n = readInt(" Enter n:");
-		int m = readInt("Enter m:");
+		int n = readPositiveInt(" Enter n:");
+		int m = readPositiveInt("Enter m:");
 		for (int i = Math.min(n, m); i >=1; i--){
 			if ( n % i == 0 && m % i ==0){
 				println(i);
 				break;
 			}
 		}
+	}
+
+	private int readPositiveInt(String text) {
+		int n = readInt(text);
+		while (n < 0){
+			println ("Enter positive number");
+			n = readInt(text);
+		}
+		return 0;
 	}
 	
 

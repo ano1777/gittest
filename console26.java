e5820fb9c142ba851dee4718623220d7dd1ffbc1
S <s>
2023-10-26 17:35:03 +0400

    {"types":"C","runs":1}

--- console26.java
+++ console26.java
@@ -1,4 +1,22 @@
+import acm.program.ConsoleProgram;
 
-public class console26 {
+public class console26  extends ConsoleProgram{
+	public void run() {
+		int num1 = readInt ("Enter numner 1:");
+		int num2 = readInt("Enter number 2: ");
+		for (int i = Math.max(num1, num2); i <= num1 * num2; i++ ){
+			if(i % num1 == 0 & i % num2 == 0){
+				print("this is the answer: " + i);
+				break;
+			}
+		}
+		
+	}
 
-}
+		
+		
+	}
+	
+	
+
+

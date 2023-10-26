ef95b200afa42a8ab9eb2b5be9fd20e5373d2622
S <s>
2023-10-26 22:42:25 +0400

    {"types":"C","runs":20}

--- assignment2.java
+++ assignment2.java
@@ -6,10 +6,10 @@ import acm.program.GraphicsProgram;
 
 public class assignment2 extends GraphicsProgram{
 	public void run() {
-		GLabel label = new GLabel("Hello, world1", 30,30);
+		GLabel label = new GLabel("Hello, world!", 30,30);
 		add (label);
 	
-		GRect frame = new GRect (20,20, label.getWidth(),label.getAscent());
+		GRect frame = new GRect (20,20, label.getWidth()+10, label.getAscent()+10);
 		add (frame);
 		frame.setFilled(false);
 		frame.setColor(Color.RED);

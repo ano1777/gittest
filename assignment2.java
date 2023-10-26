a2cab67ef1583c8b59249445fafebd733fc1c768
S <s>
2023-10-26 21:35:09 +0400

    {"types":"C","runs":13}

--- assignment2.java
+++ assignment2.java
@@ -1,4 +1,9 @@
+import acm.graphics.GLabel;
+import acm.program.GraphicsProgram;
 
-public class assignment2 {
+public class assignment2 extends GraphicsProgram{
+	public void run() {
+		add (new GLabel("hello, world!"), 30,30);
+	}
 
 }

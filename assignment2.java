ff7f427b0cf03e98bfed0ec0436670b3026959dc
S <s>
2023-10-26 22:43:44 +0400

    {"types":"C","runs":22}

--- assignment2.java
+++ assignment2.java
@@ -8,6 +8,7 @@ public class assignment2 extends GraphicsProgram{
 	public void run() {
 		GLabel label = new GLabel("Hello, world!", 30,30);
 		add (label);
+		label.setColor(Color.BLUE);
 	
 		GRect frame = new GRect (25, 15, label.getWidth()+10, label.getAscent()+10);
 		add (frame);

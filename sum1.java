74755087852c7efbc804b54b4ef6a10ad2d920f8
S <s>
2023-10-28 12:34:28 +0400

    {"types":"C","runs":8}

--- sum1.java
+++ sum1.java
@@ -4,7 +4,7 @@ public class sum1 extends ConsoleProgram {
 	public void run() {
 		double n = readInt("ENTER N :");
 		double m = readInt("ENTER M) :");
-		double total = n * m;
+		double total = n % m;
 		println("The sum is :" +total+ ".");
 	}
 

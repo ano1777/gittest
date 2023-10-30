fec17d5d9a958d1a81a580d9d4d6fe2cacf5f1da
S <s>
2023-10-30 22:41:08 +0400

    {"types":"C","runs":36}

--- ConsoleSample.java
+++ ConsoleSample.java
@@ -4,7 +4,7 @@ public class ConsoleSample extends ConsoleProgram {
 	private static final double INCH_IN_CM = 2.54;
 	public void run() {
 		double numberInCm = readInt("Enter n :");
-		double inch = numberInCm * INCH_IN_CM;
+		double inch = numberInCm / INCH_IN_CM;
 		println(inch);
 	
 	}

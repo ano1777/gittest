c4d57389f958425f73190592d1afe2695238fe82
S <s>
2023-10-30 22:31:41 +0400

    {"types":"C","runs":32}

--- ConsoleSample.java
+++ ConsoleSample.java
@@ -4,7 +4,7 @@ public class ConsoleSample extends ConsoleProgram {
 	public void run() {
 	int n = readInt("n :");
 	int m = readInt ("m :");
-	int average = ((n+m)/2);
+	double average = ((n+m)/2);
 	println (average);
 }
 }

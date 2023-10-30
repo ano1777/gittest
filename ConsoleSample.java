97e9b129fe86e1c5b56710700d497eef8c13235f
S <s>
2023-10-30 22:52:51 +0400

    {"types":"C","runs":42}

--- ConsoleSample.java
+++ ConsoleSample.java
@@ -1,12 +1,14 @@
 import acm.program.ConsoleProgram;
 
 public class ConsoleSample extends ConsoleProgram {
-	private static final double INCH_IN_CM = 2.54;
+	
 	public void run() {
 		int n = readInt("starting balance : ");
 		double m = readDouble("annual percentage :");
 		double finalBalance = (n + n*m/100);
+		double final2 = (finalBalance + finalBalance*m/100);
 		println (finalBalance);
+		println(final2);
 	
 	}
 }

04017314d6715cb16f14f3e33dc20f04bc8e0f41
S <s>
2023-10-30 23:09:10 +0400

    {"types":"C","runs":45}

--- ConsoleSample.java
+++ ConsoleSample.java
@@ -4,9 +4,8 @@ public class ConsoleSample extends ConsoleProgram {
 	private static final double PI = 3.14;
 
 	public void run() {
-		double r = readInt(" Enter radius : ");
-		double calculate = PI * Math.pow(r, 2);
-		println(calculate);
-
+		double f = readInt(" Enter degree in farenheit : ");
+		double c = 5*(f-32)/9;
+		println (c);
 	}
 }

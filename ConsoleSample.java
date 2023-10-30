a53ff6a9d9dd62eb0788a28792ea4bac1265f7f8
S <s>
2023-10-30 22:30:26 +0400

    {"types":"C","runs":27}

--- ConsoleSample.java
+++ ConsoleSample.java
@@ -1,4 +1,10 @@
+import acm.program.ConsoleProgram;
 
-public class ConsoleSample {
-
+public class ConsoleSample extends ConsoleProgram {
+	public void run() {
+	int n = readInt("n :");
+	int m = readInt ("m:");
+	int average = (n+m)/2;
+	println (average);
+}
 }

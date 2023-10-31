7f60c2c2b03d5942f03bfdc10c44fdd925c29945
S <s>
2023-10-31 08:11:45 +0400

    {"types":"C","runs":49}

--- console28.java
+++ console28.java
@@ -4,20 +4,20 @@ public class console28 extends ConsoleProgram {
 	public void run() {
 		int n = readInt(" Enter num: ");
 		int EvenNumbers = 0;
-		while(n != -1){
+		while (n != -1) {
 			n = readInt(" Enter num: ");
 			EvenNumbers = EvenNumbers + numberOfEvenNumbers(n);
 		}
 		println(EvenNumbers);
-		
+
 	}
 
 	private int numberOfEvenNumbers(int n) {
-	    int total = 0;  
-		if(n % 2 == 0){
+		int total = 0;
+		if (n % 2 == 0) {
 			total += 1;
 		}
-		
+
 		return total;
 	}
 

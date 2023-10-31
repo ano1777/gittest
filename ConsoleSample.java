8df6ef17096647eebed40f973ca36911f0e6d325
S <s>
2023-10-31 12:40:42 +0400

    {"types":"C","runs":74}

--- ConsoleSample.java
+++ ConsoleSample.java
@@ -5,11 +5,13 @@ public class ConsoleSample extends ConsoleProgram {
 	public void run() {
 		int n = readInt ("enter number");
 		int m = 0;
+		int a = 0;
 		while (n != -1){
 			n = readInt("enter number");
 		
 		}
 		m = m +a(n);
+		println ("number of evens are :" + n);
 	}
 
 	private int a(int n) {
@@ -17,6 +19,6 @@ public class ConsoleSample extends ConsoleProgram {
 		while (n % 2 == 0){
 			total += 1;
 		}
-		return m ;
+		return total ;
 	}
 }

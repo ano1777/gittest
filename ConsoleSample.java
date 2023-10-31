ee465308b681bbcaaafd23264b11e88c680f07f8
S <s>
2023-10-31 07:45:53 +0400

    {"types":"C","runs":48}

--- ConsoleSample.java
+++ ConsoleSample.java
@@ -1,11 +1,27 @@
 import acm.program.ConsoleProgram;
 
 public class ConsoleSample extends ConsoleProgram {
-	private static final double PI = 3.14;
+	
 
 	public void run() {
-		double f = readInt(" Enter degree in farenheit : ");
-		double c = 5/9*(f-32);
-		println (c);
+		int n =  readInt("Enter n :");
+		while( n != 1){
+			n = readInt("Enter n :");
+			if (n == 1){
+				addNumberOfEvenNumbers();
+				
+			}
+			
+		}
+		
+	
+	}
+
+	private void addNumberOfEvenNumbers(n) {
+		int total = 0;
+		if ( n % 2 == 0){
+			total += 1;
+		}
+		return total;
 	}
 }

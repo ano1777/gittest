de2c23bbb9fc1d83d70f6d7cc14d9aa5b8eb8f25
S <s>
2023-10-31 17:46:46 +0400

    {"types":"C","runs":84}

--- ConsoleProgram30.java
+++ ConsoleProgram30.java
@@ -1,4 +1,22 @@
+import acm.program.ConsoleProgram;
 
-public class ConsoleProgram30 {
+public class ConsoleProgram30 extends ConsoleProgram{
+	public void run() {
+		int n = readInt("Enter n");
+		
+		int reversedN = reverse(n);
+		println(reversedN);
+		
+	}
+
+	private int reverse(int n) {
+		while( n != 0){
+			int lastDigit = n %10;
+			n /= 10;
+			print(lastDigit);
+		}
+			
+		return 0;
+	}
 
 }

9c8dfa6b1e9ee044d54a77851588ee1f7cf6b86d
S <s>
2023-10-31 18:02:07 +0400

    {"types":"C","runs":88}

--- console30.java
+++ console30.java
@@ -1,5 +1,20 @@
 import acm.program.ConsoleProgram;
 
 public class console30 extends ConsoleProgram{
+	final static final int MAX_NUM= 1000000;
+	public void run() {
+		for (int i = 1; i< MAX_NUM; i++){
+			if (isPrime(i)){
+				println(i);
+				}
+			}
+	}
+	private boolean isPrime(int num) {
+		for (int i = 2; i<=Math.sqrt(num); i ++){
+			
+
+		}
+		return false;
+	}
 
 }

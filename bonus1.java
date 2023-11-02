5ac5c91b99c48841249a8869d990f7db379c73df
S <s>
2023-11-02 12:15:32 +0400

    {"types":"C","runs":2}

--- bonus1.java
+++ bonus1.java
@@ -1,24 +1,30 @@
-import stanford.karel.SuperKarel;
+import acm.program.ConsoleProgram;
 
-public class bonus1 extends SuperKarel{
+public class bonus1 extends ConsoleProgram{
 	public void run(){
-		move();
-		backupBeeperson1Pile();
-		goBack();
-		backupBeepersOn2Pile();
-		goBack();
+		while(true){
+		int n = readInt (" Enter n : ");
+		 int fibonacci = fin(n);
+		 println (fibonacci);
 		
+		}
+	}
+
+	private int fin(int n) {
+		while (n == 1 || n == 0){
+			println (n);
 			
 		}
-
-	private void backupBeeperson1Pile() {
-		while(beepersPresent()){
-			pickBeeper();
-			turnLeft();
-			move();
-			putBeeper();
+		int prev1 = 0;
+		int prev2 = 1;
 		
-	}
+	for( int i = 2; i<=n; i++){				
+		int current = prev1 + prev2;
+		prev1 = current;
+		prev2 = current + prev2;
+		}
+		
+		return prev1;
 	}
 }
 

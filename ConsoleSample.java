30b49a5e133eb7b3b40731c7703a84520e867d49
S <s>
2023-11-02 12:15:32 +0400

    {"types":"C","runs":2}

--- ConsoleSample.java
+++ ConsoleSample.java
@@ -5,6 +5,7 @@ public class ConsoleSample extends ConsoleProgram {
 
 	public void run() {
 		while(true){
+			
 		int n = readInt("enter number : ");
 		
 		int fibN = fib(n);
@@ -18,15 +19,15 @@ public class ConsoleSample extends ConsoleProgram {
 	// indices: 0 1 2 3 4 5 6
 	// values:  0 1 1 2 3 5 8
 	private int fib(int n) {
-		if (n == 0 || n == 1){
+		if (n == 0 || n == 1) { 
 			return n;
 		}
 		int prev2 = 0;
 		int prev1 = 1;
 		
 		//loop
-		for (int i = 2; i<=n; i++){
-			int current = prev2 +prev1;
+	for (int i = 2; i<=n; i++){
+			int current = prev2 + prev1;
 			prev2 = prev1;
 			prev1 = current;
 			

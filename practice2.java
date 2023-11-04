03d9630d19a13ed1f086b21673c330c79785ba64
S <s>
2023-11-04 08:56:58 +0400

    {"types":"C","runs":4}

--- practice2.java
+++ practice2.java
@@ -1,4 +1,17 @@
+import acm.graphics.GRect;
+import acm.program.GraphicsProgram;
 
-public class practice2 {
+public class practice2 extends GraphicsProgram {
+	public void run() {
+		for( int j=1; j<=14; j++){
+		for (int i=1; i<=14; i++) {
+			double x = getWidth()/2 - (i/2)*30;
+			double y = getHeight() - j * 12;
+			GRect myRect = new GRect(x, y, 30, 12);
+			add(myRect);
+		
+		}
+	}
 
+}
 }

1871ea84d818df3d7de785300f82305027b8a119
S <s>
2023-11-04 16:33:24 +0400

    {"types":"C","runs":119}

--- practice2.java
+++ practice2.java
@@ -5,7 +5,7 @@ public class practice2 extends GraphicsProgram {
 
 	public void run() {
 		int numBricks = 14;
-		for( int j=1; j<=numBricks; j++){
+		for( int j=numBricks; j>=1; j++){
 		for (int i=1; i<=14; i++) {
 			double x = getWidth()/2 - (numBricks/2)*30 + numBricks*30;
 			double y = getHeight() - j * 12;

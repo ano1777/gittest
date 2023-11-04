0598a0e71c2e50cfd88894600c54d9e4cc32de94
S <s>
2023-11-04 09:01:23 +0400

    {"types":"C","runs":5}

--- practice2.java
+++ practice2.java
@@ -5,7 +5,7 @@ public class practice2 extends GraphicsProgram {
 	public void run() {
 		for( int j=1; j<=14; j++){
 		for (int i=1; i<=14; i++) {
-			double x = getWidth()/2 - (i/2)*30;
+			double x = getWidth()/2 - (i/2)*30 + i/2;
 			double y = getHeight() - j * 12;
 			GRect myRect = new GRect(x, y, 30, 12);
 			add(myRect);

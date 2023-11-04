d6379f4e21d837273a0100c012e1606d17354ae2
S <s>
2023-11-04 16:17:29 +0400

    {"types":"C","runs":107}

--- Assignment2Pyramid.java
+++ Assignment2Pyramid.java
@@ -13,7 +13,7 @@ public class Assignment2Pyramid extends GraphicsProgram {
 
 	private void addBase() {
 		
-	for (int i = 0; i < bricksInRow; i++) {
+	for (int i = 0; i < 14; i++) {
 		for (int j = 1; j<=BRICKS_IN_BASE; j++) {   	
 				int x1 = getWidth()/2 - (BRICK_WIDTH * bricksInRow)/2 + (bricksInRow * BRICK_WIDTH) ;
 				int y1 = getHeight() - (BRICK_HEIGHT*j);

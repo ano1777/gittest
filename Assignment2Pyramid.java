016699149390cd333ba0d907b377785d946a93d2
S <s>
2023-11-04 09:30:57 +0400

    {"types":"C","runs":14}

--- Assignment2Pyramid.java
+++ Assignment2Pyramid.java
@@ -22,14 +22,9 @@ public class Assignment2Pyramid extends GraphicsProgram {
 	
 		for (int j = 1; j <=BRICKS_IN_BASE; j++) {
 		
-			double x1 = getWidth()/2 - (BRICKS_IN_BASE/2) + j*(BRICK_WIDTH/2);
-			double y1 = getHeight() - (j+1 )* BRICK_HEIGHT;
-			GRect newRow = new GRect(x1, y1, BRICK_WIDTH, BRICK_HEIGHT);
-			add(newRow);
-
 			for (int i = 1; i <= BRICKS_IN_BASE; i++) {
 				double x = getWidth() / 2 - (BRICKS_IN_BASE / 2) * BRICK_WIDTH + i * BRICK_WIDTH;
-				double y = getHeight() - BRICK_HEIGHT;
+				double y = getHeight() - j* BRICK_HEIGHT;
 				GRect brick = new GRect(x, y, BRICK_WIDTH, BRICK_HEIGHT);
 				add(brick);
 				

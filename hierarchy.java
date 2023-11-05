46119029079f9e431d3883d813c1f1bb90d71862
S <s>
2023-11-05 16:24:22 +0400

    {"types":"C","runs":114}

--- hierarchy.java
+++ hierarchy.java
@@ -5,9 +5,9 @@ import acm.program.GraphicsProgram;
 
 public class hierarchy extends GraphicsProgram {
 	private static final double RECT_WIDTH = 150;
-	private static final double RECT_HEIGHT = 60;
+	private static final double RECT_HEIGHT = 50;
 	private static final double SPACE_HORIZONTAL = 60;
-	private static final double SPACE_VERTICAL = 120;
+	private static final double SPACE_VERTICAL = 100;
 
 	/* this class adds hierarchy system of programs on canvas */
 	public void run() {

743ce0ae05532e5a59a85258114c2614c2071e99
S <s>
2023-11-05 05:57:18 +0400

    {"types":"C","runs":248}

--- practice2.java
+++ practice2.java
@@ -3,20 +3,29 @@ import acm.program.GraphicsProgram;
 
 public class practice2 extends GraphicsProgram {
 
-	public void run() {
-		int numBricks = 14;
-		for( int j=numBricks; j>=1; j++){
-		for (int i=1; i<=14; i++) {
-			double x = getWidth()/2 - (numBricks/2)*30 + numBricks*30;
-			double y = getHeight() - j * 12;
-			GRect myRect = new GRect(x, y, 30, 12);
-			add(myRect);
-			
-		
+/** Width of each brick in pixels */
+	private static final int BRICK_WIDTH = 30;
+
+/** Width of each brick in pixels */
+	private static final int BRICK_HEIGHT = 12;
+
+/** Number of bricks in the base of the pyramid */
+	private static final int BRICKS_IN_BASE = 14;
 	
-	
-		numBricks--;
-		}
-		}
+	  public void run() {
+		  addPyramid();
+		  
 }
+	private void addPyramid() {
+		int numBricks = 14;
+	for(int i =0; i<BRICKS_IN_BASE; i++) {
+		for(int j=1; j<=1;j++) {
+			double x = getWidth()/2 -(i+1)* BRICK_WIDTH+numBricks*BRICK_WIDTH;
+			double y = getHeight() -j*BRICK_HEIGHT;
+			GRect newRect = new GRect (x, y, BRICK_WIDTH, BRICK_HEIGHT);
+			add(newRect);
+			
+		}
+	}
+	}
 }

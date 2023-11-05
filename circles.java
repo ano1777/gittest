7c2a67c17af9ba9a69ba6418b92b3461b7ac0cbd
S <s>
2023-11-05 08:14:10 +0400

    {"types":"C","runs":294}

--- circles.java
+++ circles.java
@@ -1,40 +1,42 @@
 import acm.program.ConsoleProgram;
 
-public class circles extends ConsoleProgram{
-	private static final int NUM =0;
-	public void run(){
-		
+public class circles extends ConsoleProgram {
+	private static final int NUM = 0;
+
+	public void run() {
+
 		int n = readInt(" ? ");
 		int max = n;
 		int min = n;
-		 
-		
-		if ( n == NUM ) {
+
+		if (n == NUM) {
 			println(" this number is not allowed. try again.");
 			n = readInt(" ? ");
 		}
-		
+
 		while (n != NUM) {
 			n = readInt(" ? ");
-			if( max < n) {
+			if (max < n) {
 				max = n;
-			} 
-		
-			if (min > n){
+			
+
+			if (min > n) {
 				min = n;
 			}
-		}	
-		if ( n == NUM) {
-			addMaxAndMin(n, max, min);
-		
+			}
+
+			if (n == NUM) {
+				addMaxAndMin(n, max, min);
+
+			}
 		}
 
 	}
-	
+
 	private void addMaxAndMin(int n, int max, int min) {
 		println("smallest : " + min);
 		println("largest : " + max);
-		
+
 	}
 
 }

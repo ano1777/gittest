04aa04ed7cfa2ff66e74e250aebd4da4c95a2ce8
S <s>
2023-11-05 15:22:42 +0400

    {"types":"C","runs":80}

--- hierarchy.java
+++ hierarchy.java
@@ -7,7 +7,7 @@ public class hierarchy extends GraphicsProgram {
 	private static final double SPACE_BETW_RECTS = 60;
 	public void run() {
 		for (int i =0; i<3; i++) {
-			double x = getWidth() / 2 - (1.5 * RECT_WIDTH) - SPACE_BETW_RECTS + (i*RECT_WIDTH+SPACE_BETW_RECTS);
+			double x = getWidth() / 2 - (1.5 * RECT_WIDTH) - SPACE_BETW_RECTS + (i*(RECT_WIDTH+SPACE_BETW_RECTS));
 			double y = getHeight() / 3 + SPACE_BETW_RECTS * 2;
 			GRect rect = new GRect (x, y, RECT_WIDTH, RECT_HEIGHT);
 			add(rect);

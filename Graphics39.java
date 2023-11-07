fcb9a0b7a743119221a3ead6d3cd044b5f01f509
S <s>
2023-11-07 17:50:47 +0400

    {"types":"C","runs":11}

--- Graphics39.java
+++ Graphics39.java
@@ -1,19 +1,31 @@
 import acm.graphics.GOval;
 import acm.program.GraphicsProgram;
 
-public class Graphics39 extends GraphicsProgram{
+public class Graphics39 extends GraphicsProgram {
 	private static final int PAUSE_TIME = 20;
 	private static final int INITIAL_VELOCITY = 5;
-	public void run(){
-	GOval newOval = new GOval(0, getHeight()/2 - 15 , 50, 50);
-	newOval.setFilled(true);
-	add(newOval);
-	int initialX = getWidth() - 50;
 
-	while(newOval.getX() <= initialX){
-	newOval.move(5,0);
-	pause(PAUSE_TIME);
+	public void run() {
+
+		GOval newOval = new GOval(0, getHeight() / 2 - 15, 50, 50);
+		newOval.setFilled(true);
+		add(newOval);
+		int finalX = getWidth() - 50;
+		int initialX = 50;
+
+		while (newOval.getX() <= finalX) {
+			newOval.move(INITIAL_VELOCITY, 0);
+			pause(PAUSE_TIME);
+			if(newOval.getX() == finalX){
+				while(newOval.getX() >= initialX) {
+				newOval.move(INITIAL_VELOCITY, 0);
+				pause(PAUSE_TIME);
+			}
+		}
+
 	}
+
 	
-}
+	}
+
 }

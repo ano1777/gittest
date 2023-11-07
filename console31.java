18710f8bd5f38bdcd72d334af06ff0a28d83c8db
S <s>
2023-11-07 11:21:52 +0400

    {"types":"C","runs":34}

--- console31.java
+++ console31.java
@@ -1,7 +1,25 @@
+import acm.program.ConsoleProgram;
 
+public class console31 extends ConsoleProgram{
+	public void run() {
+		int n = readInt("Enter number"); 
+		int reverseNum = reverse(n);
+		println(reverseNum);
+			
+		}
 
-public class console31 extends GraphicsProgram{
-}
+	private int reverse(int n) {
+		int reverseNum = 0;
+		int lastNum = n % 10;
+		n = n / 10 ;
+		reverseNum = reverseNum * 10 + lastNum;
+		print(reverseNum);
+		
+		
+		return reverseNum;
+	}
+	}
+
 	
 	
 	

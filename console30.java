8f5489ef3a1179e446c82bbe1859684c51eb0da6
S <s>
2023-11-07 08:08:53 +0400

    {"types":"C","runs":8}

--- console30.java
+++ console30.java
@@ -1,48 +1,36 @@
 import acm.program.ConsoleProgram;
 
 public class console30 extends ConsoleProgram{
-	//final static final int MAX_NUM = 1000000;
+	final static final int MAX_NUM = 1000000;
 	public void run() {
-		int n = readInt("ENTER NUMBER : ");
-		if (isPrime(n)) {
-			println("this number is prime");
+	
+ for (int i = 1; i< MAX_NUM; i++){
+			if (isPrime(i)){
+				println(i);
+				}
 		}
 	}
-	
-		
-		
-		
-		private boolean isPrime(int n) {
-			for (int i = 0; i<=Math.sqrt(n); i++) {
-				}			
-			return true;			
-		}		
+	private boolean isPrime(int num) {
+		for (int i = 2; i<=Math.sqrt(num); i ++){
+			
+			
 }
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		//	for (int i = 1; i< MAX_NUM; i++){
-	//		if (isPrime(i)){
-	//			println(i);
-	//			}
-		//	}
-	//}
-//	private boolean isPrime(int num) {
-	//	for (int i = 2; i<=Math.sqrt(num); i ++){
-			
-			
+return false;//
+	}
+}
+	
 
-		//}
-		//return false;//
-	//}
+	//	int n = readInt("ENTER NUMBER : ");
+	//if (isPrime(n)) {
+//		println("this number is prime");
+//	}
+//}
+
+	
+	
+	
+	//private boolean isPrime(int n) {
+	//	for (int i = 0; i<=Math.sqrt(n); i++) {
+		//	}			
+	//	return true;			
+	//}		}

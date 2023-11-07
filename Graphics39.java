c85726a01c4088042010cad4f6c5baa8f895cff0
S <s>
2023-11-07 17:59:53 +0400

    {"types":"C","runs":19}

--- Graphics39.java
+++ Graphics39.java
@@ -10,7 +10,7 @@ public class Graphics39 extends GraphicsProgram {
 		GOval newOval = new GOval(0, getHeight() / 2 - 25, 50, 50);
 		newOval.setFilled(true);
 		add(newOval);
-		int finalX = getWidth();
+		int finalX = getWidth()-25;
 		int initialX = 50;
 
 		while (newOval.getX() <= finalX) {

2f5aeca8f65a0ea6887a7b89d2e1a16ab2557353
S <s>
2023-11-07 07:54:23 +0400

    {"types":"C","runs":2}

--- console30.java
+++ console30.java
@@ -14,7 +14,7 @@ public class console30 extends ConsoleProgram{
 		
 		private boolean isPrime(int n) {
 			for (int i = 0; i<=Math.sqrt(n); i++) {
-				if(n % i == 0);
+
 			}
 			return false;
 			

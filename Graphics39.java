3f14af24521809b87c01830ec19857be2c06d5db
S <s>
2023-11-07 09:18:35 +0400

    {"types":"C","runs":31}

--- Graphics39.java
+++ Graphics39.java
@@ -6,8 +6,11 @@ public class Graphics39 extends GraphicsProgram{
 	GOval newOval = new GOval(0, getHeight()/2-15 , 30, 30);
 	newOval.setFilled(true);
 	add(newOval);
+	double x = 0; 
+
+	while(x < getWidth()-30){
 	newOval.move(10,0);
 	pause(10);
-
+	}
 }
 }

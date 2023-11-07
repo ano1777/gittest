18420b637f68a8a1790a8b9869c1a8a83613d768
S <s>
2023-11-07 08:59:54 +0400

    {"types":"C","runs":18}

--- Graphics33.java
+++ Graphics33.java
@@ -1,4 +1,25 @@
+import java.awt.Color;
+import acm.graphics.GRect;
+import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
 
-public class Graphics33 {
+public class Graphics33 extends GraphicsProgram{
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+
+	private static final int MIN_RAD = 10;
+	private static final int MAX_RAD = 100;
+	public void run() {
+		
+		double x = rgen.nextDouble(getWidth()/2 - 100, getWidth()/2 + 200);
+		double y = rgen. nextDouble(getHeight()/2-100, getHeight()/2 +200);
+		double radius = rgen.nextDouble(MIN_RAD, MAX_RAD);
+		Color color = rgen. nextColor();
+
+		GRect oneRect = new GRect(x, y, 2* radius, 2*radius);
+		oneRect.setFilled(true);
+		oneRect.setColor(color);
+		add(oneRect);
+		
+	}
 
 }

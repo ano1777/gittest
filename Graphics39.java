774771b2d16f60ae3fe6431b9482d1a8ea137f28
S <s>
2023-11-07 09:10:56 +0400

    {"types":"C","runs":30}

--- Graphics39.java
+++ Graphics39.java
@@ -3,11 +3,11 @@ import acm.program.GraphicsProgram;
 
 public class Graphics39 extends GraphicsProgram{
 	public void run(){
-	GOval newOval = new GOval(0, getHeight()/2, 30, 30);
+	GOval newOval = new GOval(0, getHeight()/2-15 , 30, 30);
 	newOval.setFilled(true);
 	add(newOval);
 	newOval.move(10,0);
-	pause(1);
+	pause(10);
 
 }
 }

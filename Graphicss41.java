5ecd7371f9d3acfa4151efa0b94d97c9dd2eb6c6
S <s>
2023-11-09 18:01:03 +0400

    {"types":"C","runs":7}

--- Graphicss41.java
+++ Graphicss41.java
@@ -1,4 +1,9 @@
+import acm.program.GraphicsProgram;
 
-public class Graphicss41 {
-
+public class Graphicss41 extends GraphicsProgram{
+	public void run() {
+		getMouseMotionListeners();
+	}
+	
+     private void mouseMoved(MouseEvent e)
 }

bde3dd8f0c3c750fe73ef3248f51bcf08be1d7cc
S <s>
2023-11-09 17:45:18 +0400

    {"types":"C","runs":7}

--- bonus13.java
+++ bonus13.java
@@ -1,4 +1,15 @@
+import acm.graphics.GLine;
+import acm.program.GraphicsProgram;
 
-public class bonus13 {
+public class bonus13 extends GraphicsProgram{
+	public void run() {
+		while(true) {
+			removeAll();
+			GLine line = new GLine(0, getHeight()/2, getWidth(), getHeight()/2);
+			add(line);
+			pause(20);
+			
+		}
+	}
 
 }

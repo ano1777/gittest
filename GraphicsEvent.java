ceed3a5752d3a1516a321c39b9a4f14766d617b7
S <s>
2023-11-09 17:45:20 +0400

    {"types":"C","runs":7}

--- GraphicsEvent.java
+++ GraphicsEvent.java
@@ -1,4 +1,45 @@
+import java.awt.event.MouseEvent;
 
-public class GraphicsEvent {
+import com.sun.prism.paint.Color;
 
-}
+import acm.graphics.GObject;
+import acm.graphics.GRect;
+import acm.program.GraphicsProgram;
+
+public class GraphicsEvent  extends GraphicsProgram {
+	private GRect newRect;
+	private GRect rect2;
+	label = new GLabel("")
+	public void run(){
+		
+	GRect newRect = new GRect( 100, 100, 150,150);
+	newRect.setFilled(true);
+	add(newRect);
+	
+	addMouseListeners();
+	addKeyListeners();
+	}
+	
+	public void mouseClicked(MouseEvent e) {
+//	System.out.println("here");
+//		e. getX();
+		
+		GObject obj = getElementAt(e.getX(), e.getY());
+		
+		if(obj != null) {
+			obj.setColor(Color.RED);
+			
+		}
+		
+		if(obj == newRect) {
+
+			
+			
+		}
+		
+	}
+	public void KeyTyped(keyEvent e) {
+		label.setLabel("key typed : ")
+		e.
+	}
+	}

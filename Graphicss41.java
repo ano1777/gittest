3e2282c32ab739d37281828540df54496daa57e2
S <s>
2023-11-10 10:17:21 +0400

    {"types":"C","runs":35}

--- Graphicss41.java
+++ Graphicss41.java
@@ -1,9 +1,22 @@
+import java.awt.event.MouseEvent;
+
+import acm.graphics.GObject;
+import acm.graphics.GOval;
 import acm.program.GraphicsProgram;
 
 public class Graphicss41 extends GraphicsProgram{
+	private GOval circle;
+	
 	public void run() {
+		addMouseListeners();
 		getMouseMotionListeners();
 	}
 	
-     private void mouseMoved(MouseEvent e)
+	@Override
+     private void mouseMoved (MouseEvent e) {
+    	 GOval circle = new GOval(20, 20);
+    	 circle.setFilled(true);
+    		 add(circle, e.getX(), e.getY());
+    	 
+     }
 }

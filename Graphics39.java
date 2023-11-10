52c7d1652abc27857ce540eb5ca50f76784090ea
S <s>
2023-11-10 08:50:45 +0400

    {"types":"C","runs":9}

--- Graphics39.java
+++ Graphics39.java
@@ -6,15 +6,19 @@ public class Graphics39 extends GraphicsProgram {
 	private static final int PAUSE_TIME = 20;
 	private static final int REVERSE_VELOCITY = -10;
 	private static final int INITIAL_VELOCITY = 10;
+	private RandomGenerator rgen = RandomGenerator.getInstance();
 	public void run() {
 		GOval newOval = new GOval(0, getHeight() / 2 - 25, 50, 50);
 		newOval.setFilled(true);
 		add(newOval);
 		int finalX = getWidth() - 50;
 		int initialX = 0;
-
+        int initialVelo = rgen.nextInt(-20,20);
 		goToTheEnd(newOval, finalX, initialX);
+		newOval.move(initialVelo, initialVelo);
+		pause(PAUSE_TIME);
 	}
+	
 //go to the end of the canvas
 	private void goToTheEnd(GOval newOval, int finalX, int initialX) {
 		while (true) {
@@ -27,7 +31,7 @@ public class Graphics39 extends GraphicsProgram {
 			}
 		}
 	}
-//if the ball is on the the last coordinate of x, it starts moving backwards and gooes to the start.
+//if the ball is on the the last coordinate of x, it starts moving backwards and goes to the start.
 //this process lasts infinitively.
 	private void changeDirection(GOval newOval, int finalX, int initialX) {
 		while (newOval.getX() > 0) {
@@ -43,4 +47,6 @@ public class Graphics39 extends GraphicsProgram {
 			}
 		}
 	}
+	
+	
 }

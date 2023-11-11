428c1a00ff7e270adf30863de90dd55afcf1d280
S <s>
2023-11-11 09:41:21 +0400

    {"types":"C","runs":13}

--- Graphics42.java
+++ Graphics42.java
@@ -1,4 +1,21 @@
+import java.awt.event.MouseEvent;
 
-public class Graphics42 {
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
 
-}
+public class Graphics42 extends GraphicsProgram{
+	public void run() {
+		addMouseListeners();
+		getMouseMotionListeners();
+	}
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		GOval firstPoint = new GOval(5,5);
+		firstPoint.setFilled(true);
+		add(firstPoint, e.getX(), e.getY());
+		
+		}
+	}
+	
+
+

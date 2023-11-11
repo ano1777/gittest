6391800e4b2b57a226177e90af5ff54363e3a655
S <s>
2023-11-11 09:31:40 +0400

    {"types":"C","runs":10}

--- Graphicss41.java
+++ Graphicss41.java
@@ -17,7 +17,7 @@ public class Graphicss41 extends GraphicsProgram{
 	@Override
 	
 
-     public void mouseReleased(MouseEvent e){
+     public void mouseEntered(MouseEvent e){
     	 GOval circle = new GOval(20, 20);
     	 circle.setFilled(true);
     	 circle.setColor(Color.GREEN);

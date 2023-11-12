853207b40d060b24365444fa87e10f94c4e4ab71
S <s>
2023-11-12 16:40:16 +0400

    {"types":"C","runs":99}

--- BouncingBall.java
+++ BouncingBall.java
@@ -1,4 +1,17 @@
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
 
-public class BouncingBall {
+public class BouncingBall extends GraphicsProgram{
+		private RandomGenerator rgen = RandomGenerator.getInstance();
+		private static final int RADIUS = 20;
+		public void run() {
+			double vx = rgen.nextDouble(-3.0, 3.0);
+			double vy = rgen.nextDouble(-3.0, 3.0);
+			GOval ball = new GOval (getWidth()/2-RADIUS, getHeight()/2-RADIUS, 2*RADIUS, 2*RADIUS);
+			add(ball);
+			ball.move(vx,  vy);
+
+	}
 
 }

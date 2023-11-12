a5a4dcfe369428edce32b4045afe9d2620fcd62e
S <s>
2023-11-12 15:49:56 +0400

    {"types":"C","runs":90}

--- breakout.java
+++ breakout.java
@@ -63,7 +63,9 @@ public class breakout extends GraphicsProgram {
 	public void mouseMoved(MouseEvent e) {
 		GOval ball  = new GOval(200,200, BALL_RADIUS*2, BALL_RADIUS*2);
 		ball.setFilled(true);
-		add(ball,e. getX(), e.getY());
+		while (true) {
 		remove(ball);
-	}
+		add(ball,e. getX(), e.getY());
+		}
+}
 }

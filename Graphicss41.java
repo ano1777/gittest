ced34a64c152b3b4d48845b25d281b0f137271d1
S <s>
2023-11-14 17:13:03 +0400

    {"types":"C","runs":2}

--- Graphicss41.java
+++ Graphicss41.java
@@ -5,7 +5,7 @@ import acm.graphics.*;
 import java.awt.Color;
 import java.awt.event.*;
 public class Graphicss41 extends GraphicsProgram{
-
+    private static final int RADIUS = 10;
 	
 	public void run() {
 		addMouseListeners();
@@ -14,7 +14,7 @@ public class Graphicss41 extends GraphicsProgram{
 	}
 	@Override
      public void mouseDragged(MouseEvent e){
-    	 GOval circle = new GOval(20, 20);
+    	 GOval circle = new GOval(2*RADIUS, 2* RADIUS);
     	 circle.setFilled(true);
     	 circle.setColor(Color.GREEN);
     	 add(circle, e.getX(), e.getY());

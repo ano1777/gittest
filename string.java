f82dabb35481f04f168f59a7423d27c25168928d
S <s>
2023-11-21 20:45:42 +0400

    {"types":"C","runs":5}

--- string.java
+++ string.java
@@ -1,10 +1,11 @@
 import acm.program.ConsoleProgram;
 
 public class string extends ConsoleProgram {
-	public void run(){
-		String num = readLine("anni");
-	char ch = num.charAt(1);
+	public void run() {
+		String num = readLine("annia");
+		char ch = num.charAt(0);
 		println(ch);
+		
 	}
 
 }

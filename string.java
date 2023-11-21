efe431ae33a5486a614feeb3b6b4717a3e9c7ebd
S <s>
2023-11-21 21:30:32 +0400

    {"types":"C","runs":14}

--- string.java
+++ string.java
@@ -2,14 +2,23 @@ import acm.program.ConsoleProgram;
 
 public class string extends ConsoleProgram {
 	public void run() {
-		String num = readLine("Enter text : ");
-		char ch = readLine("Enter symbol : ").charAt(0);
-		
-//		String symbol = readLine("Enter symbol :");
-//		char symbolChar = symbol.charAt(0);
+		String text = readLine("Enter text : ");
+		char symbol = readLine("Enter symbol : ").charAt(0);
 
+		println("count is  " + countChars(text, symbol));
+		// String symbol = readLine("Enter symbol :");
+		// char symbolChar = symbol.charAt(0);
 
-	
-}
 	}
 
+	private int countChars(String text, char symbol) {
+		int countChar = 0;
+		for (int i = 0; i < text.length(); i++) {
+			if (text.charAt(i) == symbol) {
+				countChar++;
+			}
+		}
+
+		return countChar;
+	}
+}

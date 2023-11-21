0ad95a33a99111b9941906e4416332da9dcb3857
S <s>
2023-11-21 18:00:31 +0400

    {"types":"C","runs":0}

--- string47.java
+++ string47.java
@@ -1,4 +1,21 @@
+import acm.program.ConsoleProgram;
 
-public class string47 {
+public class string47 extends ConsoleProgram {
+	public void run() {
+		String text = "mariami";
+		boolean isPallindrom = isPallindrom(text);
+		println(text + "is pallindrom: " + isPallindrom );
+
+	}
+
+	private boolean isPallindrom(String text) {
+		for(int i =0; i<text.length() / 2; i++) {
+			if(text.charAt(i) != text.charAt(text.length()) - i - 1){
+			return false;
+		}
+		}
+		return true;
+	
+}
 
 }

ed36408b8c1f6c8bdd2615b381c747544ce0379d
S <s>
2023-11-21 21:22:31 +0400

    {"types":"C","runs":13}

--- string.java
+++ string.java
@@ -3,10 +3,10 @@ import acm.program.ConsoleProgram;
 public class string extends ConsoleProgram {
 	public void run() {
 		String num = readLine("Enter text : ");
-		//char ch = readLine("Enter symbol : ").charAt(0);
+		char ch = readLine("Enter symbol : ").charAt(0);
 		
-		String symbol = readLine("Enter symbol :");
-		char symbolChar = symbol.charAt(0);
+//		String symbol = readLine("Enter symbol :");
+//		char symbolChar = symbol.charAt(0);
 
 
 	

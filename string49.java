237bd4f47dabd40923b24e3ed8812746199d1938
S <s>
2023-11-21 18:00:18 +0400

    {"types":"C","runs":0}

--- string49.java
+++ string49.java
@@ -1,4 +1,14 @@
+import acm.program.ConsoleProgram;
 
-public class string49 {
+public class string49 extends ConsoleProgram{
+	public void run() {
+		String numStr = "1234";
+		int num = stringToInt(numStr);
+	}
+
+	private int stringToInt(String numStr) {
+		
+		return 0;
+	}
 
 }

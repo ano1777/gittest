57c47f2d14fece8300d9d8048d2761f059f8a340
S <s>
2023-11-22 12:14:49 +0400

    {"types":"C","runs":2}

--- string46.java
+++ string46.java
@@ -5,17 +5,19 @@ public class string46 extends ConsoleProgram {
 		String text = readLine("Enter text: ");
 		println("the most frequent symbol in this text is : " + frequentSymbol(text));
 	}
-	
-//
-//	private String frequentSymbol(String text) {
-//		int frequentChar = 0;
-//		int maxCHar = 0;
-//		for(int i =0; i<text.length(); i++) {
-//		int currCount = symbol(text, text.charAt(i));
-//		
-//		return null;
-//	}
 
+	private char frequentSymbol(String text) {
+		char frequentChar = 0;
+		int maxCharCount = 0;
+		for(int i =0; i<text.length(); i++) {
+		int currentCount = symbol(text, text.charAt(i));
+		if(currentCount > maxCharCount) {
+			currentCount = maxCharCount;
+			frequentChar = text.charAt(i);
+		}
+		}
+		return frequentChar;
+	}
 
 	private int symbol(String text, char symbolo) {
 		int countNum = 0;

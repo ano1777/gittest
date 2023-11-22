fdc1cd431c45e32fe5b1a523bf88b5594f6cf191
S <s>
2023-11-23 03:34:24 +0400

    {"types":"C","runs":50}

--- string48.java
+++ string48.java
@@ -20,7 +20,6 @@ public class string48 extends ConsoleProgram {
 	private boolean firstAppearance(String newStr, int i) {
 		char ch = newStr.charAt(i);
 		int firstIndex = newStr.indexOf(ch);
-		
 		return firstIndex == i;
 	}
 
@@ -37,32 +36,3 @@ public class string48 extends ConsoleProgram {
 	}
 
 }
-// String text = "mariami";
-// printCharStatics(text);
-//
-// }
-//
-// private void printCharStatics(String text) {
-// for(int i=0; i<text.length(); i++) {
-// int count = countSymbol(text, text.charAt(i));
-// if(isFirstOccurance (text, i)){
-// int count = countSymbol(text, currCh);
-// println(currCh );
-// }
-// }
-//
-// }
-// private boolean isFirstOccurance(String text, int i) {
-//
-// return false;
-// }
-//
-// private int countSymbol (String text, char symbol) {
-// int symbolCount = 0;
-// for(int i = 0; i < text.length(); i++) {
-// if(text.charAt(i) ==symbol) {
-// symbolCount++; //
-// }
-// }
-// return symbolCount;
-// }

9187038fc55498296535c0bfaa258cbff0f81be8
S <s>
2023-11-22 13:15:08 +0400

    {"types":"C","runs":16}

--- reversedString.java
+++ reversedString.java
@@ -1,16 +1,24 @@
 import acm.program.ConsoleProgram;
 
-public class reversedString extends ConsoleProgram{
+public class reversedString extends ConsoleProgram {
 	public void run() {
 		String str = readLine("Enter text : ");
 		println("resversed text: " + reverseString(str));
 
-}
+	}
+
+	// private String reverseString(String str) {
+	// String result = " ";
+	// for(int i = 0; i < str.length(); i++){
+	// result = str.charAt(i) + result;
+	// }
+	// return result;
+	// }
 
 	private String reverseString(String str) {
 		String result = " ";
-		for(int i = 0; i < str.length(); i++){
-			result = str.charAt(i) + result;
+		for (int i = str.length() - 1; i >= 0; i--) {
+			result = result + str.charAt(i);
 		}
 		return result;
 	}

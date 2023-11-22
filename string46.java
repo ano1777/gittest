678be6edf6a86abc5911f4e15b18ab1c7a11a846
S <s>
2023-11-22 12:29:58 +0400

    {"types":"C","runs":8}

--- string46.java
+++ string46.java
@@ -9,17 +9,17 @@ public class string46 extends ConsoleProgram {
 	private char frequentSymbol(String text) {
 		char frequentChar = 0;
 		int maxCharCount = 0;
-		for(int i =0; i<text.length(); i++) {
-		int currentCount = symbol(text, text.charAt(i));
-		if(currentCount > maxCharCount) {
-			maxCharCount = currentCount;
-			frequentChar = text.charAt(i);
-		}
+		for (int i = 0; i < text.length(); i++) {
+			int currentCount = symbolCount(text, text.charAt(i));
+			if (currentCount > maxCharCount) {
+				maxCharCount = currentCount;
+				frequentChar = text.charAt(i);
+			}
 		}
 		return frequentChar;
 	}
 
-	private int symbol(String text, char symbolo) {
+	private int symbolCount(String text, char symbolo) {
 		int countNum = 0;
 
 		for (int i = 0; i < text.length(); i++) {

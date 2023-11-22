bb97c0cfd56da0a466dbbf0da6b133aafd710859
S <s>
2023-11-23 03:34:26 +0400

    {"types":"C","runs":50}

--- string49.java
+++ string49.java
@@ -1,14 +1,23 @@
 import acm.program.ConsoleProgram;
 
-public class string49 extends ConsoleProgram{
+public class string49 extends ConsoleProgram {
 	public void run() {
 		String numStr = "1234";
 		int num = stringToInt(numStr);
 	}
 
 	private int stringToInt(String numStr) {
-		
-		return 0;
+		int num = 0;
+		for (int i = 0; i < numStr.length(); i++) {
+			int currNum = charConvertToDig(numStr, numStr.charAt(i));
+			num = num*10 + currNum;
+		}
+		return num;
+	}
+
+	private int charConvertToDig(String numStr, char ch) {
+		int integer = ch - '0';
+		return integer;
 	}
 
 }

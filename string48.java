0da15b24d05b863b68be4141d5517b6c8d2afc95
S <s>
2023-11-22 13:46:54 +0400

    {"types":"C","runs":17}

--- string48.java
+++ string48.java
@@ -2,34 +2,49 @@ import acm.program.ConsoleProgram;
 
 public class string48 extends ConsoleProgram {
 	public void run() {
-		String text = "mariami";
-		printCharStatics(text);
-
-	}
-
-	private void printCharStatics(String text) {
-		for(int i=0; i<text.length(); i++) {
-			int count = countSymbol(text, text.charAt(i));
-			if(isFirstOccurance (text, i)){
-				int count = countSymbol(text, currCh);
-				println(currCh );
-			}
-		}
+		String str = "ani";
+		String str2 = "ani";
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 	}
-	private boolean isFirstOccurance(String text, int i) {
-		
-		return false;
-	}
-
-	private int countSymbol (String text, char symbol) {
-		int symbolCount = 0;
-		for(int i = 0; i < text.length(); i++) {
-			if(text.charAt(i) ==symbol) {
-				symbolCount++; //
-			}
-		}
-		return symbolCount;
-	}
+//		String text = "mariami";
+//		printCharStatics(text);
+//
+//	}
+//
+//	private void printCharStatics(String text) {
+//		for(int i=0; i<text.length(); i++) {
+//			int count = countSymbol(text, text.charAt(i));
+//			if(isFirstOccurance (text, i)){
+//				int count = countSymbol(text, currCh);
+//				println(currCh );
+//			}
+//		}
+//		
+//	}
+//	private boolean isFirstOccurance(String text, int i) {
+//		
+//		return false;
+//	}
+//
+//	private int countSymbol (String text, char symbol) {
+//		int symbolCount = 0;
+//		for(int i = 0; i < text.length(); i++) {
+//			if(text.charAt(i) ==symbol) {
+//				symbolCount++; //
+//			}
+//		}
+//		return symbolCount;
+//	}
 
 }

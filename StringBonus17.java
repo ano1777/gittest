040a302b32d1008cc4cfe0f5177ebc76a17a146b
S <s>
2023-11-23 15:35:04 +0400

    {"types":"C","runs":20}

--- StringBonus17.java
+++ StringBonus17.java
@@ -4,9 +4,9 @@ public class StringBonus17 extends ConsoleProgram {
 	public void run() {
 		String str = "ani";
 		String str2 = "an";
-		if(1==1){
+		
 		str2 = str2 + "i";
-		}
+		
 		if(str .equals (str2) ){
 			println ("tolia");
 		}else {

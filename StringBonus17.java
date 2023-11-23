293b7af5ece577f01c92ae2e1ced54b88896e98c
S <s>
2023-11-23 15:29:26 +0400

    {"types":"C","runs":16}

--- StringBonus17.java
+++ StringBonus17.java
@@ -3,7 +3,7 @@ import acm.program.ConsoleProgram;
 public class StringBonus17 extends ConsoleProgram {
 	public void run() {
 		String str = "ani";
-		String str2 = "ani";
+		String str2 = "an";
 		if(str == str2 ){
 			println ("tolia");
 		}else {

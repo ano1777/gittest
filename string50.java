1a98ef5d4e5e1bea2e757ab97fa92c4a585ce80d
S <s>
2023-11-23 04:20:25 +0400

    {"types":"C","runs":56}

--- string50.java
+++ string50.java
@@ -2,7 +2,7 @@ import acm.program.ConsoleProgram;
 
 public class string50 extends ConsoleProgram {
 	public void run() {
-	println((int)'7' - (int) '0');
+	println((int)'7' - (int) '1');
 	println((int)('7' - '1'));
 	
 	}

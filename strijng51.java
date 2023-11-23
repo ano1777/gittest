074f993f909183450abba772b39bfc173862d75c
S <s>
2023-11-23 15:29:17 +0400

    {"types":"C","runs":15}

--- strijng51.java
+++ strijng51.java
@@ -2,7 +2,22 @@ import acm.program.ConsoleProgram;
 
 public class strijng51 extends ConsoleProgram {
 	public void run() {
+		String str = readLine(" Enter text : ");
+		removeDuplicates(str);
+	}
+
+	private void removeDuplicates(String str) {
+		for (int i= 0; i<str.length(); i++) {
+		char ch = str.charAt(i);
+		if(isFirstOccurance(str, ch)) {
+			
+		}
+		}
+	}
+
+	private boolean isFirstOccurance(String str, char ch) {
 		
+		return false;
 	}
 
 }

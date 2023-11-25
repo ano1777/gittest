df2bda5dab20efb427944dbedc6eeb25e2ed5662
S <s>
2023-11-25 12:13:17 +0400

    {"types":"C","runs":31}

--- StringBonus17.java
+++ StringBonus17.java
@@ -8,13 +8,14 @@ public class StringBonus17 extends ConsoleProgram {
 	}
 
 	private boolean checkIfItsAnagram(String str, String str2) {
-		for (int i = 0; i < str.length(); i++) {{
+		for (int i = 0; i < str.length(); i++) {
 				if (str.charAt(i) != str2.charAt(i)) {
-				}					return false;
-
+						
+				return false;
+				}
 			}			
 
-		}
+		
 
 		return true;
 	}

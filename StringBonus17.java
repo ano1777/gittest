2aab0ae1602c84ad7ae007840c56b75eace7b1f1
S <s>
2023-11-25 12:05:39 +0400

    {"types":"C","runs":22}

--- StringBonus17.java
+++ StringBonus17.java
@@ -2,16 +2,22 @@ import acm.program.ConsoleProgram;
 
 public class StringBonus17 extends ConsoleProgram {
 	public void run() {
-		String str = "ani";
-		String str2 = "an";
-		
-	
-		
-		if(str .equals (str2) ){
-			println ("tolia");
-		}else {
-			println("araa");
+		String str = readLine("Enter text : ");
+		String str2 = readLine("Enter text : ");
+		checkIfItsAnagram(str, str2);
+
+	}
+
+	private boolean checkIfItsAnagram(String str, String str2) {
+		for (int i = 0; i < str.length(); i++) {
+			for (int j = 0; j < str2.length(); j++) {
+				if (str.charAt(i) != str2.charAt(j)) {
+					return false;
+				}
+			}
 		}
+
+		return true;
 	}
 
 }

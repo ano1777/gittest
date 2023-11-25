92ae6ac534618dc75f60e33ff6ff4a4a00e11469
S <s>
2023-11-25 12:09:14 +0400

    {"types":"C","runs":25}

--- StringBonus17.java
+++ StringBonus17.java
@@ -12,14 +12,13 @@ public class StringBonus17 extends ConsoleProgram {
 		for (int i = 0; i < str.length(); i++) {
 			for (int j = 0; j < str2.length(); j++) {
 				if (str.charAt(i) != str2.charAt(j)) {
-					return false;
-				}else{
 					return true;
 				}
+				}
 			}
-		}
+		
 
-		return true;
+		return false;
 	}
 
 }

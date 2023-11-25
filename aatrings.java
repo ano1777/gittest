89270908fd5ee6bdae69016424d9a62978b19cb1
S <s>
2023-11-25 15:01:04 +0400

    {"types":"C","runs":57}

--- aatrings.java
+++ aatrings.java
@@ -4,10 +4,10 @@ public class aatrings extends ConsoleProgram {
 	public void run() {
 		String text = "ani yavelashvili";
 		String str = "";
-		
+		println(str);
 		for (int i=0; i <  text.length(); i++ ){
 			char current = str.charAt(i);
-		    //println(text.charAt(i));
+		 
 			str = str + current ;
 		}
 	}

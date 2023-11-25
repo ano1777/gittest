74aa55e53bb5871058d003590be49a9de5e893b5
S <s>
2023-11-25 12:11:53 +0400

    {"types":"C","runs":29}

--- StringBonus17.java
+++ StringBonus17.java
@@ -5,18 +5,17 @@ public class StringBonus17 extends ConsoleProgram {
 		String str = readLine("Enter text : ");
 		String str2 = readLine("Enter text : ");
 		println(checkIfItsAnagram(str, str2));
-
 	}
 
 	private boolean checkIfItsAnagram(String str, String str2) {
 		for (int i = 0; i < str.length(); i++) {
 			for (int j = 0; j < str2.length(); j++) {
 				if (str.charAt(i) != str2.charAt(j)) {
-					return false;
-				}
-				}
-			}
-		
+				}					return false;
+
+			}			
+
+		}
 
 		return true;
 	}

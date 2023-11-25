a140c4ace7a8bcc33273b60e85bc71ed263fcdaf
S <s>
2023-11-25 14:53:45 +0400

    {"types":"C","runs":54}

--- aatrings.java
+++ aatrings.java
@@ -1,4 +1,11 @@
+import acm.program.ConsoleProgram;
 
-public class aatrings {
+public class aatrings extends ConsoleProgram {
+	public void run() {
+		String text = "ani yavelashvili";
+		for (int i=0; i<  text.length(); i++ ){
+			System.out.println(text.charAt(i));
+		}
+	}
 
 }

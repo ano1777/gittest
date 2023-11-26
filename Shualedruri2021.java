0c80febfc485450308924c97db1a631a41f6f4fc
S <s>
2023-11-26 10:34:06 +0400

    {"types":"C","runs":77}

--- Shualedruri2021.java
+++ Shualedruri2021.java
@@ -5,23 +5,22 @@ import acm.program.GraphicsProgram;
 
 public class Shualedruri2021 extends GraphicsProgram {
 	public void run() {
-		addFirstPoint();
+		//addFirstPoint();
 		addMouseListeners();
 	}
 
-	private void addFirstPoint() {
-		GLine point = new GLine(0, 0, 0, 0);
-		add(point);
-	}
+
+
 
 	@Override
 	public void mouseClicked(MouseEvent e) {	
-		GLine line = new GLine(0, 0, e.getX(), e.getY());
+		GLine line = new GLine(0, 0,0,0);
 		add(line);
 		//while(e.getX()< getWidth() && e.getY()<getHeight())
 	}
 	
 	public void mouseDragged(MouseEvent e) {
 		GLine line1 = new GLine(0,0 , e.getX(), e.getY());
+		add(line1);
 	}
 }

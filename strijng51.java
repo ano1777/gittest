8c2db21603d384420a38073cb76ae41908f24f0d
S <s>
2023-11-26 12:10:48 +0400

    {"types":"C","runs":94}

--- strijng51.java
+++ strijng51.java
@@ -4,25 +4,37 @@ public class strijng51 extends ConsoleProgram {
 	public void run() {
 		String str = readLine(" Enter text : ");
 		String str2 = readLine("enter text");
-		println(str.compareTo(str2));
-	
-		
-	//	removeDuplicates(str);
+		println(sameChars(str, str2));
+
+		// removeDuplicates(str);
 	}
-//anhoiselnlo
-//hello
-//	private void removeDuplicates(String str) {
-//		for (int i= 0; i<str.length(); i++) {
-//		char ch = str.charAt(i);
-//		if(isFirstOccurance(str, ch)) {
-//			
-//		}
-//		}
-//	}
-//
-//	private boolean isFirstOccurance(String str, char ch) {
-//		
-//		return false;
-//	}
+
+	private char sameChars(String str, String str2) {
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			for(int j = 0; j<str2.length(); j++) {
+				if(ch == str2.charAt(j)){
+					println(ch);
+				}
+			}
+		}
+
+		return null;
+	}
+	// anhoiselnlo
+	// hello
+	// private void removeDuplicates(String str) {
+	// for (int i= 0; i<str.length(); i++) {
+	// char ch = str.charAt(i);
+	// if(isFirstOccurance(str, ch)) {
+	//
+	// }
+	// }
+	// }
+	//
+	// private boolean isFirstOccurance(String str, char ch) {
+	//
+	// return false;
+	// }
 
 }

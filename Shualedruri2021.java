3659e165b519c3492cb1d4bcbc2e471c4a9c87a7
S <s>
2023-11-26 22:36:25 +0400

    {"types":"C","runs":9}

--- Shualedruri2021.java
+++ Shualedruri2021.java
@@ -10,7 +10,10 @@ public class Shualedruri2021 extends ConsoleProgram {
 	double b = 2.4;
 	double c = 2.0;
 	double k = a/5;
-	println(k);
+	double d = b/2;
+	double e = c/5;
+	double ab = a+b+c;
+	println(ab);
 }
 	
 	

9c9e24101fbe1344af2048ef6e1cddda885a6fa0
S <s>
2023-11-26 22:31:51 +0400

    {"types":"C","runs":4}

--- Shualedruri2021.java
+++ Shualedruri2021.java
@@ -6,9 +6,9 @@ import acm.program.GraphicsProgram;
 
 public class Shualedruri2021 extends ConsoleProgram {
 	public void run() {
-	int a = 12;
+	double a = 12;
 	double b = 2.4;
-	int c = 2;
+	double c = 2.0;
 	double k = a/5;
 	println(k);
 }

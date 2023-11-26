75a1b4d732eb5f0f90b953227fefc7443295e72f
S <s>
2023-11-26 11:37:01 +0400

    {"types":"C","runs":86}

--- Graphics42.java
+++ Graphics42.java
@@ -8,15 +8,4 @@ import acm.program.GraphicsProgram;
 public class Graphics42 extends GraphicsProgram{
 	
 private GRect square;
-	public void run() {
-		 square = new GRect (0,0, 40,40);
-		add(square);
-		addMouseListeners();
-	}
-		public void mouseMoved(MouseEvent e) {
-			int x = 10;
-			int y = 10;
-			square.move(x, y);
-		}
-
 }

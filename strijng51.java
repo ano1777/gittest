20fd6c9c1f1dcc651e01b3780d562a7cbafc0aac
S <s>
2023-11-26 12:12:06 +0400

    {"types":"C","runs":95}

--- strijng51.java
+++ strijng51.java
@@ -4,22 +4,24 @@ public class strijng51 extends ConsoleProgram {
 	public void run() {
 		String str = readLine(" Enter text : ");
 		String str2 = readLine("enter text");
-		println(sameChars(str, str2));
+		sameChars(str, str2);
 
 		// removeDuplicates(str);
 	}
 
-	private char sameChars(String str, String str2) {
+	private void sameChars(String str, String str2) {
 		for (int i = 0; i < str.length(); i++) {
 			char ch = str.charAt(i);
 			for(int j = 0; j<str2.length(); j++) {
 				if(ch == str2.charAt(j)){
 					println(ch);
 				}
+					
+				}
 			}
 		}
 
-		return null;
+	
 	}
 	// anhoiselnlo
 	// hello

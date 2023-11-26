df88d377e9f2eadd239223ae532b42e1c4d4845a
S <s>
2023-11-26 13:34:29 +0400

    {"types":"C","runs":98}

--- Graphics42.java
+++ Graphics42.java
@@ -1,11 +1,38 @@
+import java.awt.Color;
 import java.awt.event.MouseEvent;
+
+
 
 import acm.graphics.GLine;
 import acm.graphics.GOval;
 import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
+import acm.util.RandomGenerator;
 
-public class Graphics42 extends GraphicsProgram{
+public class Graphics42 extends GraphicsProgram {
+
+	private GOval circle;
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+	private static final int CIRCLE_D = 50;
+
+	public void run() {
+		addMouseListeners();
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		Color color = rgen.nextColor();
+		circle = new GOval(CIRCLE_D, CIRCLE_D);
+		circle.setFilled(true);
+		circle.setColor(color);
 	
-private GRect square;
+		add(circle, e.getX() - CIRCLE_D/2, e.getY()-CIRCLE_D/2);
+	}
+	@Override 
+	public void mouseClicked(MouseEvent e) {
+		Color color = rgen.nextColor();
+		for(int i=0; i<5; i++) {
+			circle.setColor(color);
+		}
+	}
 }

35188720f200de7227ac42165ba6b50b5b5e4b86
S <s>
2023-11-26 22:52:23 +0400

    {"types":"C","runs":19}

--- Shualedruri2021.java
+++ Shualedruri2021.java
@@ -6,35 +6,36 @@ import acm.program.GraphicsProgram;
 
 public class Shualedruri2021 extends ConsoleProgram {
 	public void run() {
-		int a = 'd' + 1 - 'a' + 'E' + 3 + 2  +'b';
-		println(a);
-}
+		if(5 % 2 == 0 && 4 > 5-6);
+		println(true);
 	
-	
-//	private GLine line;
-//	int x =0;
-//	int y = 0;
-//
-//	public void run() {
-//		x =0;
-//		y = 0;
-//		addMouseListeners();
-//	}
-//
-//	@Override
-//	public void mousePressed(MouseEvent e) {
-//		line = new GLine(x, y, e.getX(), e.getY());
-//		add(line);
-//		
-//	}
-//	@Override
-//	public void mouseDragged(MouseEvent e) {
-//		line.setEndPoint(e.getX(), e.getY());
-//	}
-//	@Override
-//	public void mouseReleased(MouseEvent e) {
-//		x = e.getX();
-//		y= e.getY();
-//	}
+	}
+	}
+
+	// private GLine line;
+	// int x =0;
+	// int y = 0;
+	//
+	// public void run() {
+	// x =0;
+	// y = 0;
+	// addMouseListeners();
+	// }
+	//
+	// @Override
+	// public void mousePressed(MouseEvent e) {
+	// line = new GLine(x, y, e.getX(), e.getY());
+	// add(line);
+	//
+	// }
+	// @Override
+	// public void mouseDragged(MouseEvent e) {
+	// line.setEndPoint(e.getX(), e.getY());
+	// }
+	// @Override
+	// public void mouseReleased(MouseEvent e) {
+	// x = e.getX();
+	// y= e.getY();
+	// }
 
 }

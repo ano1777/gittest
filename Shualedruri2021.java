268f41ca826dd3270a6254bd7310f72e3e3f9c0b
S <s>
2023-11-26 10:58:17 +0400

    {"types":"C","runs":81}

--- Shualedruri2021.java
+++ Shualedruri2021.java
@@ -16,14 +16,15 @@ public class Shualedruri2021 extends GraphicsProgram {
 	public void mouseClicked(MouseEvent e) {
 	    int x = 0;
 	    int y =0;
-		GLine line = new GLine(x , y , x, y);
+		GLine line = new GLine(x , y , e.getX(), e.getY());
 		add(line);
+		while(true){
 		x = e.getX();
 		y = e.getY();
 		GLine lines = new GLine(x, y, e.getX(), e.getY());
 		add(lines);
 	}
-	
+	}
 //	public void mouseDragged(MouseEvent e) {
 //		GLine line1 = new GLine(0,0 , e.getX(), e.getY());
 //		add(line1);

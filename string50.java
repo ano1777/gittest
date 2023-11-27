87aab18d7f7014068344372333a64093e8d65500
S <s>
2023-11-27 16:12:42 +0400

    {"types":"C","runs":29}

--- string50.java
+++ string50.java
@@ -14,8 +14,8 @@ public class string50 extends ConsoleProgram {
 		int count = 0;
 		while (tokens.hasMoreTokens()) {
 			String newToken = tokens.nextToken();
-			String reversed = reverse(newToken);
-			println(reversed);
+		//	String reversed = reverse(newToken);
+			println(newToken);
 		}
 	
 	}

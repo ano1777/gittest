296a3d0b7d29479ab70a1700e63fa019820ee93b
S <s>
2023-11-27 18:27:28 +0400

    {"types":"C","runs":2}

--- stringleetcode.java
+++ stringleetcode.java
@@ -2,7 +2,22 @@ import acm.program.ConsoleProgram;
 
 public class stringleetcode extends ConsoleProgram{
 	public void run() {
-		
+		String haystack = ("Enter text: ");
+		String needle = ("Enter text: ");
+		int result = returnInt(haystack, needle);
+		println(result);
+	}
+
+	private int returnInt(String haystack, String needle) {
+		for(int i=0; i < haystack.length()-1; i++){
+			for(int j = 0; j<haystack.length(); j++){
+				String sub = haystack.substring(i, j);
+				if(sub.equals(needle)){
+					return(i);
+				}
+			}
+		}
+		return -1;
 	}
 
 }

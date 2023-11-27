91efe51a88977342c23cdf0f865c1f2f557e1500
S <s>
2023-11-27 18:29:38 +0400

    {"types":"C","runs":5}

--- stringleetcode.java
+++ stringleetcode.java
@@ -9,15 +9,19 @@ public class stringleetcode extends ConsoleProgram{
 	}
 
 	private int returnInt(String haystack, String needle) {
-		for(int i=0; i < haystack.length()-1; i++){
+		for(int i=0; i < (haystack.length()-1); i++){
 			for(int j = 0; j<haystack.length(); j++){
 				String sub = haystack.substring(i, j);
 				if(sub.equals(needle)){
 					return(i);
 				}
+				else{
+					return -1;
+				}
 			}
 		}
-		return (-1);
+		return -1;
+
 	}
 
 }

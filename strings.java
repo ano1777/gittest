95c30fddbe6259c5b1280830bfd68f6bf12e8240
S <s>
2023-11-28 17:54:14 +0400

    {"types":"C","runs":1}

--- strings.java
+++ strings.java
@@ -1,4 +1,20 @@
+import acm.program.ConsoleProgram;
 
-public class strings {
-
+public class strings extends ConsoleProgram{
+	public void run() {
+		String str = readLine("Enter text: ");
+		String str2 = removeDuplicates(str);
+		println(str2);
+		
+	}
+	private String removeDuplicates(String text){
+		String result = "";
+		for(int i=0; i<text.length(); i++) {
+			char ch = text.charAt(i);
+			if(text.indexOf(ch) == i){
+				result +=ch;
+			}
+		}
+		return result;
+	}
 }

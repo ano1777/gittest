872d25fa612ca71e5ac3e11e9e9425dc7d97ebf7
S <s>
2023-11-28 10:57:42 +0400

    {"types":"C","runs":19}

--- string50.java
+++ string50.java
@@ -4,27 +4,36 @@ import acm.program.ConsoleProgram;
 
 public class string50 extends ConsoleProgram {
 	public void run() {
-		String text = readLine("Enter text to tokenize: ");
-		String reversed = reverse(text);
-		println(simulateTokens(reversed));
+		String str = readLine("Enter string: ");
+		StringTokenizer newtokens = new StringTokenizer(str, " ");
+		while(newtokens.hasMoreTokens()){
+		String newTok = newtokens.nextToken();
+		println(newTok);
+		}
+		
 	}
 
-	private String simulateTokens(String text) {
-		StringTokenizer tokens = new StringTokenizer(text, " ");
-		String answer = "";
-		while (tokens.hasMoreTokens()) {
-			String newToken = tokens.nextToken();
-			String reversed = reverse(newToken);
-			answer += reversed + " ";
-		}
-		return answer;
-	}
+	// String text = readLine("Enter text to tokenize: ");
+	// String reversed = reverse(text);
+	// println(simulateTokens(reversed));
+	// }
+	//
+	// private String simulateTokens(String text) {
+	// StringTokenizer tokens = new StringTokenizer(text, " ");
+	// String answer = "";
+	// while (tokens.hasMoreTokens()) {
+	// String newToken = tokens.nextToken();
+	// String reversed = reverse(newToken);
+	// answer += reversed + " ";
+	// }
+	// return answer;
+	// }
+	//
+	// private String reverse(String text) {
+	// String s = "";
+	// for (int i = text.length() - 1; i >= 0; i--) {
+	// s += text.charAt(i);
+	// }
+	// return s;
 
-	private String reverse(String text) {
-		String s = "";
-		for (int i = text.length() - 1; i >= 0; i--) {
-			s += text.charAt(i);
-		}
-		return s;
-	}
 }

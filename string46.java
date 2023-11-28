0db641507184168aceb7bcfec4661bbe7893fa2b
S <s>
2023-11-28 09:46:21 +0400

    {"types":"C","runs":1}

--- string46.java
+++ string46.java
@@ -2,32 +2,62 @@ import acm.program.ConsoleProgram;
 
 public class string46 extends ConsoleProgram {
 	public void run() {
-		String text = readLine("Enter text: ");
-		println("the most frequent symbol in this text is : " + frequentSymbol(text));
+		String str = readLine("Enter text: ");
+		println(frequentSymb(str));
+
 	}
 
-	private char frequentSymbol(String text) {
-		int maxCount =0;
-		char mostFrequentChar = 0;
-		for (int i=0; i<text.length(); i++){
-			int count = symbolCount(text, text.charAt(i));
-			if(count > maxCount){
-				maxCount = count;
-				mostFrequentChar = text.charAt(i);
+	private char frequentSymb(String str) {
+		char frequentchar = 0;
+		int maxNum = 0;
+		for (int i = 0; i < str.length(); i++) {
+			int count = countSymbols(str, str.charAt(i));
+			if (count > maxNum) {
+				maxNum = count;
+				frequentchar = str.charAt(i);
+
 			}
 		}
-		return mostFrequentChar;
+
+		return frequentchar;
 	}
 
-	private int symbolCount(String text, char charAt) {
-		int countCharNum = 0;
-		for(int i=0; i<text.length(); i++) {
-		   if(text.charAt(i) == charAt){
-			   countCharNum ++;
-		   }
-		}
-		return countCharNum;
+	private int countSymbols(String str, char symbol) {
+		int count = 0;
+		for(int i = 0; i < str.length(); i++){
+			if(symbol == str.charAt(i)){
+				count++;
+			}
+		
+		return 0;
 	}
+	
+//		String text = readLine("Enter text: ");
+//		println("the most frequent symbol in this text is : " + frequentSymbol(text));
+//	}
+//
+//	private char frequentSymbol(String text) {
+//		int maxCount =0;
+//		char mostFrequentChar = 0;
+//		for (int i=0; i<text.length(); i++){
+//			int count = symbolCount(text, text.charAt(i));
+//			if(count > maxCount){
+//				maxCount = count;
+//				mostFrequentChar = text.charAt(i);
+//			}
+//		}
+//		return mostFrequentChar;
+//	}
+//
+//	private int symbolCount(String text, char charAt) {
+//		int countCharNum = 0;
+//		for(int i=0; i<text.length(); i++) {
+//		   if(text.charAt(i) == charAt){
+//			   countCharNum ++;
+//		   }
+//		}
+//		return countCharNum;
+//	}
 
 	
 }

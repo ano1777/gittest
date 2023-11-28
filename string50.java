1749bbe8a4d965781317db8df9e16b14829b8ef7
S <s>
2023-11-28 17:11:06 +0400

    {"types":"C","runs":1}

--- string50.java
+++ string50.java
@@ -4,39 +4,42 @@ import acm.program.ConsoleProgram;
 
 public class string50 extends ConsoleProgram {
 	public void run() {
-		String str = readLine("enter text: ");
-		String str2 = reverseWords(str);
-		println(str2);
+//		String str = readLine("enter text: ");
+//		String str2 = reverseWords(str);
+//		println(str2);
+//
+//	}
+//
+//	private String reverseWords(String str) {
+//
+//		String answer = "";
+//
+//		for (int i = 0; i < str.length(); i++) {
+//
+//			if (str.charAt(i) == ' ') {
+//
+//				String sub1 = str.substring(0, i);
+//				String sub2 = str.substring(i + 1);
+//				println(sub1);
+//				println(sub2);
+//				answer = sub2 + ' ' + sub1;
+//
+//			}
+//		}
+//		return answer;
+//	}
+//}
 
-	}
-
-	private String reverseWords(String str) {
-
-		String answer = "";
-
-		for (int i = 0; i < str.length(); i++) {
-
-			if (str.charAt(i) == ' ') {
-
-				String sub1 = str.substring(0, i);
-				String sub2 = str.substring(i + 1);
-				println(sub1);
-				println(sub2);
-				answer = sub2 + ' ' + sub1;
-
-			}
-		}
-		return answer;
-	}
+ String str = readLine("Enter string: ");
+ StringTokenizer newtokens = new StringTokenizer(str);
+ int count = 0;
+ 		while(newtokens.hasMoreTokens()){
+ 			String newTok = newtokens.nextToken();
+ 			count++;
+ 		}
+ println(count);
+ }
 }
-
-// String str = readLine("Enter string: ");
-// StringTokenizer newtokens = new StringTokenizer(str, " ");
-// while(newtokens.hasMoreTokens()){
-// String newTok = newtokens.nextToken();
-// println(newTok);
-// }
-// }
 
 // String s = readLine("enter text: ");
 // String newSt = reversed(s);

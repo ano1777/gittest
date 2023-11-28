f955463dcdd302e584827e57c3c7fb08fd6c1640
S <s>
2023-11-28 11:24:00 +0400

    {"types":"C","runs":21}

--- string50.java
+++ string50.java
@@ -4,14 +4,38 @@ import acm.program.ConsoleProgram;
 
 public class string50 extends ConsoleProgram {
 	public void run() {
-		String str = readLine("Enter string: ");
-		StringTokenizer newtokens = new StringTokenizer(str, ",");
-		while(newtokens.hasMoreTokens()){
-		String newTok = newtokens.nextToken();
-		println(newTok);
-		}
-		
-	}
+//		String str = readLine("Enter string: ");
+//		StringTokenizer newtokens = new StringTokenizer(str, " ");
+//		while(newtokens.hasMoreTokens()){
+//		String newTok = newtokens.nextToken();
+//		println(newTok);
+//		}
+//	} 
+	String s = readLine("enter text: ");
+        String newSt = reversed(s);
+        println(simulate(newSt));
+    }
+
+private String simulate (String newSt){
+    String  answer  = "";
+        StringTokenizer newTokens = new StringTokenizer(newSt, " ");
+        while(newTokens.hasMoreTokens()){
+        	String token = newTokens.nextToken();
+           String rev = reversed(token);
+           answer +=rev  + " ";
+        }
+        return answer;
+    }
+
+    private String reversed(String s){
+       String result = "";
+       for(int i = s.length(); i >=0 ; i--){
+           char ch = s.charAt(i);
+           result += ch;
+       }
+
+    }
+}
 
 	// String text = readLine("Enter text to tokenize: ");
 	// String reversed = reverse(text);

7f051fdcdb82aa5b4e418162825330f8fe7f32af
S <s>
2023-11-28 09:49:06 +0400

    {"types":"C","runs":7}

--- string46.java
+++ string46.java
@@ -14,7 +14,7 @@ public class string46 extends ConsoleProgram {
 			int count = countSymbols(str, str.charAt(i));
 			if (count > maxNum) {
 				maxNum = count;
-				frequentchar = str.charAt(i+1);
+				frequentchar = str.charAt(i);
 
 			}
 		}
@@ -33,32 +33,3 @@ public class string46 extends ConsoleProgram {
 	
 	}
 }
-//		String text = readLine("Enter text: ");
-//		println("the most frequent symbol in this text is : " + frequentSymbol(text));
-//	}
-//
-//	private char frequentSymbol(String text) {
-//		int maxCount =0;
-//		char mostFrequentChar = 0;
-//		for (int i=0; i<text.length(); i++){
-//			int count = symbolCount(text, text.charAt(i));
-//			if(count > maxCount){
-//				maxCount = count;
-//				mostFrequentChar = text.charAt(i);
-//			}
-//		}
-//		return mostFrequentChar;
-//	}
-//
-//	private int symbolCount(String text, char charAt) {
-//		int countCharNum = 0;
-//		for(int i=0; i<text.length(); i++) {
-//		   if(text.charAt(i) == charAt){
-//			   countCharNum ++;
-//		   }
-//		}
-//		return countCharNum;
-//	}
-
-	
-

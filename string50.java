4ceb7f4d686f9f13a52edf1fcca18e3abf42fe58
S <s>
2023-11-28 12:16:19 +0400

    {"types":"C","runs":33}

--- string50.java
+++ string50.java
@@ -7,66 +7,60 @@ public class string50 extends ConsoleProgram {
 		String str = readLine("enter text: ");
 		String str2 = reverseWords(str);
 		println(str2);
-		
-		
+
 	}
 
 	private String reverseWords(String str) {
-		
+
 		String answer = "";
-	
-		for(int i=0; i<str.length(); i++){
-		
-			while(true){
-			if(str.charAt(i) == ' '){
-	
+
+		for (int i = 0; i < str.length(); i++) {
+
+			if (str.charAt(i) == ' ') {
+
 				String sub1 = str.substring(0, i);
-				String sub2 = str.substring(i+1);
+				String sub2 = str.substring(i + 1);
 				println(sub1);
 				println(sub2);
-				answer = sub2 + ' ' + sub1; 
-			}
+				answer = sub2 + ' ' + sub1;
+
 			}
 		}
 		return answer;
 	}
-	}
+}
 
+// String str = readLine("Enter string: ");
+// StringTokenizer newtokens = new StringTokenizer(str, " ");
+// while(newtokens.hasMoreTokens()){
+// String newTok = newtokens.nextToken();
+// println(newTok);
+// }
+// }
 
-		// String str = readLine("Enter string: ");
-		// StringTokenizer newtokens = new StringTokenizer(str, " ");
-		// while(newtokens.hasMoreTokens()){
-		// String newTok = newtokens.nextToken();
-		// println(newTok);
-		// }
-		// }
-		
-		
-		
-//		String s = readLine("enter text: ");
-//		String newSt = reversed(s);
-//		println(simulate(newSt));
-//	}
+// String s = readLine("enter text: ");
+// String newSt = reversed(s);
+// println(simulate(newSt));
+// }
 //
-//	private String simulate(String newSt) {
-//		String answer ="";
-//		StringTokenizer newTokens = new StringTokenizer(newSt, " ");
-//		while (newTokens.hasMoreTokens()) {
-//			String token = newTokens.nextToken();
-//			String rev = reversed(token);
-//			answer += rev + " ";
-//		}
-//		return answer;
-//	}
+// private String simulate(String newSt) {
+// String answer ="";
+// StringTokenizer newTokens = new StringTokenizer(newSt, " ");
+// while (newTokens.hasMoreTokens()) {
+// String token = newTokens.nextToken();
+// String rev = reversed(token);
+// answer += rev + " ";
+// }
+// return answer;
+// }
 //
-//	private String reversed(String s) {
-//		String result = "";
-//		for (int i = s.length() - 1; i >= 0; i--) {
-//			char ch = s.charAt(i);
-//			result += ch;
-//		}
-//		return result;
+// private String reversed(String s) {
+// String result = "";
+// for (int i = s.length() - 1; i >= 0; i--) {
+// char ch = s.charAt(i);
+// result += ch;
+// }
+// return result;
 //
-//	}
-//}
-
+// }
+// }

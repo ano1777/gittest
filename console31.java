a98af76e27aabe4a241b84e95536136136af4428
S <s>
2023-11-28 12:23:14 +0400

    {"types":"C","runs":37}

--- console31.java
+++ console31.java
@@ -2,25 +2,45 @@ import acm.program.ConsoleProgram;
 
 public class console31 extends ConsoleProgram {
 	public void run() {
-		int n = readInt("Enter number : ");
-		if (numIsPrime(n)) {
-			println("this number is prime");
-
-		} else{
-			
-			println("this number is not prime");
-		}
+		int k = readInt("Enter num: ");
+		if(isPrime(k)){
+			println("the number is prime");
+		}else{ 
+			println("the number is not prime: ");
+		}	
+		
+		
 	}
+//		int n = readInt("Enter number : ");
+//		if (numIsPrime(n)) {
+//			println("this number is prime");
+//
+//		} else{
+//			
+//			println("this number is not prime");
+//		}
+//	}
+//
+//	
+//
+//	private boolean numIsPrime(int n) {
+//		for (int i = 2; i <= Math.sqrt(n); i++) {
+//			if (n % i == 0) {
+//				return false;
+//			}
+//		}
+//		return true;
+//	}
+//	
 
-	
 
-	private boolean numIsPrime(int n) {
-		for (int i = 2; i <= Math.sqrt(n); i++) {
-			if (n % i == 0) {
-				return false;
+	private boolean isPrime(int k) {
+		for(int i=2; i<Math.sqrt(k); i++){
+			if(k%i == 0){
+				return true;
 			}
 		}
-		return true;
-	}
 	
+		return false;
+	}
 }

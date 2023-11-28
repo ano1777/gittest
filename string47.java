ccf9a8edc435833acadb77957bb9ef1f90e77260
S <s>
2023-11-28 09:54:12 +0400

    {"types":"C","runs":8}

--- string47.java
+++ string47.java
@@ -2,18 +2,34 @@ import acm.program.ConsoleProgram;
 
 public class string47 extends ConsoleProgram {
 	public void run() {
-		while(true) {
-		String string = readLine("Enter text :");
-		if(isPallindrom (string)) {
-			println("your number is a pallindrom. ");
-		}else {
-			println("your number is not a pallindrom. ");
+		String str = readLine("Enter text: ");
+		if(isPallindrom(str)){
+			println("it is pallindrom");
+		}else{
+			println("it is not pallindrom");
 		}
 	}
-	}
-	private boolean isPallindrom(String string) {
-		for (int i = 0; i < string.length() / 2; i++) {
-			if (string.charAt(i) != string.charAt(string.length() - 1 - i)) {
+//		while(true) {
+//		String string = readLine("Enter text :");
+//		if(isPallindrom (string)) {
+//			println("your number is a pallindrom. ");
+//		}else {
+//			println("your number is not a pallindrom. ");
+//		}
+//	}
+//	}
+//	private boolean isPallindrom(String string) {
+//		for (int i = 0; i < string.length() / 2; i++) {
+//			if (string.charAt(i) != string.charAt(string.length() - 1 - i)) {
+//				return false;
+//			}
+//		}
+//		return true;
+//	}
+
+	private boolean isPallindrom(String str) {
+		for(int i = 0; i< str.length()/2; i++){
+			if(str.charAt(i) != str.charAt(str.length()-1-i)){
 				return false;
 			}
 		}

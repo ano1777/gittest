eefa44f23a3a220e7292fe1146febbb33b4fdc98
S <s>
2023-11-29 10:08:25 +0400

    {"types":"AC","runs":4}

--- Graphicss41.java
+++ Graphicss41.java
@@ -6,8 +6,36 @@ import java.awt.Color;
 import java.awt.event.*;
 public class Graphicss41 extends GraphicsProgram{
     private static final int RADIUS = 10;
+
+import java.awt.event.MouseEvent;
+
+import acm.graphics.GLabel;
+import acm.graphics.GRect;
+import acm.program.GraphicsProgram;
+
+public class shualeduri2017 extends GraphicsProgram {
 	
 	public void run() {
+		
+		int x = getWidth()-SQUARE_LENGTH;
+		int y = getHeight()-SQUARE_LENGTH;
+		GRect square = new GRect(x, y, SQUARE_LENGTH, SQUARE_LENGTH);
+		add(square);
+		GLabel score = new GLabel("0");
+		add(score, x + SQUARE_LENGTH/2 - score.getWidth()/2, y + SQUARE_LENGTH/2 -score.getAscent()/2);
+		addMouseListeners();
+}
+	public void mouseDragged (MouseEvent e){
+		double x = e.getX();
+		double y = e.getY();
+	    //if x1>x2  marjvnidan marcxnivaa da unda daiklos
+		//if x2>x1 marcxnidan marjvnivaa da unda gaizardos
+		if(score != 0){
+			
+		}
+	}
+}
+	/*public void run() {
 
 		addMouseListeners();
 		getMouseMotionListeners();
@@ -28,3 +56,4 @@ public class Graphicss41 extends GraphicsProgram{
    	 add(circle, e.getX()-RADIUS, e.getY()-RADIUS);
 	}
 }
+*/

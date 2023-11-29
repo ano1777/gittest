f11b124e640a9912476c042d5ff04813299fc4ec
S <s>
2023-11-29 09:29:30 +0400

    {"types":"C","runs":1}

--- shualeduri2017.java
+++ shualeduri2017.java
@@ -1,4 +1,33 @@
+import acm.program.ConsoleProgram;
 
-public class shualeduri2017 {
+public class shualeduri2017 extends ConsoleProgram {
+	public void run() {
+		String str = readLine();
+		String str2 = readLine();
+		println(spellWord(str, str2));
+	}
+
+	private boolean spellWord(String str, String str2) {
+		int a = 0;
+		int b = 0;
+		for(char i=0; i<200; i++){
+			for(int f=0; f<str.length(); i++){
+			if(str.charAt(f) == i) {
+				a++;
+			}
+		}
+			for(int j=0; j<str2.length(); j++){
+				if(str2.charAt(j) == i) {
+					b++;
+				}
+			}
+			if(b>a) {
+				return false;
+			}
+		
+	}
+		return true;
 
 }
+}
+

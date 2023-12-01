9698b2dabe7e3f5dc409f36a32ce242f6caeb004
S <s>
2023-12-01 12:03:30 +0400

    {"types":"C","runs":1}

--- randomSpeed.java
+++ randomSpeed.java
@@ -1,4 +1,43 @@
+import acm.program.ConsoleProgram;
 
-public class randomSpeed {
+public class randomSpeed extends ConsoleProgram {
+	public void run() {
+		String first = readLine();
+		String sec = readLine();
+		println(isAnagram(first, sec));
+	}
 
+	private boolean isAnagram(String first, String sec) {
+		int m =0;
+		int n =0;
+		if(first.length() != sec.length())
+			return false;
+		else
+			for(int i=0; i<first.length(); i++){
+				char symb = first.charAt(i);
+				 n = countChars(first, symb);
+				 println(n);
+			}
+		for(int j=0; j<sec.length(); j++){
+			  char cha = sec.charAt(j);
+			   m = countChars(sec, cha);
+			   println(m);
+		}
+		if(n == m){
+		return true;
+		}
+		return true;
+	}
+
+	private int countChars(String text, char ch) {
+		int number = 0;
+
+		for (int i = 0; i < text.length(); i++) {
+			if(ch == text.charAt(i)){
+				number ++;
+			}
+
+		}
+		return number;
+	}
 }

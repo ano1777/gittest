c5fc31de450bca3d6e7082ced8e7a520b811f202
S <s>
2023-12-01 12:38:12 +0400

    {"types":"C","runs":19}

--- randomSpeed.java
+++ randomSpeed.java
@@ -8,39 +8,36 @@ public class randomSpeed extends ConsoleProgram {
 	}
 
 	private boolean isAnagram(String first, String sec) {
-	
-		int num1 =0;
-		int num2 =0;
-		if(first.length() != sec.length())
+
+		int num1 = 0;
+		int num2 = 0;
+		if (first.length() != sec.length())
 			return false;
 		else
-			for(int i=0; i<first.length(); i++){
+			for (int i = 0; i < first.length(); i++) {
 				char symb = first.charAt(i);
-				 num1 += (int) symb;
+				num1 += (int) symb;
 			}
-		 println(num1);
-	
 
-		for(int j=0; j<sec.length(); j++){
-			  char cha = sec.charAt(j);
-			   num2+= (int)cha;
+		for (int j = 0; j < sec.length(); j++) {
+			char cha = sec.charAt(j);
+			num2 += (int) cha;
 		}
-		println(num2);
-		if(num1!=num2){
-		return false;
+		if (num1 != num2) {
+			return false;
 		}
 		return true;
 	}
 
-//	private int countChars(String text, char ch) {
-//		int number = 0;
-//
-//		for (int i = 0; i < text.length(); i++) {
-//			if(ch == text.charAt(i)){
-//				number ++;
-//			}
-//
-//		}
-//		return number;
-//	}
+	// private int countChars(String text, char ch) {
+	// int number = 0;
+	//
+	// for (int i = 0; i < text.length(); i++) {
+	// if(ch == text.charAt(i)){
+	// number ++;
+	// }
+	//
+	// }
+	// return number;
+	// }
 }

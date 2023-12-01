1392ed8128563df761061083844aaf70043c4642
S <s>
2023-12-01 17:52:13 +0400

    {"types":"C","runs":52}

--- shualedurebi.java
+++ shualedurebi.java
@@ -1,4 +1,23 @@
+import acm.program.ConsoleProgram;
 
-public class shualedurebi {
+public class shualedurebi  extends ConsoleProgram{
+	public void run() {
+		String father = readLine();
+		String child = readLine();
+		println(iAmYourFather(father, child));
+	}
+
+	private boolean iAmYourFather(String father, String child) {
+		int maxLength = 0;
+		for (int i=0; i<child.length()-1; i++) {
+			for(int j=1; j < father.length(); j++) {
+				String sub = father.substring(i,j);
+				if(sub.equals(child)){
+					maxLength = sub.length();
+				}
+			}
+		}
+		return maxLength > father.length()/2;
+	}
 
 }

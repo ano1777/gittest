f6a9866ff10b770c79b1dd609cabb9dacd45b3b0
S <s>
2023-12-01 12:15:38 +0400

    {"types":"C","runs":13}

--- randomSpeed.java
+++ randomSpeed.java
@@ -29,8 +29,8 @@ public class randomSpeed extends ConsoleProgram {
 			   num2+= (int)cha;
 		}
 		println(num2);
-		if(num1==num2){
-		return true;
+		if(num1!=num2){
+		return false;
 		}
 		return true;
 	}

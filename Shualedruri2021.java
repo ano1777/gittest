12e6a6ccd5eae0fdadab2ff8255dc3f1b1cafb19
S <s>
2023-12-01 16:31:11 +0400

    {"types":"C","runs":48}

--- Shualedruri2021.java
+++ Shualedruri2021.java
@@ -8,7 +8,7 @@ public class Shualedruri2021 extends ConsoleProgram {
 	public void run() {
 
 		String a = "iyo arabets rostevan";
-		String b = "iyo res rostevan";
+		String b = "iyo rets rostevn";
 		println(manipulation(a, b));
 
 	}

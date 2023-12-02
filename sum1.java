9e9a244533300f067a957c12e5fea57ade778339
S <s>
2023-12-02 12:58:35 +0400

    {"types":"C","runs":116}

--- sum1.java
+++ sum1.java
@@ -3,41 +3,80 @@ import acm.program.ConsoleProgram;
 
 public class sum1 extends ConsoleProgram {
 	public void run() {
-		String str = readLine();
-		for(int i=0; i<str.length(); i++) {
-			char ch = str.charAt(i);
-			
-		}
-		
-	}
+		        String str = readLine();
+		        println(reverse(str));
+		    }
 
-	private String upperCase(String str, char ch) {
-		String str2 = str.toUpperCase();
-		return str2;
-	}
-}
-//		int x =1;
-//		int y = 2;
-//		GOval o = new GOval(2,1);
-//		println(one(x, 2, o));
-//		
-//		
-//		
+		    private String reverse(String str) {
+		        String newStr = "";
+		        for (int i = 0; i < str.length(); i++) {
+		            char ch = str.charAt(i);
+		            if (Character.isLetter(ch)) {
+		                newStr = createNew(newStr, ch);
+		            } else {
+		                newStr += ch;
+		            }
+		        }
+		        return newStr;
+		    }
+
+		    private String createNew(String str, char ch) {
+		        return ch + str;
+		    }
+		}
+	
+//		String str = readLine();
+//		println(reverse(str));
 //	}
 //
-//	private String one(int x, int i, GOval o) {
-//		String s = "hello world";
-//		x = racxa(x/y, (int)o.getHeight(), s);
-//		o.scale(2);
-//		y = racxa(x, y+(int)o.getHeigth(), s)
-//		return null;
+//	private String reverse(String str){
+//		String newStr;
+//		for(int i=0; i<str.length(); i++) {
+//			char ch = str.charAt(i);
+//			//if(Character.isLetter(str.charAt(i))) {
+//				  newStr = createNew(str, ch);
+//	//	}
 //	}
+//		return newStr;
+//	}
+//
+//	private String createNew(String str, char ch) {
+//		String result = "";
 //		
-		
-//		double n = readInt("ENTER N :");
-//		double m = readInt("ENTER M) :");
-//		double total = n % m;
-//		println("The sum is :" +total+ ".");
-	
+//			for (int i = str.length(); i >= 0; i--) {
+//				if (Character.isLetter(ch)) {
+//				result += ch;
+//			}
+//				else {
+//					i = i-1;;
+//				}
+//		}
+//		return result;
+//	}
+//}
+//
 
 
+
+// int x =1;
+// int y = 2;
+// GOval o = new GOval(2,1);
+// println(one(x, 2, o));
+//
+//
+//
+// }
+//
+// private String one(int x, int i, GOval o) {
+// String s = "hello world";
+// x = racxa(x/y, (int)o.getHeight(), s);
+// o.scale(2);
+// y = racxa(x, y+(int)o.getHeigth(), s)
+// return null;
+// }
+//
+
+// double n = readInt("ENTER N :");
+// double m = readInt("ENTER M) :");
+// double total = n % m;
+// println("The sum is :" +total+ ".");

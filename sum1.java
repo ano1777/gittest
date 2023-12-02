d243ea36e6e3efa8cef5071e30a244ffe077516d
S <s>
2023-12-02 09:34:35 +0400

    {"types":"C","runs":104}

--- sum1.java
+++ sum1.java
@@ -1,11 +1,33 @@
+import acm.graphics.GOval;
 import acm.program.ConsoleProgram;
 
 public class sum1 extends ConsoleProgram {
 	public void run() {
-		double n = readInt("ENTER N :");
-		double m = readInt("ENTER M) :");
-		double total = n % m;
-		println("The sum is :" +total+ ".");
+		String str = "ania";
+		str.toUpperCase();
 	}
+//		int x =1;
+//		int y = 2;
+//		GOval o = new GOval(2,1);
+//		println(one(x, 2, o));
+//		
+//		
+//		
+//	}
+//
+//	private String one(int x, int i, GOval o) {
+//		String s = "hello world";
+//		x = racxa(x/y, (int)o.getHeight(), s);
+//		o.scale(2);
+//		y = racxa(x, y+(int)o.getHeigth(), s)
+//		return null;
+//	}
+//		
+		
+//		double n = readInt("ENTER N :");
+//		double m = readInt("ENTER M) :");
+//		double total = n % m;
+//		println("The sum is :" +total+ ".");
+	
 
 }

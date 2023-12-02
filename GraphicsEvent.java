db425da79b2569d7762a28528fd0a39c87a09329
S <s>
2023-12-02 10:43:03 +0400

    {"types":"C","runs":112}

--- GraphicsEvent.java
+++ GraphicsEvent.java
@@ -1,45 +1,62 @@
+import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 
 import com.sun.prism.paint.Color;
 
+import acm.graphics.GLabel;
 import acm.graphics.GObject;
 import acm.graphics.GRect;
 import acm.program.GraphicsProgram;
 
 public class GraphicsEvent  extends GraphicsProgram {
-	private GRect newRect;
-	private GRect rect2;
-	label = new GLabel("")
-	public void run(){
-		
-	GRect newRect = new GRect( 100, 100, 150,150);
-	newRect.setFilled(true);
-	add(newRect);
-	
-	addMouseListeners();
-	addKeyListeners();
-	}
-	
-	public void mouseClicked(MouseEvent e) {
-//	System.out.println("here");
-//		e. getX();
-		
-		GObject obj = getElementAt(e.getX(), e.getY());
-		
-		if(obj != null) {
-			obj.setColor(Color.RED);
-			
-		}
-		
-		if(obj == newRect) {
-
-			
-			
-		}
+	GLabel label;
+	public void run() {
+		 label = new GLabel("key typed");
+		add(label, 100,100);
+		addKeyListeners();
+		addMouseListeners();
 		
 	}
-	public void KeyTyped(keyEvent e) {
-		label.setLabel("key typed : ")
-		e.
+	public void mouseDragged(MouseEvent e) {
+		
 	}
+	public void keyTyped(KeyEvent e) {
+		label.setLabel("keey typed" + e.getKeyChar());
 	}
+}
+//	private GRect newRect;
+//	private GRect rect2;
+//	label = new GLabel("")
+//	public void run(){
+//		
+//	GRect newRect = new GRect( 100, 100, 150,150);
+//	newRect.setFilled(true);
+//	add(newRect);
+//	
+//	addMouseListeners();
+//	addKeyListeners();
+//	}
+//	
+//	public void mouseClicked(MouseEvent e) {
+////	System.out.println("here");
+////		e. getX();
+//		
+//		GObject obj = getElementAt(e.getX(), e.getY());
+//		
+//		if(obj != null) {
+//			obj.setColor(Color.RED);
+//			
+//		}
+//		
+//		if(obj == newRect) {
+//
+//			
+//			
+//		}
+//		
+//	}
+//	public void KeyTyped(keyEvent e) {
+//		label.setLabel("key typed : ")
+//		e.
+//	}
+//	}

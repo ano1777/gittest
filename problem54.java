0b4fb4a622bed0b93ff89da59d9e7e3de8383f5a
S <s>
2023-12-07 23:25:54 +0400

    {"types":"C","runs":3}

--- problem54.java
+++ problem54.java
@@ -6,8 +6,10 @@ public class problem54 extends ConsoleProgram {
 	public void run() {
 	int  n = readInt("Enter num :");
 	int sum = 0;
-	for(int i=0; i<n; i++) {
-		sum += n;
+	for(int i=1; i<n+1; i++) {
+		int number = readInt("enter number # " + i + " :");
+		
+		sum += number;
 	}
 	println(sum);
 	}

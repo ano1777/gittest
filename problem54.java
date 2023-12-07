b660e7a33fa8cec80ccc81658a33db66420f78dd
S <s>
2023-12-07 23:34:15 +0400

    {"types":"C","runs":7}

--- problem54.java
+++ problem54.java
@@ -4,14 +4,15 @@ import acm.program.ConsoleProgram;
 
 public class problem54 extends ConsoleProgram {
 	public void run() {
-	int  n = readInt("Enter num :");
-	int sum = 0;
-	for(int i=1; i<n+1; i++) {
-		int number = readInt("enter number # " + i + " :");
-		
-		sum += number;
-	}
-	println(sum);
-	}
+		int n = readInt("Enter num :");
 
+		int maxNum = 0;
+		for (int i = 1; i < n + 1; i++) {
+			int number = readInt("enter number # " + i + " :");
+			if (number > maxNum) {
+				maxNum = number;
+			}
+		}
+		println(maxNum);
+	}
 }

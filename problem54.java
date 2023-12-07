d74492b83591fad48a19d72a430a659465f07d38
S <s>
2023-12-07 20:18:17 +0400

    {"types":"C","runs":9}

--- problem54.java
+++ problem54.java
@@ -1,9 +1,12 @@
+import java.util.Arrays;
+
 import acm.program.ConsoleProgram;
 
 public class problem54 extends ConsoleProgram {
 	public void run() {
-		int n = readInt();
-		
+		int[] n = {1,4,5,6,8};
+		n[2]= n[1]+n[0];
+		println("n is :" + Arrays.toString(n));
 	}
 
 }

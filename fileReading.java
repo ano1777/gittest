7bb324438fe695d2fc612dfa33d2727dd224d10f
S <s>
2023-12-07 15:42:53 +0400

    {"types":"C","runs":1}

--- fileReading.java
+++ fileReading.java
@@ -1,4 +1,28 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
-public class fileReading {
+import acm.program.ConsoleProgram;
 
+public class fileReading extends ConsoleProgram {
+	public void run() {
+		BufferedReader reader;
+		try {
+			reader = new BufferedReader(new FileReader("C:/Users/99559/Desktop/Practice/Practice/fileReading.java"));
+			while (true) {
+				String line = reader.readLine();
+				if (line == null) {
+					break;
+				}
+				
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }

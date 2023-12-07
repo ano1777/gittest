0dbb07d6a766cf31762ba1d39edc1ab51a2ca20d
S <s>
2023-12-07 20:18:54 +0400

    {"types":"C","runs":10}

--- problem54.java
+++ problem54.java
@@ -5,7 +5,7 @@ import acm.program.ConsoleProgram;
 public class problem54 extends ConsoleProgram {
 	public void run() {
 		int[] n = {1,4,5,6,8};
-		n[2]= n[1]+n[0];
+		n[2]= n[1]+n[4];
 		println("n is :" + Arrays.toString(n));
 	}
 

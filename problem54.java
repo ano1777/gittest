500a8dd850f00188d4c93f041e70a30793281aa9
S <s>
2023-12-07 23:53:02 +0400

    {"types":"C","runs":8}

--- problem54.java
+++ problem54.java
@@ -7,12 +7,18 @@ public class problem54 extends ConsoleProgram {
 		int n = readInt("Enter num :");
 
 		int maxNum = 0;
+		int secondMaxNum = 0;
 		for (int i = 1; i < n + 1; i++) {
 			int number = readInt("enter number # " + i + " :");
 			if (number > maxNum) {
 				maxNum = number;
+				if (number > maxNum && number > secondMaxNum) {
+					secondMaxNum = maxNum;
+					maxNum = number;
+				}
 			}
 		}
 		println(maxNum);
+		println(secondMaxNum);
 	}
 }

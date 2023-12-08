c59412cbfe9aaccdc9b30f59a6c9d73081bdf601
S <s>
2023-12-08 12:18:17 +0400

    {"types":"C","runs":20}

--- problem54.java
+++ problem54.java
@@ -4,13 +4,15 @@ import acm.program.ConsoleProgram;
 
 public class problem54 extends ConsoleProgram {
 	public void run() {
-		int n = readInt();
-		int[] num = new int[n];
-		for(int i=0; i<num.length; i++) {
-			n = 3;
+		//int n = readInt();
+		int[] num = new int[4];
+		int[] arr = new int[5];
+		println(Arrays.equals(num,arr));
+		//for(int i=0; i<num.length; i++) {
+			// = 3;
 			
 		}
-		println(num);
+	//	println(num);
 //	    Arrays.sort(num);
 //	    println(num);
 		

dd81625603b51d6a98c70b75c4f9be6ad567af35
S <s>
2023-12-08 13:59:09 +0400

    {"types":"C","runs":58}

--- problem54.java
+++ problem54.java
@@ -3,34 +3,43 @@ import java.util.Arrays;
 import acm.program.ConsoleProgram;
 
 public class problem54 extends ConsoleProgram {
+	int maxIndex = -1;
+
 	public void run() {
 		int n = readInt();
 		int[] arr = new int[n];
 		for (int i = 0; i < n; i++) {
 			arr[i] = readInt();
 		}
-		int max = Integer.MIN_VALUE;
-		println(max);
+
 		println(maxNuber(arr));
 		println(secondMax(arr));
 	}
 
 	private int secondMax(int[] arr) {
-		
-		return 0;
+		int secondMax = 0;
+		for (int i = 0; i < arr.length; i++) {
+			int next = arr[i];
+			if (next > secondMax && i != maxIndex) {
+				secondMax = next;
+			}
+		}
+		return secondMax;
 	}
 
 	private int maxNuber(int[] arr) {
 		int maxNum = 0;
 		for (int i = 0; i < arr.length; i++) {
-			if (arr[i] > maxNum) {
-				maxNum = arr[i];
+			int next = arr[i];
+			if (next > maxNum) {
+				maxNum = next;
+				maxIndex = i;
 			}
 		}
 		return maxNum;
 	}
-
 }
+
 // int n = readInt("Enter num :");
 
 // int maxNum = 0;

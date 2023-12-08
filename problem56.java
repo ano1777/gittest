f7fde894cc6165f7fef1ae4ae623520be5372ed2
S <s>
2023-12-08 14:23:56 +0400

    {"types":"C","runs":62}

--- problem56.java
+++ problem56.java
@@ -1,4 +1,20 @@
+import java.util.Arrays;
 
-public class problem56 {
+import acm.program.ConsoleProgram;
+
+public class problem56 extends ConsoleProgram {
+	public void run() {
+		int[] numbers = {3, 5, 9, 10, 4};
+		int length = numbers.length;
+		
+		for(int i=0; i < length/2; i++) {
+			int temp = numbers[i];
+			numbers[i] = numbers[length - 1 - i] ;
+			numbers[length - 1 - i] = temp;
+		}
+		
+		println(Arrays.toString(numbers));
+		
+	}
 
 }

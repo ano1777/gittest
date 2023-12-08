029de4a4cec99c4fde7e8178ba1194dcd9409b2c
S <s>
2023-12-08 15:25:16 +0400

    {"types":"C","runs":75}

--- problem56.java
+++ problem56.java
@@ -1,80 +1,109 @@
 import java.util.Arrays;
 
 import acm.program.ConsoleProgram;
+
 //
-//public class problem56 extends ConsoleProgram {
-//	int length;
-//	public void run() {
-//		int n = readInt();
-//		int[] arr = new int[n];
-//		for(int i=0; i<arr.length; i++) {
-//			
-//		}
-//		
-//		
-//	}
-//		int n = readInt();
-//
-//		int[] numbers = new int[n];
-//		for (int j = 0; j < n; j++) {
-//			numbers[j] = readInt("enter numbeer : ");
-//		}
-//	
-//		println(Arrays.toString(numbers));
-//		 length = numbers.length;
-//		reverseElements(numbers);
-//
-//	}
-//
-//	private void reverseElements(int[] numbers) {
-//
-//		for (int i = 0; i < length / 2; i++) {
-//			int temp = numbers[i];
-//			numbers[i] = numbers[length - 1 - i];
-//			numbers[length - 1 - i] = temp;
-//		}
-//
-//		println(Arrays.toString(numbers));
-//		
-//	}
-	
-	public class problem56 extends ConsoleProgram {
-		public void run() {
-			int n = readInt("enter  n: ");
-			int[] nums = new int[n];
-			for (int i = 0; i < n; i++) {
-				nums[i] = readInt("enter  nums[" + i+ "]: ");
-			}
-			
-//			for testing
-//			int[] nums = {3, 4, 5, 1, 2, 7, 6};
-			
-			println(Arrays.toString(nums));
-			sort(nums);
-			println(Arrays.toString(nums));
+public class problem56 extends ConsoleProgram {
+	int length;
+
+	public void run() {
+		int n = readInt();
+		int[] arr = new int[n];
+		for (int i = 0; i < n; i++) {
+			arr[i] = readInt("enter number: ");
 		}
 		
-		private void sort(int[] nums) {
-			for(int i = 0; i < nums.length; i++) {
-				int j = findMinIndex(nums, i);
-				swap(nums, i, j);
-			}
-		}
+		println(Arrays.toString(arr));
+		sorter(arr);
+		println(Arrays.toString(arr));
+	}
 
-		private void swap(int[] nums, int i, int j) {
-			int tmp = nums[i];
-			nums[i] = nums[j];
-			nums[j] = tmp;
-		}
-
-		private int findMinIndex(int[] nums, int i) {
-			int minIndexSoFar = i;
-			for(int j = i; j < nums.length; j++) {
-				if(nums[j] < nums[minIndexSoFar]) {
-					minIndexSoFar = j;
-				}
-			}
-			return minIndexSoFar;
+	private void sorter(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			int j = minNumb(arr, i);
+			swap(arr, j, i);
 		}
 	}
 
+	private void swap(int[] arr, int j, int i) {
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+
+	}
+
+	private int minNumb(int[] arr, int i) {
+		int minIndex = i;
+		for (int j = i; j < arr.length; j++) {
+			if(arr[j] < arr[minIndex]){
+				minIndex = j;
+			}
+		}
+		return minIndex;
+	}
+}
+
+// int n = readInt();
+//
+// int[] numbers = new int[n];
+// for (int j = 0; j < n; j++) {
+// numbers[j] = readInt("enter numbeer : ");
+// }
+//
+// println(Arrays.toString(numbers));
+// length = numbers.length;
+// reverseElements(numbers);
+//
+// }
+//
+// private void reverseElements(int[] numbers) {
+//
+// for (int i = 0; i < length / 2; i++) {
+// int temp = numbers[i];
+// numbers[i] = numbers[length - 1 - i];
+// numbers[length - 1 - i] = temp;
+// }
+//
+// println(Arrays.toString(numbers));
+//
+// }
+
+// public class problem56 extends ConsoleProgram {
+// public void run() {
+// int n = readInt("enter n: ");
+// int[] nums = new int[n];
+// for (int i = 0; i < n; i++) {
+// nums[i] = readInt("enter nums[" + i+ "]: ");
+// }
+
+// for testing
+// int[] nums = {3, 4, 5, 1, 2, 7, 6};
+
+// println(Arrays.toString(nums));
+// sort(nums);
+// println(Arrays.toString(nums));
+// }
+//
+// private void sort(int[] nums) {
+// for(int i = 0; i < nums.length; i++) {
+// int j = findMinIndex(nums, i);
+// swap(nums, i, j);
+// }
+// }
+//
+// private void swap(int[] nums, int i, int j) {
+// int tmp = nums[i];
+// nums[i] = nums[j];
+// nums[j] = tmp;
+// }
+//
+// private int findMinIndex(int[] nums, int i) {
+// int minIndexSoFar = i;
+// for(int j = i; j < nums.length; j++) {
+// if(nums[j] < nums[minIndexSoFar]) {
+// minIndexSoFar = j;
+// }
+// }
+// return minIndexSoFar;
+// }
+// }

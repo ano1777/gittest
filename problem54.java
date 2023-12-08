e5d5edea113c79a045714d3313a14c73f4dc3043
S <s>
2023-12-08 13:47:03 +0400

    {"types":"C","runs":57}

--- problem54.java
+++ problem54.java
@@ -5,64 +5,34 @@ import acm.program.ConsoleProgram;
 public class problem54 extends ConsoleProgram {
 	public void run() {
 		int n = readInt();
-		int[] numbers = new int[n];
+		int[] arr = new int[n];
 		for (int i = 0; i < n; i++) {
-			numbers[i] = readInt("enter number: ");
+			arr[i] = readInt();
 		}
-		println(Arrays.toString(numbers));
-	//	println(isSorted(numbers));
-		println(sorted2(numbers));
+		int max = Integer.MIN_VALUE;
+		println(max);
+		println(maxNuber(arr));
+		println(secondMax(arr));
 	}
-//
-//	private boolean isSorted(int[] numbers) {
-//		for (int i = 1; i < numbers.length; i++) {
-//			if (numbers[i] < numbers[i - 1]) {
-//				return false;
-//			}
-//		}
-//		return true;
-//	}
-	 private boolean sorted2 (int [] numbers) {
-	    int[] numbersCopy = Arrays.copyOf(numbers, numbers.length); 
-	    Arrays.sort(numbersCopy);
-	    
-	    return Arrays.equals(numbers, numbersCopy);
-	 }
+
+	private int secondMax(int[] arr) {
+		
+		return 0;
+	}
+
+	private int maxNuber(int[] arr) {
+		int maxNum = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] > maxNum) {
+				maxNum = arr[i];
+			}
+		}
+		return maxNum;
+	}
+
 }
-	// int[] num = new int[5];
-	// String[] array = new String[3];
-	// array [0] = " ";
-	// array [1] = "ani";
-	// array [2] = "kavelashvili";
-	// int k = num[2];
-	// println(k);
-	// println(Arrays.toString(array));
-	//
-	// int[] arr = {2,5,20,1,10};
-	// num[0] = 20;
-	// num[1] = 5;
-	// num[2] = 230;
-	// num[3] = 19;
-	// num[4] = 3;
-	// println("this arrays are equal: " + Arrays.equals(num,arr));
-	// Arrays.sort(num);
-	// println(Arrays.toString(num));
-	// Arrays.sort(arr);
-	// println(Arrays.toString(arr));
-	// Arrays.fill(num, 3);
-	// println(Arrays.toString(num));
-	//
-	// for(int i=0; i<arr.length; i ++){
-	//
-	// }
-
-	// println(num);
-	// Arrays.sort(num);
-	// println(num);
-
-
 // int n = readInt("Enter num :");
-//
+
 // int maxNum = 0;
 // int secondMaxNum = 0;
 // if (maxNum < secondMaxNum) {

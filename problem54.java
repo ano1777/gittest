403ad34ac0b02af5584bdf26c05eb2e827498c91
S <s>
2023-12-08 13:00:48 +0400

    {"types":"C","runs":47}

--- problem54.java
+++ problem54.java
@@ -4,42 +4,64 @@ import acm.program.ConsoleProgram;
 
 public class problem54 extends ConsoleProgram {
 	public void run() {
-		//int n = readInt();
-		int[] num = new int[5];
-		String[] array = new String[3];
-		array [0] = " ";
-		array [1] = "ani";
-		array [2] = "kavelashvili";
-		int k = num[2];	
-		println(k);
-		println(Arrays.toString(array));
-	
-		int[] arr = {2,5,20,1,10};
-		num[0]= 20;
-		num[1] = 5;
-		num[2] = 230;
-		num[3] = 19;
-		num[4] = 3;
-		println("this arrays are equal: " + Arrays.equals(num,arr));
-		Arrays.sort(num);
-		println(Arrays.toString(num));
-		Arrays.sort(arr);
-		println(Arrays.toString(arr));
-		Arrays.fill(num, 3);
-		println(Arrays.toString(num));
+		int n = readInt();
+		int[] numbers = new int [n];
+		for(int i=0; i<n; i++) {
+			 numbers[i] = readInt("enter number: ");
+		}
+		println(Arrays.toString(numbers));
+		println(isSorted(numbers));
+
 		
+		
+	}
+
+		private boolean isSorted(int[] numbers) {
+		    for(int i=0; i<numbers.length; i++) {
+		    	if(numbers[i]<numbers[i+1]) {
+		    		return false;
+		    	}
+		    }
+			return true;
+		}
+			
+//		int[] num = new int[5];
+//		String[] array = new String[3];
+//		array [0] = " ";
+//		array [1] = "ani";
+//		array [2] = "kavelashvili";
+//		int k = num[2];	
+//		println(k);
+//		println(Arrays.toString(array));
+//
+//		int[] arr = {2,5,20,1,10};
+//		num[0] = 20;
+//		num[1] = 5;
+//		num[2] = 230;
+//		num[3] = 19;
+//		num[4] = 3;
+//		println("this arrays are equal: " + Arrays.equals(num,arr));
+//		Arrays.sort(num);
+//		println(Arrays.toString(num));
+//		Arrays.sort(arr);
+//		println(Arrays.toString(arr));
+//		Arrays.fill(num, 3);
+//		println(Arrays.toString(num));
+//		
+//		for(int i=0; i<arr.length; i ++){
+//			
+//		}
 		
 
 	
-	
-		//for(int i=0; i<num.length; i++) {
-			// = 3;
 			
-		}
+		
+
 	//	println(num);
 //	    Arrays.sort(num);
 //	    println(num);
-		
+
+
 		
 		
 		

12630e1df7e5b41b71e1e75dff4ccd68f2481553
S <s>
2023-12-21 17:59:39 +0400

    {"types":"C","runs":0}

--- aatrings.java
+++ aatrings.java
@@ -2,19 +2,23 @@ import acm.program.ConsoleProgram;
 
 public class aatrings extends ConsoleProgram {
 	public void run() {
-	int num1 = readInt("Enter number: ");
-	int num2 = readInt("Enter second number: ");
-	int answer = log(num1, num2);
-	println(log(num1,num2));
+		
+		//iteratori sashualebas gvadzlevs yvela elementi moiaro nebismier setshi,
+		//tokenaizeris msgavsi funqcia aqvs.
 	}
-
-	private int log(int num1, int num2) {
-		int ans = 0;
-		int k = num1;
-		while(num1 <= num2) {
-			num1 *= k;
-			ans++;
-		}
-		return ans;
-	}
+//	int num1 = readInt("Enter number: ");
+//	int num2 = readInt("Enter second number: ");
+//	int answer = log(num1, num2);
+//	println(log(num1,num2));
+//	}
+//
+//	private int log(int num1, int num2) {
+//		int ans = 0;
+//		int k = num1;
+//		while(num1 <= num2) {
+//			num1 *= k;
+//			ans++;
+//		}
+//		return ans;
+//	}
 }

1f56db06377a48727c307edd780d0df85413563d
S <s>
2023-12-22 17:02:46 +0400

    {"types":"C","runs":17}

--- strings.java
+++ strings.java
@@ -3,6 +3,7 @@ import acm.program.ConsoleProgram;
 public class strings extends ConsoleProgram{
 	public void run() {
 		int[][] matrix = new int[3][3];
+		matrix.toString();
 		print(matrix);
 		
 	}

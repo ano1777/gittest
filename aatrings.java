8b4273a3b2f4ad39102557a77731a266de0d73a9
S <s>
2023-12-22 16:53:25 +0400

    {"types":"C","runs":7}

--- aatrings.java
+++ aatrings.java
@@ -2,10 +2,15 @@ import acm.program.ConsoleProgram;
 
 public class aatrings extends ConsoleProgram {
 	public void run() {
+		int [][] bb = new int[4][5];
+		for(int[] num : bb) {
+			print(num);
+		}
 		
+	}
 		//iteratori sashualebas gvadzlevs yvela elementi moiaro nebismier setshi,
 		//tokenaizeris msgavsi funqcia aqvs.
-	}
+	
 //	int num1 = readInt("Enter number: ");
 //	int num2 = readInt("Enter second number: ");
 //	int answer = log(num1, num2);

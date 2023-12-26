1dd1eae9595fb414cbd5cca036f2513cddccda0c
S <s>
2023-12-26 15:58:16 +0400

    {"types":"C","runs":6}

--- problem58.java
+++ problem58.java
@@ -1,4 +1,21 @@
+import acm.program.ConsoleProgram;
 
-public class problem58 {
+public class problem58 extends ConsoleProgram{
+	public void run() {;
+		int[][] matrix1 = {
+				{1, 2, 3},
+				{9, 7, 8},
+				{4, 5, 6}};
+		int[][] matrix2 = {
+				{1, 2, 3},
+				{9, 7, 8},
+				{4, 5, 10}};
+		int[][] matrix3 = {
+				{1, 2, 3},
+				{4, 5, 6}};
+		println(matrix1);
+		println(matrix2);
+		println(matrix3);
+	}
 
 }

b276633f583e68101bbac8895c82ab6d9d810a5f
S <s>
2024-01-07 15:10:45 +0400

    {"types":"C","runs":31}

--- assignment2.java
+++ assignment2.java
@@ -1,6 +1,7 @@
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JLabel;
@@ -14,17 +15,32 @@ import acm.graphics.GRect;
 import acm.program.ConsoleProgram;
 
 public class assignment2 extends ConsoleProgram {
+	private JRadioButton small;
+	private JRadioButton medium;
+	private JRadioButton large;
 	public void init() {
-		JLabel label = new JLabel("ani");
-		add(label, SOUTH);
-		JButton button = new JButton("press");
-		add(button, SOUTH);
-		JCheckBox box = new JCheckBox();
-		add(box, NORTH);
-		JSlider slider = new JSlider();
-		add(slider, NORTH);
-		JRadioButton butn = new JRadioButton();
-		add(butn, NORTH);
+//		JLabel label = new JLabel("ani");
+//		add(label, SOUTH);
+//		JButton button = new JButton("press");
+//		add(button, SOUTH);
+//		JCheckBox box = new JCheckBox();
+//		add(box, NORTH);
+//		JSlider slider = new JSlider();
+//		add(slider, NORTH);
+		small = new JRadioButton("small");
+		medium = new JRadioButton("med");
+		large = new JRadioButton("large");
+		
+		
+		ButtonGroup size = new ButtonGroup();
+		size.add(small);
+		size.add(medium);
+		size.add(large);
+		
+		
+		add(small, NORTH);
+		add(medium, NORTH);
+		add(large, NORTH);
 	}
 
 	public void run() {

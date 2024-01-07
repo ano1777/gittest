9a6f5faa1fc6ff33805f97764f4067f83adeb4aa
S <s>
2024-01-07 15:32:45 +0400

    {"types":"C","runs":35}

--- assignment2.java
+++ assignment2.java
@@ -1,5 +1,6 @@
 import java.awt.Color;
 import java.awt.event.ActionEvent;
+import java.awt.event.MouseEvent;
 
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
@@ -11,13 +12,21 @@ import javax.swing.JTextField;
 
 import acm.graphics.GLabel;
 import acm.graphics.GLine;
+import acm.graphics.GOval;
 import acm.graphics.GRect;
 import acm.program.ConsoleProgram;
+import acm.program.GraphicsProgram;
 
-public class assignment2 extends ConsoleProgram {
+public class assignment2 extends GraphicsProgram {
 	private JRadioButton small;
 	private JRadioButton medium;
 	private JRadioButton large;
+	private static final int SMALL_SIZE = 30;
+	private static final int MED_SIZE = 50;
+	private static final int LARGE_SIZE = 70;
+	double x ;
+	double y;
+	private GOval oval;
 	public void init() {
 //		JLabel label = new JLabel("ani");
 //		add(label, SOUTH);
@@ -27,6 +36,7 @@ public class assignment2 extends ConsoleProgram {
 //		add(box, NORTH);
 //		JSlider slider = new JSlider();
 //		add(slider, NORTH);
+		
 		small = new JRadioButton("small");
 		medium = new JRadioButton("med");
 		large = new JRadioButton("large");
@@ -37,7 +47,7 @@ public class assignment2 extends ConsoleProgram {
 		size.add(medium);
 		size.add(large);
 		
-//		small.setSelected(true);
+		medium.setSelected(true);
 		add(small, NORTH);
 		add(medium, NORTH);
 		add(large, NORTH);
@@ -51,13 +61,28 @@ public class assignment2 extends ConsoleProgram {
 		field.setText("ano");
 		field.addActionListener(this);
 		addActionListeners();
+		addMouseListeners();
 	}
 
+	public void mouseListeners(MouseEvent e) {
+		x = e.getX();
+		y = e.getY();
+	}
 	public void actionPerformed(ActionEvent e) {
 		String command = e.getActionCommand();
 		if(command.equals("press")) {
 		println("ano");
-		} else {
+		
+		} 
+		else if (command.equals("small")) {
+			oval = new GOval(SMALL_SIZE, SMALL_SIZE);
+			add(oval, x, y);
+		}
+		else if( command.equals("medium")) {
+			oval = new GOval(MED_SIZE, MED_SIZE);
+			add(oval,x, y);
+		}
+				else {
 			println ("look");
 		}
 	}

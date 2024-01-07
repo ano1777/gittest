504d8a1e00562a5dcf45930e6d008bb2dc36ada3
S <s>
2024-01-07 13:40:47 +0400

    {"types":"C","runs":28}

--- assignment2.java
+++ assignment2.java
@@ -39,9 +39,8 @@ public class assignment2 extends ConsoleProgram {
 
 	public void actionPerformed(ActionEvent e) {
 		String command = e.getActionCommand();
-		if(command.equals("ani")) {
-		println("ano");
+		println(command);
 	}
 
 }
-}
+

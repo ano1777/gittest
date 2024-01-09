6f949571d3390603ebd7c6c9fcde72d0528e33e9
S <s>
2024-01-09 17:47:38 +0400

    {"types":"C","runs":1}

--- csGroupTest.java
+++ csGroupTest.java
@@ -1,4 +1,18 @@
+import java.util.ArrayList;
 
-public class csGroupTest {
+import acm.program.ConsoleProgram;
 
+public class csGroupTest  extends ConsoleProgram {
+	public void run() {
+		ArrayList<String> students = new ArrayList<>();
+		students.add("sansi23");
+		students.add("akave23");
+		csGroup classB = new csGroup();
+		println(classB.getScore("akave23"));
+		classB.setScore("akave23", 15.0);
+		println(classB.getScore("akave23"));
+		classB.addStudent("akave22");
+		println(classB.getScore("akave22"));
+		
+	}
 }

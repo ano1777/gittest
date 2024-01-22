c237f422b48c7a40c06af86957ec125ba749f271
S <s>
2024-01-22 23:31:27 +0400

    {"types":"C","runs":56}

--- problem63to66.java
+++ problem63to66.java
@@ -15,27 +15,26 @@ public class problem63to66 extends ConsoleProgram {
 				break;
 			}
 			String person = readLine("Enter person: ");
-			
+
 			names.put(person, new ArrayList<String>());
 
 			ArrayList<String> friendsList = names.get(person);
 			friendsList.add(friend);
-			
+
 			names.put(person, friendsList);
 		}
-		
+
 		int mostFriends = 0;
 		String popularPerson = "";
 		for (String key : names.keySet()) {
 			int friendCount = names.get(key).size();
 			if (friendCount > mostFriends) {
 				mostFriends = friendCount;
-				}
-				popularPerson = key;
-			
+			}
+			popularPerson = key;
+
 		}
-		println(popularPerson); //+ " has the most friends : "+ mostFriends);
-		
+		println(popularPerson + " has the most friends : "+ mostFriends); // );
 
 	}
 }

d551fae61c78cbacf97d9bd068d1082e221deddd
S <s>
2024-01-22 19:52:02 +0400

    {"types":"C","runs":28}

--- problem61.java
+++ problem61.java
@@ -8,22 +8,23 @@ public class problem61 extends ConsoleProgram {
 	public void run() {
 		int m = readInt("Enter sum: ");
 		int n = readInt("Enter the num of entries: ");
-		ArrayList<Integer> numList = new  ArrayList<Integer>();
-		
+		ArrayList<Integer> numList = new ArrayList<Integer>();
+
 		for (int i = 0; i < n; i++) {
 			int number = readInt("Enter number: ");
 			numList.add(number);
 		}
 		println(numList.toString());
-		
-		for(int i = 0; i < numList.size() - 1; i ++) {
-			for(int j = i + 1; j < numList.size(); j ++) {
-				if ((numList.get(i) + numList.get(j)) == m ) {
-					println(numList.get(i) + " " + numList.get(j) );
-				} else {
-					println("that numbers do not exist");
+
+		for (int i = 0; i < numList.size() - 1; i++) {
+			for (int j = i + 1; j < numList.size(); j++) {
+				if ((numList.get(i) + numList.get(j)) == m) {
+					println(numList.get(i) + " " + numList.get(j));
 				}
 			}
 		}
+		println("that numbers do not exist");
+
 	}
+
 }

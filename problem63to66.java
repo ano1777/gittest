31912a8c1dde48f091ed36a25af2f35be5c2c301
S <s>
2024-01-22 22:30:10 +0400

    {"types":"C","runs":37}

--- problem63to66.java
+++ problem63to66.java
@@ -1,4 +1,38 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 
-public class problem63to66 {
+import acm.program.ConsoleProgram;
 
+public class problem63to66 extends ConsoleProgram {
+	private static final String SENTINEL = "";
+
+	public void run() {
+		HashMap<String, ArrayList<String>> names = new HashMap<String, ArrayList<String>>();
+		while(true){
+			String friend = readLine("Enter friend: ");
+			String person = readLine("Enter parson: ");
+			if(friend.equals(SENTINEL)){
+				break;
+			}
+			names.put(person, new ArrayList<String>());
+			
+		   	ArrayList<String> friendsList = new ArrayList<String>();
+			friendsList.add(friend);
+			
+			names.put(person, friendsList);
+			
+			for(String key : names.keySet()) {
+				int mostFriends = 0;
+				String popularPerson = "";
+				if(names.get(key).size() > mostFriends) {
+					mostFriends = names.get(key).size();
+					popularPerson = key;
+				}
+				
+				 println(popularPerson);
+			}
+		
+			
+		}
+	}
 }

611f7d33882afc5d94f000a0a015e310410d3422
S <s>
2024-01-22 21:05:15 +0400

    {"types":"C","runs":32}

--- problem62.java
+++ problem62.java
@@ -1,4 +1,25 @@
+import java.util.HashMap;
 
-public class problem62 {
+import acm.program.ConsoleProgram;
+//62. კონსოლიდან შეგვყავს სახელები, მანამ სანამ არ შევიყვანთ ცარიელ სტრინგს. დაბეჭდეთ
+//სახელი და მის გასწვრივ რაოდენობა რამდენჯერ შევიყვანეთ ეს სახელი.
+
+public class problem62 extends ConsoleProgram {
+	private static final String SENTINEL = "";
+	public void run() {
+		HashMap<String, Integer> namesList = new HashMap<String, Integer>();
+		while(true) {
+			String name = readLine("Enter name: ");
+			if(name.equals(SENTINEL)) {
+				break;
+			}
+			if(!namesList.containsKey(name)) {
+				namesList.put(name, 0);
+			}
+			int value = namesList.get(name) + 1;
+			namesList.put(name, value);
+		}
+		println(namesList.toString());
+	}
 
 }

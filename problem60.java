f7ff1b47b4421d1d05454a90e19bb79b437d7147
S <s>
2024-01-22 19:04:58 +0400

    {"types":"C","runs":5}

--- problem60.java
+++ problem60.java
@@ -6,16 +6,24 @@ import acm.program.ConsoleProgram;
 //რიცხვები შებრუნებული მიმდევრობით.
 public class problem60 extends ConsoleProgram {
 	private static final int SENTINEL = -1;
+
 	public void run() {
 		ArrayList<Integer> numList = new ArrayList<Integer>();
 		while (true) {
 			int number = readInt("Enter number");
-			if(number == SENTINEL) {
+			if (number == SENTINEL) {
 				break;
 			}
-		
+
 			numList.add(number);
-				
+
+		}
+		println(numList.toString());
+		for (int i = 0; i < numList.size(); i++) {
+			int memorizedNum = numList.get(numList.size() -i);
+			numList.set(numList.size() - i, numList.get(i));
+			numList.set(i, memorizedNum);
+			
 		}
 		println(numList.toString());
 	}

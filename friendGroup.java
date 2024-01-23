c7de594ce7e6e1a8fa108a697fe925a36df2d51d
S <s>
2024-01-23 16:52:44 +0400

    {"types":"C","runs":41}

--- friendGroup.java
+++ friendGroup.java
@@ -4,16 +4,19 @@ import java.util.HashMap;
 public class friendGroup {
 	HashMap<String, ArrayList<String>> friendships ;
 	public friendGroup() {
+		friendships = new HashMap<String, ArrayList<String>>();
 		System.out.println("constructor called");
 	}
 	
 	public void addFriendship(String friend, String person) {
-		friendships = new HashMap<String, ArrayList<String>>();
+	
 		if(!friendships.containsKey(person)) {
 			friendships.put(person, new ArrayList<String>());
 		}
+		
 		ArrayList<String> friends = friendships.get(person);
 		friends.add(friend);
+		
 		System.out.println(friendships.toString());
 	}
 	

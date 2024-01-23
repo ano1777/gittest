8a5a417833296a727c451e14468a55fe9315da3c
S <s>
2024-01-23 18:00:47 +0400

    {"types":"C","runs":50}

--- friendGroup.java
+++ friendGroup.java
@@ -2,62 +2,66 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class friendGroup {
-	HashMap<String, ArrayList<String>> friendships ;
+	HashMap<String, ArrayList<String>> friendships;
+
 	public friendGroup() {
 		friendships = new HashMap<String, ArrayList<String>>();
-		//System.out.println("constructor called");
+		// System.out.println("constructor called");
 	}
-	
+
 	public void addFriendship(String friend, String person) {
-	
-		if(!friendships.containsKey(person)) {
+
+		if (!friendships.containsKey(person)) {
 			friendships.put(person, new ArrayList<String>());
 		}
-		
+
 		ArrayList<String> friends = friendships.get(person);
-		friends.add(friend);
-	
+		if (!friends.contains(friend)) {
+			friends.add(friend);
+		}
 		System.out.println(friendships);
-		
 	}
 	
-	public String mostPopularPerson () {
+	public int getAmountOfFriends(String person) {
+		return friendships.get(person).size();
+	}
+
+	public String mostPopularPerson() {
 		int maxAmount = 0;
 		String popular = "";
-		for(String key: friendships.keySet()){
+		for (String key : friendships.keySet()) {
 			int currFriendsAmount = friendships.get(key).size();
-			if(currFriendsAmount > maxAmount) {
+			if (currFriendsAmount > maxAmount) {
 				maxAmount = currFriendsAmount;
 				popular = key;
 			}
 		}
-		return popular;	
+		return popular;
 	}
-	
+
 	public String getFriendsAsString(String person) {
-		if(!friendships.containsKey(person)) {
+		if (!friendships.containsKey(person)) {
 			return "Does not have friends";
 		}
-		
-		ArrayList<String> friends = friendships.get(person);	
+
+		ArrayList<String> friends = friendships.get(person);
 		return friends.toString();
 	}
-	
+
 	public ArrayList<String> getAllNames() {
 		ArrayList<String> allNames = new ArrayList<String>();
-		for(String person: friendships.keySet()) {
-			if(!allNames.contains(person)){
+		for (String person : friendships.keySet()) {
+			if (!allNames.contains(person)) {
 				allNames.add(person);
 			}
-		ArrayList<String> friends = friendships.get(person);
-		for(String friend: friends) {
-			if(!allNames.contains(friend)) {
-				allNames.add(friend);
+			ArrayList<String> friends = friendships.get(person);
+			for (String friend : friends) {
+				if (!allNames.contains(friend)) {
+					allNames.add(friend);
+				}
 			}
-		  }
 		}
 		return allNames;
-		
-		
+
 	}
 }

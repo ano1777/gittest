5303ad596cdf538894d40b182742edf1ef5b27f9
S <s>
2024-01-23 16:46:20 +0400

    {"types":"C","runs":38}

--- GroupTesst.java
+++ GroupTesst.java
@@ -2,20 +2,28 @@ import java.util.ArrayList;
 
 import acm.program.ConsoleProgram;
 
-public class GroupTesst  extends ConsoleProgram {
+public class GroupTesst extends ConsoleProgram {
 	public void run() {
+
 		friendGroup friends = new friendGroup();
-		
-		
-		
-//		ArrayList<String> students = new ArrayList<>();
-//		students.add("sansi23");
-//		students.add("akave23");
-//		csGroup classB = new csGroup(students);
-//		println(classB.getScore("akave23"));
-//		classB.setScore("akave23", 15.0);
-//		println(classB.getScore("akave23"));
-//		classB.addStudent("akave22");
-//		println(classB.getScore("akave22"));	
+		while (true) {
+			String friend = readLine("enter friend: ");
+			if (friend.equals("")) {
+				break;
+			}
+			String person = readLine("enter person: ");
+
+			friends.addFriendship(friend, person);
+		}
+
+		// ArrayList<String> students = new ArrayList<>();
+		// students.add("sansi23");
+		// students.add("akave23");
+		// csGroup classB = new csGroup(students);
+		// println(classB.getScore("akave23"));
+		// classB.setScore("akave23", 15.0);
+		// println(classB.getScore("akave23"));
+		// classB.addStudent("akave22");
+		// println(classB.getScore("akave22"));
 	}
 }

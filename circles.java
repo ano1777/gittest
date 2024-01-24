86c934d206e05edc7d9d9aea91634582de5e8a68
S <s>
2024-01-24 22:47:32 +0400

    {"types":"C","runs":203}

--- circles.java
+++ circles.java
@@ -89,8 +89,8 @@ public class circles extends GraphicsProgram {
 			System.out.println(ovals);
 		} else if (obj == ovals.get(ovals.size() - 3)) {
 			remove(obj);
-			remove(getElementAt(obj.getX() + 60, y1));
-			remove(getElementAt(obj.getX() + 120, y1));
+//			remove(getElementAt(obj.getX() + 60, y1));
+//			remove(getElementAt(obj.getX() + 120, y1));
 			ovals.remove(ovals.size() - 3);
 			ovals.remove(ovals.size() - 2);
 			ovals.remove(ovals.size() - 1);

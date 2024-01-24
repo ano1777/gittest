972ce760d58fa2639460344fff5d571ab734cef3
S <s>
2024-01-24 18:05:16 +0400

    {"types":"C","runs":154}

--- rearrange.java
+++ rearrange.java
@@ -10,39 +10,33 @@ public class rearrange extends ConsoleProgram {
 	public void run() {
 		letters = new HashMap<String, Integer>();
 		sent = readLine("enter sentence: ");
-		// if (!sent.contains(" ")) {
-		// word = sent;
-		// letters.put(word, word.length());
-		// } else {
 		for (int i = 0; i < sent.length(); i++) {
-			
-			
+			if (sent.contains(" ")) {
 				char ch = sent.charAt(i);
 				if (ch == ' ') {
 					word = sent.substring(0, i);
 					sent = sent.substring(i + 1);
 					letters.put(word, word.length());
 					println(sent);
-					
-					if (!sent.contains(" ")) {
-						word = sent;
-						letters.put(word, word.length());
-					} 
+				} else if (!sent.contains(" ")) {
+					word = sent;
+					letters.put(word, word.length());
+				}
 
 			}
 		}
 		println(letters);
-//		int maxAmount = 0;
-//		String newSent = "";
-//		for (String key : letters.keySet()) {
-//			int currentAmount = letters.get(key);
-//			if (currentAmount > maxAmount) {
-//				maxAmount = currentAmount;
-//				newSent += " " + key;
-//			} else {
-//				newSent = key + " " + newSent;
-//			}
-//		}
-//		print(newSent);
+		// int maxAmount = 0;
+		// String newSent = "";
+		// for (String key : letters.keySet()) {
+		// int currentAmount = letters.get(key);
+		// if (currentAmount > maxAmount) {
+		// maxAmount = currentAmount;
+		// newSent += " " + key;
+		// } else {
+		// newSent = key + " " + newSent;
+		// }
+		// }
+		// print(newSent);
 	}
 }

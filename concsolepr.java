16d5c9612b1dbf7fe57f91d7d7995899e229846a
S <s>
2024-01-24 18:52:07 +0400

    {"types":"C","runs":163}

--- concsolepr.java
+++ concsolepr.java
@@ -4,27 +4,28 @@ import acm.program.ConsoleProgram;
 import acm.util.RandomGenerator;
 
 public class concsolepr extends ConsoleProgram {
-private RandomGenerator rgen;
-	public void run(){
-		int [] arr = {2, 7, 3, 1, 9, 8, 5, 20, 19};
+	private RandomGenerator rgen;
+
+	public void run() {
+		int[] arr = { 2, 7, 3, 1, 9, 8, 5, 20, 19 };
 		println(Arrays.toString(arr));
 		badSort(arr);
 		println(Arrays.toString(arr));
-}
-	
-	private void badSort(int [] arr) {
-		   rgen = RandomGenerator.getInstance();
-		while(!sorted(arr)) {
-			int a =rgen.nextInt(0, arr.length - 1);
-			int b = rgen.nextInt(0, arr.length -1);
-			
+	}
+
+	private void badSort(int[] arr) {
+		rgen = RandomGenerator.getInstance();
+		while (!sorted(arr)) {
+			int a = rgen.nextInt(0, arr.length - 1);
+			int b = rgen.nextInt(0, arr.length - 1);
+
 			int max = Math.max(a, b);
 			int min = Math.min(a, b);
-			
-			if(arr[max] < arr[min]){
+
+			if (arr[max] < arr[min]) {
 				swap(arr[max], arr[min]);
 			}
-			
+break;
 		}
 	}
 
@@ -33,13 +34,13 @@ private RandomGenerator rgen;
 		i = j;
 		j = temp;
 	}
-	
+
 	private boolean sorted(int[] arr) {
-		for(int i = 1; i < arr.length; i ++) {
-			if(arr[i] < arr[i - 1]) {
+		for (int i = 1; i < arr.length; i++) {
+			if (arr[i] < arr[i - 1]) {
 				return false;
 			}
 		}
 		return true;
 	}
-} 
+}

1c2ed41634800f10f75a7ffba15ffb1e5c7a616c
S <s>
2024-01-24 19:14:50 +0400

    {"types":"C","runs":168}

--- concsolepr.java
+++ concsolepr.java
@@ -7,40 +7,61 @@ public class concsolepr extends ConsoleProgram {
 	private RandomGenerator rgen;
 
 	public void run() {
-		int[] arr = { 2, 7, 3, 1, 9, 8, 5, 20, 19 };
-		println(Arrays.toString(arr));
-		badSort(arr);
-		println(Arrays.toString(arr));
+		String text = readLine();
+		int[] arr = { 1, 5 };
+		addSpaces(text, arr);
+		// int[] arr = {2, 7, 3, 1, 9, 8, 5, 20, 19};
+		// println(Arrays.toString(arr));
+		// badSort(arr);
+		// println(Arrays.toString(arr));
 	}
 
-	private void badSort(int[] arr) {
-		rgen = RandomGenerator.getInstance();
-		while (!sorted(arr)) {
-			int a = rgen.nextInt(0, arr.length - 1);
-			int b = rgen.nextInt(0, arr.length - 1);
-
-			int max = Math.max(a, b);
-			int min = Math.min(a, b);
-
-			if (arr[max] < arr[min]) {
-				swap(arr, max, min);
-			}
-
-		}
-	}
-
-	private void swap(int[] arr, int i, int j) {
-		int temp = arr[i];
-		arr[i] =arr[j];
-		arr[j] = temp;
-	}
-
-	private boolean sorted(int[] arr) {
-		for (int i = 1; i < arr.length; i++) {
-			if (arr[i] < arr[i - 1]) {
-				return false;
+	private void addSpaces(String str, int[] arr) {
+		for (int i = 0; i < str.length(); i++) {
+			for (int j = 0; j < arr.length; j++) {
+				if (i == arr[j]) {
+					String firstPart = str.substring(0, i);
+					String secondPart = str.substring(i);
+					str = firstPart + " " + secondPart;
+				}
 			}
 		}
-		return true;
 	}
+
+	// private void badSort(int[] arr) {
+	// // rgen = RandomGenerator.getInstance();
+	// //while (!sorted(arr)) {
+	//
+	// for(int i = 0; i < arr.length; i ++) {
+	// for(int )
+	// }
+	//
+	//
+	//// int a = rgen.nextInt(0, arr.length - 1);
+	//// int b = rgen.nextInt(0, arr.length - 1);
+	////
+	//// int max = Math.max(a, b);
+	//// int min = Math.min(a, b);
+	////
+	//// if (arr[max] < arr[min]) {
+	//// swap(arr, max, min);
+	//// }
+	//
+	// //}
+	// }
+	//
+	// private void swap(int[] arr, int i, int j) {
+	// int temp = arr[i];
+	// arr[i] = arr[j];
+	// arr[j] = temp;
+	// }
+	//
+	// private boolean sorted(int[] arr) {
+	// for (int i = 1; i < arr.length; i++) {
+	// if (arr[i] < arr[i - 1]) {
+	// return false;
+	// }
+	// }
+	// return true;
+	// }
 }

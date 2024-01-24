231a2bbdff60afcc1b9313db59d187697545aa5a
S <s>
2024-01-24 18:22:37 +0400

    {"types":"C","runs":159}

--- concsolepr.java
+++ concsolepr.java
@@ -1,14 +1,32 @@
+import java.util.Arrays;
+
 import acm.program.ConsoleProgram;
 
 public class concsolepr extends ConsoleProgram {
 	public void run(){
+		int[] arr = {4, 6, 1, 9, 22, 3};
 		
-		println("this program counts the average of two integers");
-	 	double num1 = readInt("Enter num1");
-	 	double num2 = readInt("Enter num2");
-		double averageInDoubles = (double)(num1+num2)/2;
-		print(averageInDoubles);
+		while(!sorted(arr)) {
+			for(int i = 0 ; i < arr.length; i ++) {
+				for(int j = i; j < arr.length; j ++){
+					if(arr[j] < arr[i]){
+						arr[j] = arr[i];
+						arr[i] = arr[j];
+					}
+				}
+			}
+		}
+		println(Arrays.toString(arr));
 		
+	}
+
+	private boolean sorted(int[] arr) {
+		for(int i = 0; i < arr.length - 1; i ++) {
+			if(arr[i] > arr[i + 1]) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 }

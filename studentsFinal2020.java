a54de6b07944d5a398cc0f0efdef07cb9f219e57
S <s>
2024-01-24 16:17:23 +0400

    {"types":"C","runs":141}

--- studentsFinal2020.java
+++ studentsFinal2020.java
@@ -1,4 +1,29 @@
+import java.util.ArrayList;
+import java.util.HashMap;
 
-public class studentsFinal2020 {
+import acm.program.ConsoleProgram;
 
+public class studentsFinal2020 extends ConsoleProgram{
+	HashMap<String, ArrayList<String>> studentFriends;
+	HashMap<String, HashMap<String, Double>> studentsSubjectsGrades;
+	HashMap<String, Double> courseAverage;
+	public void run() {
+		luckyStudent();
+		getAverageGradeOfEachCourse();
+		getAverageGradeOfAllCourses();
+		
+	}
+	private String luckyStudent(HahMap<String, ArrayList<String>> , HashMap<String,  Hashap<String, Double>>) {
+		
+	}
+	//yvela kursis sashualo nishnebis sashualo, anu kursebis sashualoebis jami gayofili kursebis raodenobaze
+	private void getAverageGradeOfAllCourses() {
+		
+	}
+	
+	//kursis sashualo qula - titoeuli kursis yvela studentis qulebis jami gayofili mat raodenobaze.
+	private void getAverageGradeOfEachCourse() {
+		
+	}
+	
 }

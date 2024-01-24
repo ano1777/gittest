d34e8a6d98ced0fab3af2ea4df63685b2073ab18
S <s>
2024-01-24 23:39:41 +0400

    {"types":"C","runs":219}

--- circles.java
+++ circles.java
@@ -39,17 +39,23 @@
 //		println("largest : " + max);
 //
 //	}
-
-import acm.program.*;
-import acm.graphics.*;
-
 import java.awt.Color;
-import java.awt.event.*;
-import java.util.ArrayList;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+
+import acm.graphics.GLabel;
+import acm.graphics.GObject;
+import acm.graphics.GOval;
+import acm.program.GraphicsProgram;
+
+//https://www.youtube.com/playlist?list=PL6qg6DlVkjd6XenORSl6px7Z_NweBs8yf
 public class circles extends GraphicsProgram {
-	ArrayList<GOval> coins;
-	double y1;
+	private static final int N_COINS = 11; // ქვების რაოდენობა
+	private static final int COIN_SIZE = 25; // ქვის ზომა
+	private static final int COIN_STEP = 10; // ქვებს შორის მანძილი
+
+	private ArrayList<GOval> coins;
+	private int player = 0;
 
 	public void run() {
 		addCoins();
@@ -57,30 +63,22 @@ public class circles extends GraphicsProgram {
 	}
 
 	private void addCoins() {
-		coins = new ArrayList<GOval>();
-		double x = (getWidth() - 11 * 50 - 10 * 10) / 2.0;
-		y1 = getHeight() / 2 - 25;
-		for (int i = 0; i < 11; i++) {
-			GOval oval = new GOval(50, 50);
-			oval.setFilled(true);
-			oval.setColor(Color.BLACK);
-			oval.setFillColor(Color.GRAY);
-			double xebi = x + i * (50 + 10);
-			add(oval, xebi, y1);
-			coins.add(i, oval);
+		coins = new ArrayList<>();
+
+		double startX = (getWidth() - N_COINS * COIN_SIZE - (N_COINS - 1) * COIN_STEP) / 2.0;
+		for (int i = 0; i < N_COINS; i++) {
+			GOval coin = new GOval(COIN_SIZE, COIN_SIZE);
+			coin.setFilled(true);
+			coin.setFillColor(Color.GRAY);
+			double x = startX + i * (COIN_SIZE + COIN_STEP);
+			double y = (getHeight() - COIN_SIZE) / 2;
+			add(coin, x, y);
+			coins.add(coin);
 		}
 	}
 
-	private void removeCoins(int num) {
-		for (int i = 0; i > num; i++) {
-			remove(coins.get(coins.size() - 1));
-			coins.remove(coins.size() - 1);
-		}
-
-	}
-
-	public void mousePressed(MouseEvent e) {
-		GObject obj = getElementAt(e.getX(), e.getY());
+	public void mouseClicked(MouseEvent E) {
+		GObject obj = getElementAt(E.getX(), E.getY());
 		int num = -1;
 		if (coins.size() > 0 && obj == coins.get(coins.size() - 1)) {
 			num = 1;
@@ -94,5 +92,20 @@ public class circles extends GraphicsProgram {
 
 		removeCoins(num);
 
+		player = (player + 1) % 2;
+		if (coins.size() == 0) {
+			playerWon(player);
+		}
+	}
+
+	private void playerWon(int player) {
+		add(new GLabel("Player " + (player + 1) + " won!"), 50, 50);
+	}
+
+	private void removeCoins(int num) {
+		for (int i = 0; i < num; i++) {
+			remove(coins.get(coins.size() - 1));
+			coins.remove(coins.size() - 1);
+		}
 	}
 }

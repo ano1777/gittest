79b8db780dd8d5c1f9cb40ef612993d0bc1b1599
S <s>
2024-01-29 19:48:31 +0400

    {"types":"C","runs":230}

--- shualedurebi.java
+++ shualedurebi.java
@@ -5,17 +5,10 @@ import java.util.HashMap;
 import acm.program.ConsoleProgram;
 
 public class shualedurebi extends ConsoleProgram {
-	HashMap<String, ArrayList<String>> mealIngr;
-	// puri - marili,fqvili
-	// xachapuri - marili, fqvili, yveli
-	// burg - puri, yveli, salati
-
-	ArrayList<String> fridge;
-	// yveli, fqvili, marili
 
 	public void run() {
-		mealIngr = new HashMap<String, ArrayList<String>>();
-	    fridge = new ArrayList<String>();
+		HashMap<String, ArrayList<String>> mealIngr = new HashMap<String, ArrayList<String>>();
+		ArrayList<String> fridge = new ArrayList<String>();
 
 		fridge.add("yveli");
 		fridge.add("fqvili");
@@ -34,31 +27,27 @@ public class shualedurebi extends ConsoleProgram {
 		burg.add("salati");
 		burg.add("puri");
 		burg.add("yveli");
+		
+		ArrayList<String> an = new ArrayList<String>();
+		an.add("banani");
+		an.add("puri");
 
 		mealIngr.put("puri", puris);
 		mealIngr.put("xacha", xachas);
 		mealIngr.put("burg", burg);
+		mealIngr.put("ann", an);
 
 		println(kitchen(mealIngr, fridge));
 	}
 
 	private ArrayList<String> kitchen(HashMap<String, ArrayList<String>> mealIngr, ArrayList<String> fridge) {
 		ArrayList<String> canDoMeals = new ArrayList<String>();
-
 		for (String meal : mealIngr.keySet()) {
 			ArrayList<String> receipt = mealIngr.get(meal);
 			if (canDo(receipt, fridge)) {
 				canDoMeals.add(meal);
 			}
-		}
-//			for (String eachIngr : receipt) {
-//				if (canDo(eachIngr)) {
-//					if (!canDoMeals.contains(meal)) {
-//						canDoMeals.add(meal);
-//					}
-//				}
-//			}
-		
+		}		
 			return canDoMeals;
 	}
 

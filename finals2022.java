84d756978a06faec0de3af6c91ff5b9c53e6f414
S <s>
2024-01-30 12:35:42 +0400

    {"types":"C","runs":241}

--- finals2022.java
+++ finals2022.java
@@ -8,31 +8,26 @@ public class finals2022 extends ConsoleProgram {
 
 	// aaaaaddssssiiiii
 	private String compressedStr(String str) {
-		String newPart1 = "";
+	
 		String res = "";
+		int count = 1;
 
-		for (int i = 0; i < str.length() - 1; i++) {
+		for (int i = 1; i < str.length(); i++) {
 			char ch = str.charAt(i);
-			char ch2 = str.charAt(i + 1);
-			if (ch == str.length() - 1 && !Character.isDigit(ch)) {
-				str += 1;
+			char ch2 = str.charAt(i - 1);
+			res += ch2;
+			if (ch2 == ch) {
+				count++;
+				
+			} else {
+				res += count;
+				count = 1;
+				res += ch;
 			}
-			if (ch != ch2) {
-				String part1 = str.substring(0, i + 1);
-				String part2 = str.substring(i + 1);
-				int amount = 0;
-				for (int j = 0; j < part1.length(); j++) {
-					amount++;
-				}
-				newPart1 = ch + String.valueOf(amount);
-				res = newPart1 + part2;
-
-				if (res.length() < str.length()) {
-					return str;
-				}
+			if (res.length() > str.length()) {
+				return str;
 			}
 		}
 		return res;
 	}
-
 }

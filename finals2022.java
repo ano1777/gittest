c0c528755b3975f6000fe32f40353d8554201340
S <s>
2024-01-30 14:16:53 +0400

    {"types":"C","runs":265}

--- finals2022.java
+++ finals2022.java
@@ -1,42 +1,51 @@
+import java.util.Arrays;
+
 import acm.program.ConsoleProgram;
 
 public class finals2022 extends ConsoleProgram {
 	private int amount;
 	public void run() {
-		String s = readLine();
-		println(amount);
-		println(blowUp(s));
+		String a = "gvcaja";
+		char[] arr  = a.toCharArray();
+		Arrays.sort(arr);
+		String sorted = new String(arr);
+		println(sorted);
 	}
-
-	/// a2/s6/a1/3
-	private String blowUp(String s) {
-		if(s.length() == 0){
-			return "";
-		}
-		
-		String res = "" + s.charAt(0);
-		for (int i = 1; i < s.length(); i++) {
-			if (Character.isDigit(i)) {
-				char ch = s.charAt(i);
-			amount = 	Character.getNumericValue(ch);
-				// amount = Character.getNumericValue(s.charAt(i));    // int =2 2 a unda daematos
-		
-				for(int j = 0; j < amount; j ++){
-					res+= s.charAt(i-1);
-				}
-				if(i != s.length()-1){
-					res += s.charAt(i+1);
-				}
-				
-				
-			}
-
-		}
-		return res;
-
 	}
-}
-// aaaaaddssssiiiii
+//		String s = readLine();
+//		println(amount);
+//		println(blowUp(s));
+//	}
+//
+//	/// a2/s6/a1/3
+//	private String blowUp(String s) {
+//		if(s.length() == 0){
+//			return "";
+//		}
+//		
+//		String res = "" + s.charAt(0);
+//		for (int i = 1; i < s.length(); i++) {
+//			if (Character.isDigit(i)) {
+//				char ch = s.charAt(i);
+//			amount = Character.getNumericValue(ch);
+//				// amount = Character.getNumericValue(s.charAt(i));    // int =2 2 a unda daematos
+//		
+//				for(int j = 0; j < amount; j ++){
+//					res+= s.charAt(i-1);
+//				}
+//				if(i != s.length()-1){
+//					res += s.charAt(i+1);
+//				}
+//				
+//				
+//			}
+//
+//		}
+//		return res;
+//
+//	}
+//}
+//// aaaaaddssssiiiii
 // private String compressedStr(String str) {
 // if(str.length() ==0){
 // return "";

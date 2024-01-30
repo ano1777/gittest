ac1e1add4cd1e53490f3917c7efae6b86d730d10
S <s>
2024-01-30 22:59:46 +0400

    {"types":"C","runs":20}

--- goodFriends.java
+++ goodFriends.java
@@ -61,13 +61,12 @@ public class goodFriends extends GraphicsProgram implements ComponentListener {
 
 	@Override
 	public void componentMoved(ComponentEvent arg0) {
-		run();
+		
 	}
 	@Override
 	public void componentResized(ComponentEvent arg0) {
 		removeAll();
 		run();
-		
 	}
 
 	@Override

c28909d8a53a3984f11dd292305ae5b80627cc46
S <s>
2024-01-30 22:09:42 +0400

    {"types":"C","runs":1}

--- goodFriends.java
+++ goodFriends.java
@@ -1,30 +1,88 @@
+import java.awt.event.ActionEvent;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+import acm.graphics.GRect;
 import acm.program.ConsoleProgram;
+import acm.program.GraphicsProgram;
 
-public class goodFriends extends ConsoleProgram{
-	Map<String, ArrayList<String>> friends;
-	Map<String, HashMap<String, Double>> courses;
-	
+public class goodFriends extends GraphicsProgram {
+	private JTextField name;
+	private JLabel nameLabel;
+	private JLabel x;
+	private JTextField xText;
+	private JLabel y;
+	private JLabel width;
+	private JLabel height;
+	private JTextField ytext;
+	private JTextField heightText;
+	private JTextField widthText;
+	private JButton add;
 
+	public void init() {
+
+		nameLabel = new JLabel("name");
+		add(nameLabel, SOUTH);
+
+		name = new JTextField(5);
+		add(name, SOUTH);
+
+		x = new JLabel("x");
+		add(x, SOUTH);
+
+		xText = new JTextField(5);
+		xText.addActionListener(this);
+		add(xText, SOUTH);
+
+		y = new JLabel("y");
+		add(y, SOUTH);
+
+		ytext = new JTextField(5);
+		ytext.addActionListener(this);
+		add(ytext, SOUTH);
+
+		height = new JLabel("width");
+		add(height, SOUTH);
+
+		heightText = new JTextField(5);
+		heightText.addActionListener(this);
+		add(heightText, SOUTH);
+
+		width = new JLabel("width");
+		add(width, SOUTH);
+
+		widthText = new JTextField(5);
+		widthText.addActionListener(this);
+		add(widthText, SOUTH);
+
+		add = new JButton("Add");
+		add(add, SOUTH);
+
+		addActionListeners();
+		addMouseListeners();
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		int x = Integer.parseInt(xText.getText());
+		int y = Integer.parseInt(ytext.getText());
+		int width = Integer.parseInt(widthText.getText());
+		int height = Integer.parseInt(heightText.getText());
 	
-	
-	
-	
-	private String goodFriends() {
-		String result = "";
-		int maxLength = 0;
-		for(String key : friends.keySet()) {
-			ArrayList<String> goodFriens
+		if (e.getSource() == "add") {
+			if (name.getText() != ("") && xText.getText() != ("") && ytext.getText() != ("")
+					&& widthText.getText() != ("") && heightText.getText() != ("")) {
+
+				GRect rectName = new GRect(width, height);
+				add(rectName, x, y);
+
+			}
 		}
-		
-		return result;
-		
 	}
-	
-	private ArrayList <String> getGoodfRIENDS(String name) {
-		
+
+	public void run() {
+
 	}
+
 }
-
-
-
-

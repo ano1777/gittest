1e3f4dc0189bf86b1d647cec0003a43d6e2da247
S <s>
2024-01-30 22:35:10 +0400

    {"types":"C","runs":3}

--- goodFriends.java
+++ goodFriends.java
@@ -11,78 +11,32 @@ import acm.program.GraphicsProgram;
 public class goodFriends extends GraphicsProgram {
 	private JTextField name;
 	private JLabel nameLabel;
-	private JLabel x;
-	private JTextField xText;
-	private JLabel y;
-	private JLabel width;
-	private JLabel height;
-	private JTextField ytext;
-	private JTextField heightText;
-	private JTextField widthText;
-	private JButton add;
 
 	public void init() {
-
-		nameLabel = new JLabel("name");
+		nameLabel = new JLabel("directions");
 		add(nameLabel, SOUTH);
 
-		name = new JTextField(5);
+		name = new JTextField(10);
 		add(name, SOUTH);
-
-		x = new JLabel("x");
-		add(x, SOUTH);
-
-		xText = new JTextField(5);
-		xText.addActionListener(this);
-		add(xText, SOUTH);
-
-		y = new JLabel("y");
-		add(y, SOUTH);
-
-		ytext = new JTextField(5);
-		ytext.addActionListener(this);
-		add(ytext, SOUTH);
-
-		height = new JLabel("width");
-		add(height, SOUTH);
-
-		heightText = new JTextField(5);
-		heightText.addActionListener(this);
-		add(heightText, SOUTH);
-
-		width = new JLabel("width");
-		add(width, SOUTH);
-
-		widthText = new JTextField(5);
-		widthText.addActionListener(this);
-		add(widthText, SOUTH);
-
-		add = new JButton("Add");
-		add.addActionListener(this);
-		add(add, SOUTH);
 
 		addActionListeners();
 		addMouseListeners();
 	}
 
 	public void actionPerformed(ActionEvent e) {
-		int x = Integer.parseInt(xText.getText());
-		int y = Integer.parseInt(ytext.getText());
-		int width = Integer.parseInt(widthText.getText());
-		int height = Integer.parseInt(heightText.getText());
-	
-		if (e.getSource() == "add") {
-			if (name.getText() != ("") && xText.getText() != ("") && ytext.getText() != ("")
-					&& widthText.getText() != ("") && heightText.getText() != ("")) {
-
-				GRect rectName = new GRect(width, height);
-				add(rectName, x, y);
-
-			}
+		if (e.getSource() == name) {
 		}
 	}
 
 	public void run() {
+		double size = Math.min(getWidth(), getHeight());
+		int rectSize = (int) size / 8;
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < 8; j++) {
+				GRect myRect = new GRect(rectSize, rectSize);
+				add(myRect, rectSize *i, rectSize *j);
+			}
+		}
 
 	}
 

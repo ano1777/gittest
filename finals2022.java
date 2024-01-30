c65ab6f08db1d0f9812da6ccfac30559129612e7
S <s>
2024-01-30 13:20:30 +0400

    {"types":"C","runs":253}

--- finals2022.java
+++ finals2022.java
@@ -8,15 +8,22 @@ public class finals2022 extends ConsoleProgram {
 
 	/// a2/s6/a1/3
 	private String blowUp(String s) {
+		if(s.length() == 0){
+			return "";
+		}
 		
 		String res = "" + s.charAt(0);
 		for (int i = 1; i < s.length(); i++) {
 			if (Character.isDigit(i)) {
 				String integer = "" + s.charAt(i);
 				int amount = Integer.parseInt(integer);// int =2 2 a unda daematos
-				for(int j =0; j < amount; j ++){
-					res+= s.charAt(i);
+				for(int j = 0; j < amount; j ++){
+					res+= s.charAt(i-1);
 				}
+				if(i != s.length()-1){
+					res += s.charAt(i+1);
+				}
+				
 				
 			}
 

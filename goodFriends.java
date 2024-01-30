ee00075e0117c2ecab4c51e959cf5f47c6a504e7
S <s>
2024-01-30 19:26:35 +0400

    {"types":"C","runs":265}

--- goodFriends.java
+++ goodFriends.java
@@ -1,4 +1,30 @@
+import acm.program.ConsoleProgram;
 
-public class goodFriends {
+public class goodFriends extends ConsoleProgram{
+	Map<String, ArrayList<String>> friends;
+	Map<String, HashMap<String, Double>> courses;
+	
 
+	
+	
+	
+	
+	private String goodFriends() {
+		String result = "";
+		int maxLength = 0;
+		for(String key : friends.keySet()) {
+			ArrayList<String> goodFriens
+		}
+		
+		return result;
+		
+	}
+	
+	private ArrayList <String> getGoodfRIENDS(String name) {
+		
+	}
 }
+
+
+
+

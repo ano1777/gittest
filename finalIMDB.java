c5d5499d2ab1e4dc718a48539e24aac4926d6ab9
S <s>
2024-01-31 15:39:54 +0400

    {"types":"C","runs":59}

---
+++ finalIMDB.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import acm.program.ConsoleProgram;
+
+public class finalIMDB extends ConsoleProgram{
+	HashMap<String, ArrayList<String>> filmsActors;
+	HashMap<String, HashMap<String, Double>> studentsSubjectsGrades;
+	HashMap<String, Double> courseAverage;
+	public void run() {
+		
+	}
+}
